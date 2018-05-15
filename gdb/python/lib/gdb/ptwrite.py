a37e7a5f2a3b7d45a673bf0f281df393187c6ae2
Felix Willgerodt <felix.willgerodt@intel.com>
2018-05-15 15:42:24 +0200

    [PTWRITE] btrace, python: Enable ptwrite listener registration.
    With this patch a default ptwrite listener is registered upon start of GDB.
    It prints the plain ptwrite payload as hex.  The default listener can be
    overwritten by registering a custom listener in python or by registering
    "None", for no output at all.  Registering a listener function creates per
    thread copies to allow unique internal states per thread.
    
    gdb/ChangeLog:
    2018-05-15  Felix Willgerodt  <felix.willgerodt@intel.com>
    
    	* btrace.c (ftrace_add_pt): Add call to
    	apply_ext_lang_ptwrite_listener.
    	* btrace.h (btrace_thread_info): New member ptw_listener.
    	* data-directory/Makefile.in: Add ptwrite.py.
    	* extension-priv.h (struct extension_language_ops): New member
    	apply_ptwrite_listener.
    	* extension.c (apply_ext_lang_ptwrite_listener): New function.
    	* extension.h (apply_ext_lang_ptwrite_listener): New declaration.
    	* guile/guile.c (guile_extension_ops): Add
    	gdbscm_load_ptwrite_listener.
    	* python/lib/gdb/ptwrite.py: New file.
    	* python/py-record-btrace.c (recpy_initialize_listener,
    	get_ptwrite_listener): New function.
    	* python/py-record-btrace.h (recpy_initialize_listener): New
    	declaration.
    	* python/py-record.c (gdbpy_load_ptwrite_listener): New function.
    	* python/python-internal.h (gdbpy_load_ptwrite_listener): New
    	declaration.
    	* python/python.c (python_extension_ops): New member
    	gdbpy_load_ptwrite_listener.
    
    Signed-off-by: Felix Willgerodt <felix.willgerodt@intel.com>

---
+++ gdb/python/lib/gdb/ptwrite.py
@@ -0,0 +1,81 @@
+# Ptwrite utilities.
+# Copyright (C) 2018 Free Software Foundation, Inc.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Utilities for working with ptwrite listeners."""
+
+import gdb
+from copy import deepcopy
+
+
+def default_listener(payload, ip):
+    """Default listener that is active upon starting GDB."""
+    return "payload: {:#x}".format(payload)
+
+# This dict contains the per thread copies of the listener function and the
+# global template listener, from which the copies are created.
+_ptwrite_listener = {"global" : default_listener}
+
+
+def _update_listener_dict(thread_list):
+    """Helper function to update the listener dict.
+
+    Discards listener copies of threads that already exited and registers
+    copies of the listener for new threads."""
+    # thread_list[x].ptid returns the tuple (pid, lwp, tid)
+    lwp_list = [i.ptid[1] for i in thread_list]
+
+    # clean-up old listeners
+    for key in _ptwrite_listener.keys():
+      if key not in lwp_list and key != "global":
+        _ptwrite_listener.pop(key)
+
+    # Register listener for new threads
+    for key in lwp_list:
+        if key not in _ptwrite_listener.keys():
+            _ptwrite_listener[key] = deepcopy(_ptwrite_listener["global"])
+
+
+def _clear_traces(thread_list):
+    """Helper function to clear the trace of all threads."""
+    current_thread = gdb.selected_thread()
+
+    for thread in thread_list:
+        thread.switch()
+        gdb.current_recording().clear_trace()
+
+    current_thread.switch()
+
+
+def register_listener(listener):
+    """Register the ptwrite listener function."""
+    if listener is not None and not callable(listener):
+        raise TypeError("Listener must be callable!")
+
+    thread_list = gdb.Inferior.threads(gdb.selected_inferior())
+    _clear_traces(thread_list)
+
+    _ptwrite_listener.clear()
+    _ptwrite_listener["global"] = listener
+
+    _update_listener_dict(thread_list)
+
+
+def get_listener():
+    """Returns the listeners of the current thread."""
+    thread_list = gdb.Inferior.threads(gdb.selected_inferior())
+    _update_listener_dict(thread_list)
+
+    return _ptwrite_listener[gdb.selected_thread().ptid[1]]

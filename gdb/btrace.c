6ef3854b3e00fa531d754f653ca478db8531c927
Felix Willgerodt <felix.willgerodt@intel.com>
2018-05-15 15:42:24 +0200

    ptwrite, btrace, python: Enable ptwrite listener registration.
    With this patch a default ptwrite listener is registered upon start of GDB.
    It prints the plain ptwrite payload as hex.  The default listener can be
    overwritten by registering a custom listener in python or by registering
    "None", for no output at all.  Registering a listener function creates per
    thread copies to allow unique internal states per thread.
    
    gdb/ChangeLog:
    2018-05-15  Felix Willgerodt  <felix.willgerodt@intel.com>
    
    	* btrace.c (ftrace_add_pt): Add call to
    	apply_ext_lang_ptwrite_listener.
    	* btrace.h (btrace_thread_info): New member ptw_listener.
    	* data-directory/Makefile.in: Add ptwrite.py.
    	* extension-priv.h (struct extension_language_ops): New member
    	apply_ptwrite_listener.
    	* extension.c (apply_ext_lang_ptwrite_listener): New function.
    	* extension.h (apply_ext_lang_ptwrite_listener): New declaration.
    	* guile/guile.c (guile_extension_ops): Add
    	gdbscm_load_ptwrite_listener.
    	* python/lib/gdb/ptwrite.py: New file.
    	* python/py-record-btrace.c (recpy_initialize_listener,
    	get_ptwrite_listener): New function.
    	* python/py-record-btrace.h (recpy_initialize_listener): New
    	declaration.
    	* python/py-record.c (gdbpy_load_ptwrite_listener): New function.
    	* python/python-internal.h (gdbpy_load_ptwrite_listener): New
    	declaration.
    	* python/python.c (python_extension_ops): New member
    	gdbpy_load_ptwrite_listener.
    
    Signed-off-by: Felix Willgerodt <felix.willgerodt@intel.com>

--- gdb/btrace.c
+++ gdb/btrace.c
@@ -34,6 +34,7 @@
 #include "gdbsupport/rsp-low.h"
 #include "gdbcmd.h"
 #include "cli/cli-utils.h"
+#include "extension.h"
 #include "gdbarch.h"
 
 /* For maintenance commands.  */
@@ -1316,6 +1317,8 @@ ftrace_add_pt (struct btrace_thread_info *btinfo,
   struct btrace_function *bfun;
   uint64_t offset;
   int status;
+
+  apply_ext_lang_ptwrite_listener (inferior_ptid);
 
   for (;;)
     {

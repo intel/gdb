becf75725d63abe3ea19f81ce4298f0d0797ac1e
Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>
2018-11-15 15:08:06 +0100

    testsuite: add SYCL support
    Add lib files, board files, and basic test scenarios for SYCL.  The
    scenarios test the ability to define and hit breakpoints inside and
    outside a SYCL kernel, and running a SYCL program until termination.
    The test cases will be skipped if it is found out that the compiler
    does not support SYCL.
    
    The SYCL device can be selected by passing the SYCL program two
    command line options:
    
      1. the device type, whose value is one of {"host", "cpu", "gpu",
      "accelerator"}.
      2. a substring of the device name.
    
    These arguments are *not* passed explicitly by the programmer, but via
    the selection of a SYCL target board. Sample board files are also
    included in this patch.
    
    gdb/testsuite/ChangeLog:
    2018-11-15  Tankut Baris Aktemur  <tankut.baris.aktemur@intel.com>
    
    	* boards/sycl-cpu-intel.exp: New file.
    	* boards/sycl-cpu.exp: New file.
    	* boards/sycl-host.exp: New file.
    	* boards/sycl-intelfpga.exp: New file.
    	* boards/sycl-intelgt.exp: New file.
    	* gdb.sycl/break-before-kernel.exp: New file.
    	* gdb.sycl/break.exp: New file.
    	* gdb.sycl/break2.exp: New file.
    	* gdb.sycl/run-to-termination.exp: New file.
    	* gdb.sycl/single-task.cpp: New file.
    	* lib/sycl-hello.cpp: New file.
    	* lib/sycl-util.cpp: New file.
    	* lib/sycl.exp: New file.
    
    Signed-off-by: Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>

---
+++ gdb/testsuite/lib/sycl-hello.cpp
@@ -0,0 +1,44 @@
+/* This testcase is part of GDB, the GNU debugger.
+
+   Copyright 2019 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <CL/sycl.hpp>
+#include "sycl-util.cpp"
+
+static int numbers[8];
+
+int
+main (int argc, char *argv[])
+{
+  cl::sycl::queue deviceQueue {get_sycl_queue (argc, argv)};
+  cl::sycl::range<1> length {8};
+
+  { /* Extra scope enforces waiting on the kernel.  */
+    cl::sycl::buffer<int, 1> buf {numbers, length};
+    deviceQueue.submit ([&] (cl::sycl::handler& cgh)
+      {
+	auto accessor
+	  = buf.get_access<cl::sycl::access::mode::read_write> (cgh);
+
+	cgh.parallel_for<class SyclHello> (length, [=] (cl::sycl::id<1> wiID)
+	  {
+	    accessor[wiID] = wiID[0] + 1;
+	  });
+      });
+  }
+
+  return 0;
+}

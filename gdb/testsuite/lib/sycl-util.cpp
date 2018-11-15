9a7ad297bd3382e2077f1740738cf3d8872b50f4
Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>
2018-11-15 15:08:06 +0100

    testsuite: add SYCL support
    Add lib files, board files, and basic test scenarios for SYCL.  The
    scenarios test the ability to define and hit breakpoints inside and
    outside a SYCL kernel, and running a SYCL program until termination.
    The test cases will be skipped if it is found out that the compiler
    does not support SYCL.
    
    The SYCL device can be selected by passing the SYCL program two
    command line options:
    
      1. the device type, whose value is one of {"host", "cpu", "gpu",
      "accelerator"}.
      2. a substring of the device name.
    
    These arguments are *not* passed explicitly by the programmer, but via
    the selection of a SYCL target board. Sample board files are also
    included in this patch.
    
    gdb/testsuite/ChangeLog:
    2018-11-15  Tankut Baris Aktemur  <tankut.baris.aktemur@intel.com>
    
    	* boards/sycl-cpu-intel.exp: New file.
    	* boards/sycl-cpu.exp: New file.
    	* boards/sycl-host.exp: New file.
    	* boards/sycl-intelfpga.exp: New file.
    	* boards/sycl-intelgt.exp: New file.
    	* gdb.sycl/break-before-kernel.exp: New file.
    	* gdb.sycl/break.exp: New file.
    	* gdb.sycl/break2.exp: New file.
    	* gdb.sycl/run-to-termination.exp: New file.
    	* gdb.sycl/single-task.cpp: New file.
    	* lib/sycl-hello.cpp: New file.
    	* lib/sycl-util.cpp: New file.
    	* lib/sycl.exp: New file.
    
    Signed-off-by: Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>

---
+++ gdb/testsuite/lib/sycl-util.cpp
@@ -0,0 +1,79 @@
+/* This testcase is part of GDB, the GNU debugger.
+
+   Copyright 2019 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* Utility file for SYCL test programs to enable explicit selection of
+   a SYCL device.  Include this file in each SYCL test program.  */
+
+#include <CL/sycl.hpp>
+#include <iostream>
+#include <vector>
+
+static cl::sycl::queue
+get_sycl_queue (int argc, char *argv[])
+{
+  if (argc <= 2)
+    {
+      std::cout << "Usage: " << argv[0]
+		<< " <host|cpu|gpu|accelerator>"
+		<< " <device name substring>" << std::endl;
+      exit (1);
+    }
+
+  std::string type_arg {argv[1]};
+  std::string name_arg {argv[2]};
+
+  cl::sycl::info::device_type type;
+
+  if (type_arg.compare ("host") == 0)
+    type = cl::sycl::info::device_type::host;
+  else if (type_arg.compare ("cpu") == 0)
+    type = cl::sycl::info::device_type::cpu;
+  else if (type_arg.compare ("gpu") == 0)
+    type = cl::sycl::info::device_type::gpu;
+  else if (type_arg.compare ("accelerator") == 0)
+    type = cl::sycl::info::device_type::accelerator;
+  else
+    {
+      std::cout << "SYCL: Unrecognized device type '"
+		<< type_arg << "'" << std::endl;
+      exit (1);
+    }
+
+  std::vector<cl::sycl::device> devices
+    = cl::sycl::device::get_devices (type);
+
+  for (const cl::sycl::device &device : devices)
+    {
+      std::string dev_name
+	= device.get_info<cl::sycl::info::device::name> ();
+      std::string platform_name
+	= device.get_platform ().get_info<cl::sycl::info::platform::name> ();
+      std::string version
+	= device.get_info<cl::sycl::info::device::driver_version> ();
+
+      if (dev_name.find (name_arg) != std::string::npos)
+	{
+	  std::cout << "SYCL: Using device: [" << dev_name << "]"
+		    << " from [" << platform_name << "]"
+		    << " version [" << version << "]" << std::endl;
+	  return cl::sycl::queue {device}; /* return-sycl-queue */
+	}
+    }
+
+  std::cout << "SYCL: Could not select a device" << std::endl;
+  exit (1);
+}

55d217e112026e509fa799478a95eb54f6530888
Tim Wiederhake <tim.wiederhake@intel.com>
2019-01-03 09:00:11 +0100

    productization: Set Python Home.
    This patch makes GDB treat the environment variable "INTEL_PYTHONHOME" the
    same as "PYTHONHOME". This renaming allows us to avoid name clashes with
    pre-installed Python versions on the customer's computer.
    
    Previous versions of this patch also included an access check, silently
    ignoring INTEL_PYTHONHOME if the path was invalid. We prefer to fail loudly.
    
    gdb/ChangeLog:
    2019-01-03  Tim Wiederhake  <tim.wiederhake@intel.com>
    
    	* python/python.c: Set Python Home.
    
    Change-Id: I9c358219fec2096a24073f5574e6165d8d91ab4f
    Signed-off-by: Tim Wiederhake <tim.wiederhake@intel.com>

--- gdb/python/python.c
+++ gdb/python/python.c
@@ -1651,6 +1651,18 @@ do_start_initialization ()
   static wchar_t *progname_copy;
 #endif
 
+  char *intel_pythonhome = getenv ("INTEL_PYTHONHOME");
+  if (intel_pythonhome != NULL)
+    {
+#ifdef IS_PY3K
+      auto wintel_pythonhome = Py_DecodeLocale (intel_pythonhome, NULL);
+      if (wintel_pythonhome != NULL)
+	Py_SetPythonHome (wintel_pythonhome);
+#else
+      Py_SetPythonHome (intel_pythonhome);
+#endif
+    }
+
 #ifdef WITH_PYTHON_PATH
   /* Work around problem where python gets confused about where it is,
      and then can't find its libraries, etc.

05426383431d2f57561b825fdcc9b42ffc1bde43
Bakhtiari, Arash <arash.bakhtiari@intel.com>
2019-02-12 09:16:27 +0100

    Add command "info tsx_abort_reason"
    Inserted python script to decode abort reason from an expression.
    This includes the usage of registers and improves usability.
    A simple test case abusing of EAX was introduced and taking an
    application variable an abort reason was simulated. Idea is to cover
    all tests without having to work on real hardware.
    
    gdb/ChangeLog:
    2013-03-21  Walfred Tedeschi  <walfred.tedeschi@intel.com>
    
    	* python/lib/gdb/command/tsx.py: New File.
    	* data-directory/Makefile.in: Add tsx.py.
    
    doc/ChangeLog:
    2013-03-21  Walfred Tedeschi  <walfred.tedeschi@intel.com>
    
    	* gdb.texinfo: Add text on abort reasons of speculative execution.
    
    gdb/testsuite/ChangeLog:
    2013-03-21  Walfred Tedeschi  <walfred.tedeschi@intel.com>
    
    	* gdb.tsx/info_tsx_abort_reason.c: New file.
    	* gdb.tsx/info_tsx_abort_reason.exp: New file.

---
+++ gdb/python/lib/gdb/command/tsx.py
@@ -0,0 +1,67 @@
+# Copyright (C) 2013-2020 Free Software Foundation, Inc.
+#
+# This file is part of GDB.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Actual new command "info tsx-abort-reason"
+#
+
+import gdb
+
+class info_tsx_abort_reason (gdb.Command):
+    """Translates the abort reason encoded in an expression into human readable language."""
+
+    def __init__ (self):
+        gdb.Command.__init__ (self, "info tsx-abort-reason", gdb.COMMAND_STATUS)
+
+    def eval_parameter (self, parameter):
+        return gdb.parse_and_eval (parameter)
+
+    def print_reason (self, reason, val, nested, retry):
+        print("%s%s%s[0x%x]." % (reason, retry, nested, val))
+
+    def print_xaborted (self, xabort, val):
+        print("xabort %u, [0x%x]." % (xabort, val))
+
+    def decode_xabort (self, val):
+        return (val >> 16) & 0xFF
+
+    def decode (self, val):
+        reason = "unknown, "
+        retry = ""
+        nested = ""
+        if val & 1:
+            xabort = self.decode_xabort (val)
+            self.print_xaborted (xabort, val)
+            return
+        if val & 2:
+            retry = "retry possible, "
+        if val & 4:
+            reason = "conflict/race, "
+        if val & 8:
+            reason = "buffer overflow, "
+        if val & 16:
+            reason = "breakpoint hit, "
+        if val & 32:
+            nested = "on nested transactions, "
+        self.print_reason (reason, val, nested, retry)
+
+    def invoke (self, arg, from_tty):
+        if not arg:
+            raise gdb.GdbError ("\"info tsx-abort-reason\" command takes one argument.")
+        val = self.eval_parameter (arg)
+        self.decode (val)
+
+info_tsx_abort_reason ()

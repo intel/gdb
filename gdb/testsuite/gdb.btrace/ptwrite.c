c3404f08b6b89729c4e0533f6cebdf7cbd8a31c4
Felix Willgerodt <felix.willgerodt@intel.com>
2019-02-18 15:50:49 +0100

    [PTWRITE] btrace: Extend ptwrite event decoding.
    Call the ptwrite listener function whenever a ptwrite event is decoded.
    The returned string is written to the aux_data string table and a
    corresponding auxiliary instruction is appended to the function segment.
    
    gdb/ChangeLog:
    2019-02-18  Felix Willgerodt  <felix.willgerodt@intel.com>
    
    	* btrace.c (handle_pt_insn_events): Handle ptev_ptwrite.
    
    gdb/testsuite/ChangeLog:
    2019-02-18  Felix Willgerodt  <felix.willgerodt@intel.com>
    
    	* gdb.btrace/ptwrite.c: New file.
    	* gdb.btrace/ptwrite.exp: New file.
    	* gdb.btrace/x86_64-ptwrite.S: New file.
    	* lib/gdb.exp (skip_btrace_ptw_tests): New function.
    
    gdb/doc/ChangeLog:
    2019-02-18  Felix Willgerodt  <felix.willgerodt@intel.com>
    
    	* python.texi (gdb.ptwrite): New documentation.
    
    Signed-off-by: Felix Willgerodt <felix.willgerodt@intel.com>

---
+++ gdb/testsuite/gdb.btrace/ptwrite.c
@@ -0,0 +1,40 @@
+/* This testcase is part of GDB, the GNU debugger.
+
+ Copyright 2018 Free Software Foundation, Inc.
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <stdint.h>
+
+void
+ptwrite64 (uint64_t value)
+{
+  asm volatile ("PTWRITE %0;" : : "b" (value));
+}
+
+void
+ptwrite32 (uint32_t value)
+{
+  asm volatile ("PTWRITE %0;" : : "b" (value));
+}
+
+int
+main (void)
+{
+
+  ptwrite64 (0x42);
+  ptwrite32 (0x43);
+
+  return 0;
+}

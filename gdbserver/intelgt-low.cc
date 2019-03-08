5308ed6c095a9c529d31f86339b68d95b6b45c15
Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>
2019-03-08 18:19:35 +0100

    gdbserver: add configuration and an empty low target for intelgt
    Extend the configure script to check for linking against the Intel GT
    debug library (libigfxdbg).  A configure error is issued if the
    library is not found and the target is intelgt.  The location of the
    library can be configured using the "--with-libigfxdbg-prefix"
    argument.  The library is not linked even when available if the target
    is not intelgt.
    
    gdb/gdbserver/ChangeLog:
    2019-03-07  Mircea Gherzan  <mircea.gherzan@intel.com>
    	    Tankut Baris Aktemur  <tankut.baris.aktemur@intel.com>
    
    	* configure.srv (intelgt-*-elf): New target.
    	* configure.ac: Add check for linking with the Intel GT debug
    	library.  Implicitly add the '--with-libigfxdbg-prefix' argument.
    	* configure: Re-generate.
    	* intelgt-low.c: New file.
    	* Makefile.in (LIBIGFXDBG): New variable substituted by configure.
    
    Signed-off-by: Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>

---
+++ gdbserver/intelgt-low.cc
@@ -0,0 +1,30 @@
+/* Low-level interface for the Intel(R) Graphics Technology target,
+   for the remote server of GDB.
+   Copyright (C) 2019-2020 Free Software Foundation, Inc.
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include "server.h"
+#include "igfxdbg.h"
+#include "tdesc.h"
+
+int using_threads = 1;
+
+void
+initialize_low (void)
+{
+  set_target_ops (nullptr);
+}

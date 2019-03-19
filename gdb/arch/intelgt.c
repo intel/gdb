ad23d6a12dc4485aa118ec4e5e6ff84c2cc87c22
Markus Metzger <markus.t.metzger@intel.com>
2019-03-19 13:54:21 +0100

    gdb, intelgt: add intelgt definitions
    Provide Intel GT architecture-specific definitions that can be used by
    both the low target at the server side and tdep at the GDB side.  In
    particular, define a registry for convenient lookup operations
    regarding Intel GT registers.
    
    Co-authored-by: Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>
    Co-authored-by: Mihails Strasuns <mihails.strasuns@intel.com>
    Co-authored-by: Natalia Saiapova <natalia.saiapova@intel.com>

---
+++ gdb/arch/intelgt.c
@@ -0,0 +1,190 @@
+/* Copyright (C) 2019-2020 Free Software Foundation, Inc.
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include "gdbsupport/common-defs.h"
+#include "intelgt.h"
+#include <stdlib.h>
+
+namespace intelgt {
+
+/* Get the bit at POS in INST.  */
+
+bool
+get_inst_bit (const gdb_byte inst[], int pos)
+{
+  if (pos < 0 || (MAX_INST_LENGTH * 8) <= pos)
+    internal_error (_("bad bit offset: %d"), pos);
+
+  const int idx = pos >> 3;
+  const int off = pos & 7;
+  const int mask = 1 << off;
+  const gdb_byte byte = inst[idx];
+
+  return (byte & mask) != 0;
+}
+
+/* Set the bit at POS in INST.  */
+
+bool
+set_inst_bit (gdb_byte inst[], int pos)
+{
+  if (pos < 0 || (MAX_INST_LENGTH * 8) <= pos)
+    internal_error (_("bad bit offset: %d"), pos);
+
+  const int idx = pos >> 3;
+  const int off = pos & 7;
+  const int mask = 1 << off;
+  const gdb_byte byte = inst[idx];
+
+  const bool old = (byte & mask) != 0;
+  inst[idx] |= mask;
+
+  return old;
+}
+
+/* Clear the bit at POS in INST.  */
+
+bool
+clear_inst_bit (gdb_byte inst[], int pos)
+{
+  if (pos < 0 || (MAX_INST_LENGTH * 8) <= pos)
+    internal_error (_("bad bit offset: %d"), pos);
+
+  const int idx = pos >> 3;
+  const int off = pos & 7;
+  const int mask = 1 << off;
+  const gdb_byte byte = inst[idx];
+
+  const bool old = (byte & mask) != 0;
+  inst[idx] &= ~mask;
+
+  return old;
+}
+
+xe_version
+get_xe_version (unsigned int device_id)
+{
+  switch (device_id)
+    {
+      case 0x4F80:
+      case 0x4F81:
+      case 0x4F82:
+      case 0x4F83:
+      case 0x4F84:
+      case 0x4F85:
+      case 0x4F86:
+      case 0x4F87:
+      case 0x4F88:
+      case 0x5690:
+      case 0x5691:
+      case 0x5692:
+      case 0x5693:
+      case 0x5694:
+      case 0x5695:
+      case 0x5696:
+      case 0x5697:
+      case 0x5698:
+      case 0x56A0:
+      case 0x56A1:
+      case 0x56A2:
+      case 0x56A3:
+      case 0x56A4:
+      case 0x56A5:
+      case 0x56A6:
+      case 0x56A7:
+      case 0x56A8:
+      case 0x56A9:
+      case 0x56B0:
+      case 0x56B1:
+      case 0x56B2:
+      case 0x56B3:
+      case 0x56BA:
+      case 0x56BB:
+      case 0x56BC:
+      case 0x56BD:
+      case 0x56C0:
+      case 0x56C1:
+      case 0x56C2:
+      case 0x56CF:
+      case 0x7D40:
+      case 0x7D45:
+      case 0x7D67:
+      case 0x7D41:
+      case 0x7D55:
+      case 0x7DD5:
+      case 0x7D51:
+      case 0x7DD1:
+	return XE_HPG;
+
+      case 0x0201:
+      case 0x0202:
+      case 0x0203:
+      case 0x0204:
+      case 0x0205:
+      case 0x0206:
+      case 0x0207:
+      case 0x0208:
+      case 0x0209:
+      case 0x020A:
+      case 0x020B:
+      case 0x020C:
+      case 0x020D:
+      case 0x020E:
+      case 0x020F:
+      case 0x0210:
+	return XE_HP;
+
+      case 0x0BD0:
+      case 0x0BD4:
+      case 0x0BD5:
+      case 0x0BD6:
+      case 0x0BD7:
+      case 0x0BD8:
+      case 0x0BD9:
+      case 0x0BDA:
+      case 0x0BDB:
+      case 0x0B69:
+      case 0x0B6E:
+	return XE_HPC;
+
+      case 0x6420:
+      case 0x64A0:
+      case 0x64B0:
+
+      case 0xE202:
+      case 0xE20B:
+      case 0xE20C:
+      case 0xE20D:
+      case 0xE212:
+	return XE2;
+
+      case 0xB080:
+      case 0xB081:
+      case 0xB082:
+      case 0xB083:
+      case 0xB08F:
+      case 0xB090:
+      case 0xB0A0:
+      case 0xB0B0:
+	return XE3;
+
+      default:
+	return XE_INVALID;
+    }
+}
+
+} /* namespace intelgt */
---
+++ gdb/arch/intelgt.h
@@ -0,0 +1,215 @@
+/* Copyright (C) 2019-2020 Free Software Foundation, Inc.
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#ifndef ARCH_INTELGT_H
+#define ARCH_INTELGT_H
+
+#include "gdbsupport/tdesc.h"
+#include <string>
+#include <vector>
+
+namespace intelgt {
+
+/* Various arch constants.  */
+
+enum breakpoint_kind
+  {
+    BP_INSTRUCTION = 1,
+  };
+
+/* The length of a full and compact IntelGT instruction in bytes.  */
+
+constexpr int MAX_INST_LENGTH = 16;
+constexpr int COMPACT_INST_LENGTH = 8;
+
+/* Feature names.
+
+   They correspond to register sets defined in zet_intel_gpu_debug.h.  We
+   declare feature names in the order used in that header.
+
+   The SBA register set consists of a set of base registers in the order
+   defined in that header file.
+
+   Not all registers have DWARF numbers.  See DWARF_REGSETS below for a
+   list of features that do.  */
+constexpr const char* feature_grf = "org.gnu.gdb.intelgt.grf";
+constexpr const char* feature_addr = "org.gnu.gdb.intelgt.addr";
+constexpr const char* feature_flag = "org.gnu.gdb.intelgt.flag";
+constexpr const char *feature_ce = "org.gnu.gdb.intelgt.ce";
+constexpr const char *feature_sr = "org.gnu.gdb.intelgt.sr";
+constexpr const char *feature_cr = "org.gnu.gdb.intelgt.cr";
+constexpr const char *feature_tdr = "org.gnu.gdb.intelgt.tdr";
+constexpr const char* feature_acc = "org.gnu.gdb.intelgt.acc";
+constexpr const char* feature_mme = "org.gnu.gdb.intelgt.mme";
+constexpr const char *feature_sp = "org.gnu.gdb.intelgt.sp";
+constexpr const char* feature_sba = "org.gnu.gdb.intelgt.sba";
+constexpr const char *feature_dbg = "org.gnu.gdb.intelgt.dbg";
+constexpr const char *feature_fc = "org.gnu.gdb.intelgt.fc";
+constexpr const char *feature_mf = "org.gnu.gdb.intelgt.mf";
+constexpr const char *feature_debugger = "org.gnu.gdb.intelgt.debugger";
+constexpr const char *feature_scratch = "org.gnu.gdb.intelgt.scratch";
+constexpr const char *feature_scalar = "org.gnu.gdb.intelgt.scalar";
+
+/* Register sets/groups needed for DWARF mapping.  Used for
+   declaring static arrays for various mapping tables.  */
+
+enum dwarf_regsets : int
+{
+  regset_sba = 0,
+  regset_grf,
+  regset_addr,
+  regset_flag,
+  regset_acc,
+  regset_mme,
+  regset_count
+};
+
+/* Map of dwarf_regset values to the target description
+   feature names.  */
+
+constexpr const char *dwarf_regset_features[regset_count] = {
+  feature_sba,
+  feature_grf,
+  feature_addr,
+  feature_flag,
+  feature_acc,
+  feature_mme
+};
+
+/* Instruction details.  */
+
+enum
+{
+  /* The opcode mask for bits 6:0.  */
+  opc_mask = 0x7f,
+
+  /* Branch instruction opcodes.  */
+  opc_branchd = 0x21,
+  opc_branchc = 0x23,
+  opc_goto = 0x2e,
+
+  /* Send instruction opcodes.  */
+  opc_send = 0x31,
+  opc_sendc = 0x32,
+
+  /* DPAS instruction opcodes.  */
+  opc_dpas = 0x59,
+  opc_dpasw = 0x5a,
+};
+
+/* Selected instruction control bit positions.  */
+
+enum
+{
+  /* AtomicCtrl bit for non-compact instructions.  */
+  ctrl_atomic = 32,
+
+  /* FwdCtrl bit for non-compact instructions.  Only used for atomic
+     instructions.  */
+  ctrl_fwd = 33,
+
+  /* The End Of Thread control.  Only used for SEND and SENDC.  */
+  ctrl_eot_send = 34,
+};
+
+/* Get the bit at POS in INST.  */
+
+bool get_inst_bit (const gdb_byte inst[], int pos);
+
+/* Set the bit at POS in INST.  */
+
+bool set_inst_bit (gdb_byte inst[], int pos);
+
+/* Clear the bit at POS in INST.  */
+
+bool clear_inst_bit (gdb_byte inst[], int pos);
+
+static inline bool
+is_compacted_inst (const gdb_byte inst[])
+{
+  /* Check the CmptCtrl flag (bit 29).  */
+  return inst[3] & 0x20;
+}
+
+static inline int
+breakpoint_bit_offset (const gdb_byte inst[])
+{
+  return (is_compacted_inst (inst) ? 7 : 30);
+}
+
+static inline bool
+set_breakpoint (gdb_byte inst[])
+{
+  return set_inst_bit (inst, breakpoint_bit_offset (inst));
+}
+
+static inline bool
+clear_breakpoint (gdb_byte inst[])
+{
+  return clear_inst_bit (inst, breakpoint_bit_offset (inst));
+}
+
+static inline bool
+has_breakpoint (const gdb_byte inst[])
+{
+  return get_inst_bit (inst, breakpoint_bit_offset (inst));
+}
+
+static inline unsigned int
+inst_length_compacted ()
+{
+  return COMPACT_INST_LENGTH;
+}
+
+static inline unsigned int
+inst_length_full ()
+{
+  return MAX_INST_LENGTH;
+}
+
+static inline unsigned int
+inst_length (const gdb_byte inst[])
+{
+  return (is_compacted_inst (inst)
+	  ? inst_length_compacted ()
+	  : inst_length_full ());
+}
+
+/* The encoding for XE version enumerates follows this pattern, which is
+   aligned with the IGA encoding.  */
+
+#define XE_VERSION(MAJ, MIN) (((MAJ) << 24) | (MIN))
+
+/* Supported GDB GEN platforms.  */
+
+enum xe_version
+{
+  XE_INVALID = 0,
+  XE_HP = XE_VERSION (1, 1),
+  XE_HPG = XE_VERSION (1, 2),
+  XE_HPC = XE_VERSION (1, 4),
+  XE2 = XE_VERSION (2, 0),
+  XE3 = XE_VERSION (3, 0),
+};
+
+/* Helper function to translate the device id to a device version.  */
+
+xe_version get_xe_version (unsigned int device_id);
+
+} /* namespace intelgt */
+
+#endif

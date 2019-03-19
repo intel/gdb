2040e87e2c7ce054ff67a7cfb2b2b02d3cc1de94
Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>
2019-03-19 13:54:21 +0100

    gdb/arch: add intelgt definitions
    Provide Intel GT architecture-specific definitions that can be used by
    both the low target at the server side and tdep at the GDB side.  In
    particular, define a registry for convenient lookup operations
    regarding Intel GT registers.
    
    gdb/ChangeLog:
    2019-03-07  Tankut Baris Aktemur  <tankut.baris.aktemur@intel.com>
    
    	* arch/intelgt.c: New file.
    	* arch/intelgt.h: New file.
    
    Signed-off-by: Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>

---
+++ gdb/arch/intelgt.h
@@ -0,0 +1,145 @@
+/* Copyright (C) 2019 Free Software Foundation, Inc.
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#ifndef ARCH_INTELGT_H
+#define ARCH_INTELGT_H
+
+#include "gdbsupport/tdesc.h"
+#include <map>
+#include <string>
+#include <vector>
+
+namespace intelgt {
+
+/* Supported Intel(R) Graphics Technology versions.  */
+
+enum class version : unsigned char
+  {
+    Gen9 = 9,
+  };
+
+/* Register information.  */
+
+enum class reg_group : unsigned short
+  {
+    Address,
+    Accumulator,
+    Flag,
+    ChannelEnable,
+    StackPointer,
+    State,
+    Control,
+    NotificationCount,
+    InstructionPointer,
+    ThreadDependency,
+    Timestamp,
+    FlowControl,
+    Grf,
+    ExecMaskPseudo,
+  };
+
+struct gt_register
+{
+  /* The name of the register.  */
+  std::string name;
+
+  /* The group that the register belongs to.  */
+  reg_group group;
+
+  /* The index of the register within its group.  */
+  unsigned short local_index;
+
+  /* The size of the register in terms of bytes.  */
+  unsigned short size_in_bytes;
+
+  explicit gt_register (std::string name, reg_group group,
+			unsigned short local_index,
+			unsigned short size_in_bytes);
+};
+
+enum breakpoint_kind
+  {
+    BP_INSTRUCTION = 1,
+  };
+
+/* Architectural information for an Intel(R) Graphics Technology
+   version.  One instance per Gen version is created.  Instances can
+   be accessed through the factory method 'get_or_create'.  */
+
+class arch_info
+{
+public:
+  arch_info (unsigned int num_grfs);
+
+  /* Return the total number of registers.  */
+  unsigned int num_registers ();
+
+  /* The number of GRF registers.  */
+  unsigned int grf_reg_count () const;
+
+  /* Return the register at INDEX.  */
+  const gt_register &get_register (int index);
+
+  /* Return the name of the register at INDEX.  */
+  const char *get_register_name (int index);
+
+  /* The length of an instruction in bytes.  */
+  virtual unsigned int inst_length_compacted () = 0;
+
+  virtual unsigned int inst_length_full () = 0;
+
+  /* The maximum size of a register.  */
+  virtual unsigned int max_reg_size () = 0;
+
+  /* Return true if the given INST is compacted; false otherwise.  */
+  virtual bool is_compacted_inst (const gdb_byte inst[]) = 0;
+
+  /* The breakpoint instruction used for this arch.  */
+  virtual const gdb_byte *breakpoint_inst () = 0;
+
+  /* The length of the breakpoint instruction.  */
+  virtual unsigned int breakpoint_inst_length () = 0;
+
+  /* The index of the PC register.  */
+  virtual int pc_regnum () const = 0;
+
+  /* The index of the SP register.  */
+  virtual int sp_regnum () const = 0;
+
+  /* The index of the 'emask' register.  */
+  virtual int emask_regnum () const = 0;
+
+  /* Factory method to ensure one instance per version.  */
+  static arch_info *get_or_create (version vers);
+
+protected:
+
+  /* The collection of registers (GRF + ARF).  */
+  std::vector<gt_register> regs;
+
+private:
+
+  /* The arch info instances created per version.  */
+  static std::map<version, arch_info *> infos;
+
+  /* Number of registers of various categories.  */
+  const unsigned int num_grfs;
+};
+
+} /* namespace intelgt */
+
+#endif

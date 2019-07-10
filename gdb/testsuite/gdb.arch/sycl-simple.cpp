fa466b923b2e45bd497839c65eb0327e797de62c
Albertano Caruso <albertano.caruso@intel.com>
2019-07-10 11:24:30 +0200

    gdbserver/intelgt: add disassemble feature for the Intel GT architecture.
    gdb/ChangeLog:
    2019-06-19  Kai Trojahner  <kai.trojahner@intel.com>
    	    Albertano Caruso  <albertano.caruso@intel.com>
    
    	* Makefile.in: Add LIBIGA64 support.
    	* configure.in: Re-generate.
    	* configure: Re-generate.
    	* configure.ac: Add LIBIGA64 support.
    	* intelgt-tdep.c: Add disassemble feature.
    
    gdb/testsuite/ChangeLog:
    2019-06-19  Albertano Caruso  <albertano.caruso@intel.com>
    
    	* gdb.arch/intelgt-disassemble.exp: New file.
    	* gdb.arch/sycl-simple.exp: New file.
    
    Signed-off-by: Albertano Caruso <albertano.caruso@intel.com>

---
+++ gdb/testsuite/gdb.arch/sycl-simple.cpp
@@ -0,0 +1,43 @@
+/* This testcase is part of GDB, the GNU debugger.
+
+   Copyright 2019 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <CL/sycl.hpp>
+#include "../lib/sycl-util.cpp"
+
+int
+main (int argc, char *argv[])
+{
+  int data = 42;
+
+  { /* Extra scope enforces waiting on the kernel.  */
+    cl::sycl::queue deviceQueue {get_sycl_queue (argc, argv)};
+    cl::sycl::buffer<int, 1> buf {&data, cl::sycl::range<1> {1}};
+
+    deviceQueue.submit ([&] (cl::sycl::handler& cgh)
+    {
+      auto accessor
+	= buf.get_access<cl::sycl::access::mode::write> (cgh);
+
+      cgh.single_task<class simple_kernel> ([=] ()
+      {
+	accessor[0] = 99; /* inside-kernel */
+      });
+    });
+  }
+
+  return 0;
+}

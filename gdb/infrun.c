12f115791b5cf54718ec37cd8baf928002dcfa5b
Mihails Strasuns <mihails.strasuns@intel.com>
2019-09-04 10:50:12 +0200

    gdb, infrun: assertion for a possibly uninitialized variable
    It is expected that the loop will always be able to find an inferior,
    expressing that via an assertion will fix -Wmaybe-uninitialized warning
    from a compiler.
    
    gdb/ChangeLog:
    2019-09-04  Mihails Strasuns  <mihails.strasuns@intel.com>
    
    	* infrun.c (do_target_wait): assertion for a possibly
    	uninitialized variable.
    
    Signed-off-by: Mihails Strasuns <mihails.strasuns@intel.com>

--- gdb/infrun.c
+++ gdb/infrun.c
@@ -4154,6 +4154,8 @@ do_target_wait (ptid_t wait_ptid, execution_control_state *ecs,
 	  break;
 	}
 
+  gdb_assert (selected);
+
   /* Now poll for events out of each of the matching inferior's
      targets, starting from the selected one.  */
 

30e54d86f8d81d426caf30592194504af7357d0d
Mihails Strasuns <mihails.strasuns@intel.com>
2019-09-16 12:50:52 +0200

    testsuite, sycl: add tests for SYCL xmethods
    Actual xmethod implementation is expected to be provided by a SYCL
    compiler runtime.
    
    gdb/testsuite/ChangeLog:
    2019-09-16  Mihails Strasuns  <mihails.strasuns@intel.com>
    
    	* gdb.sycl/accessor1d.cpp: New file.
    	* gdb.sycl/accessor1d.exp: New file.
    	* gdb.sycl/accessor2d.cpp: New file.
    	* gdb.sycl/accessor2d.exp: New file.
    	* gdb.sycl/accessor3d.cpp: New file.
    	* gdb.sycl/accessor3d.exp: New file.
    
    Signed-off-by: Mihails Strasuns <mihails.strasuns@intel.com>

---
+++ gdb/testsuite/gdb.sycl/accessor2d.cpp
@@ -0,0 +1,44 @@
+/* This testcase is part of GDB, the GNU debugger.
+
+   Copyright 2019 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include "../lib/sycl-util.cpp"
+#include <CL/sycl.hpp>
+
+int
+main (int argc, char *argv[])
+{
+  float data[2][2] = { { 1, 2 }, { 3, 4 } };
+
+  {
+    cl::sycl::queue queue = { get_sycl_queue (argc, argv) };
+    cl::sycl::buffer<float, 2> buffer
+	= { &data[0][0], cl::sycl::range<2> { 2, 2 } };
+    queue.submit ([&] (cl::sycl::handler &cgh)
+      {
+	auto input
+	  = buffer.get_access<cl::sycl::access::mode::read_write> (cgh);
+
+	cgh.single_task<class simple_kernel_2> ([=] ()
+	  {
+	    auto id = cl::sycl::id<2> (1, 1);
+	    float value = input[id];
+	    float dummy = value; /* kernel-line */
+	  });
+      });
+  }
+  return 0;
+}

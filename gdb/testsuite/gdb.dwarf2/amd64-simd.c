e0e772c3a4c4581880d6cc5074a9532753922ac2
Markus Metzger <markus.t.metzger@intel.com>
2020-01-28 14:05:55 +0100

    gdb, testsuite, simd: test Intel SIMD extensions
    Add tests for Intel SIMD extensions using GDB's DWARF assembler to
    describe vectorized code on IA.
    
    The test depends on the TSA variable being described as an array, not as a
    pointer.  In the latter case, we would not be able to describe the partial
    registerization of the pointed-to array object.
    
    I hope that we can make this work with unbounded arrays or with arrays
    declared as pointers in the future.
    
    gdb/testsuite/ChangeLog:
    2020-08-05  Markus Metzger  <markus.t.metzger@intel.com>
    
    	* gdb.dwarf2/amd64-simd-sse.S: New test.
    	* gdb.dwarf2/amd64-simd-sse.c: New test.
    	* gdb.dwarf2/amd64-simd.c: New test.
    	* gdb.dwarf2/amd64-simd.exp: New file.
    	* gdb.dwarf2/amd64-simd.h: New test.

---
+++ gdb/testsuite/gdb.dwarf2/amd64-simd.c
@@ -0,0 +1,44 @@
+/* Copyright (C) 2020 Free Software Foundation, Inc.
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* Test program for DW_AT_INTEL_simd_width and
+   DW_OP_INTEL_push_simd_lane.  */
+
+#include "amd64-simd.h"
+
+
+#define N 1024
+
+struct ts tsa[N];
+
+int
+main (void)
+{
+  int i, result;
+
+  for (i = 0; i < N; ++i)
+    {
+      tsa[i].a = 100 + i;
+      tsa[i].b = 7;
+    }
+
+  result = test (tsa, 5);
+  if (result < 0)
+    result = -1;
+
+  return result;
+}

3a08bc6bce5b98ad3fbfc50ecb2e31178c2deee0
Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>
2020-03-06 14:05:43 +0100

    gdb, gdbserver: add Intel GT Gen12 support
    gdb/ChangeLog:
    2020-03-06  Tankut Baris Aktemur  <tankut.baris.aktemur@intel.com>
    
    	* arch/intelgt.h (enum class version): Add Gen12 value.
    	* arch/intelgt.c (arch_info_gen12): New declaration aliased
    	to arch_info_gen11.
    	(arch_info::get_or_create): Handle Gen12 case.
    	* features/intelgt-arf12.c: New file.
    	* features/intelgt-arf12.xml: New file.
    	* features/Makefile (FEATURE_XMLFILES): Add
    	features/intelgt-arf12.xml.
    	* intelgt-tdep.c (GT_FEATURE_ARF12): New #define.
    	(intelgt_version_from_tdesc): Add check for ARF12 feature.
    	(intelgt_gdbarch_init): Ditto.
    	(intelgt_initialize_gdbarch_data): Handle Gen12 case.
    
    gdb/gdbserver/ChangeLog:
    2020-03-06  Tankut Baris Aktemur  <tankut.baris.aktemur@intel.com>
    
    	* configure.srv (intelgt-*-elf) <srv_xmlfiles>: Include
    	intelgt-arf12.xml.
    	* intelgt-low.c: Add "features/intelgt-arf12.c" to includes.
    	(intelgt_process_target::attach): Handle Gen12 case.
    	(intelgt_process_target::create_target_description): Ditto.
    
    Signed-off-by: Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>

---
+++ gdb/features/intelgt-arf12.c
@@ -0,0 +1,35 @@
+/* THIS FILE IS GENERATED.  -*- buffer-read-only: t -*- vi:set ro:
+  Original: intelgt-arf12.xml */
+
+#include "gdbsupport/tdesc.h"
+
+static int
+create_feature_intelgt_arf12 (struct target_desc *result, long regnum)
+{
+  struct tdesc_feature *feature;
+
+  feature = tdesc_create_feature (result, "org.gnu.gdb.intelgt.arf12");
+  tdesc_create_reg (feature, "a0", regnum++, 1, NULL, 256, "uint256");
+  tdesc_create_reg (feature, "acc0", regnum++, 1, NULL, 256, "uint256");
+  tdesc_create_reg (feature, "acc1", regnum++, 1, NULL, 256, "uint256");
+  tdesc_create_reg (feature, "acc2", regnum++, 1, NULL, 256, "uint256");
+  tdesc_create_reg (feature, "acc3", regnum++, 1, NULL, 256, "uint256");
+  tdesc_create_reg (feature, "acc4", regnum++, 1, NULL, 256, "uint256");
+  tdesc_create_reg (feature, "acc5", regnum++, 1, NULL, 256, "uint256");
+  tdesc_create_reg (feature, "acc6", regnum++, 1, NULL, 256, "uint256");
+  tdesc_create_reg (feature, "acc7", regnum++, 1, NULL, 256, "uint256");
+  tdesc_create_reg (feature, "acc8", regnum++, 1, NULL, 256, "uint256");
+  tdesc_create_reg (feature, "acc9", regnum++, 1, NULL, 256, "uint256");
+  tdesc_create_reg (feature, "f0", regnum++, 1, NULL, 32, "uint32");
+  tdesc_create_reg (feature, "f1", regnum++, 1, NULL, 32, "uint32");
+  tdesc_create_reg (feature, "ce", regnum++, 1, NULL, 32, "uint32");
+  tdesc_create_reg (feature, "sp", regnum++, 1, NULL, 128, "uint128");
+  tdesc_create_reg (feature, "sr0", regnum++, 1, NULL, 128, "uint128");
+  tdesc_create_reg (feature, "cr0", regnum++, 1, NULL, 128, "uint128");
+  tdesc_create_reg (feature, "ip", regnum++, 1, NULL, 32, "uint32");
+  tdesc_create_reg (feature, "tdr", regnum++, 1, NULL, 128, "uint128");
+  tdesc_create_reg (feature, "tm0", regnum++, 1, NULL, 128, "uint128");
+  tdesc_create_reg (feature, "emask", regnum++, 1, NULL, 32, "uint32");
+  tdesc_create_reg (feature, "iemask", regnum++, 1, NULL, 32, "uint32");
+  return regnum;
+}

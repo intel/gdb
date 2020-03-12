a0e8da85f3282d6d9bc401ac7bcea1134f369107
Natalia Saiapova <natalia.saiapova@intel.com>
2020-03-12 13:18:46 +0100

    gdb: look for location for until command only in the current program space.
    This patch is fixing the following problem.
    
    Consider a gdb-session with 2 inferiors with the same executable.  Then,
    'until' command returns an error:
    
    Reading symbols from /local/build/gdb/testsuite/outputs/gdb.base/break/break...
    (gdb) add-inferior
    [New inferior 2]
    Added inferior 2
    (gdb) inferior 2
    [Switching to inferior 2 [<null>] (<noexec>)]
    (gdb) file /local/build/gdb/testsuite/outputs/gdb.base/break/break
    Reading symbols from /local/build/gdb/testsuite/outputs/gdb.base/break/break...
    (gdb) b main
    Breakpoint 1 at 0x11ad: main. (2 locations)
    (gdb) r
    Starting program: /local/build/gdb/testsuite/outputs/gdb.base/break/break
    
    Breakpoint 1, main (argc=1, argv=0x7fffffffe278, envp=0x7fffffffe288) at /local/gdb/gdb/testsuite/gdb.base/break.c:43
    43          if (argc == 12345) {  /* an unlikely value < 2^16, in case uninited */ /* set breakpoint 6 here */
    (gdb) until 49
    Couldn't get information on specified line.
    
    gdb/ChangeLog:
    2020-03-12  Natalia Saiapova  <natalia.saiapova@intel.com>
    
    	* breakpoint.c (until_break_command): Update.
    	* linespec.c (decode_line_with_last_displayed): New argument.
    	(decode_line_with_last_displayed_allow_empty): New argument.
    	* linespec.h: (decode_line_with_last_displayed): New argument.
    	(decode_line_with_last_displayed_allow_empty): New argument.
    
    gdb/testsuite/ChangeLog:
    2020-03-12  Natalia Saiapova  <natalia.saiapova@intel.com>
    
    	* gdb.multi/multi-until.c: New test.
    	* gdb.multi/multi-until.exp: New file.
    
    Signed-off-by: Natalia Saiapova <natalia.saiapova@intel.com>

--- gdb/linespec.c
+++ gdb/linespec.c
@@ -3242,27 +3242,30 @@ decode_line_with_current_source (const char *string, int flags)
 /* See linespec.h.  */
 
 std::vector<symtab_and_line>
-decode_line_with_last_displayed (const char *string, int flags)
+decode_line_with_last_displayed (const char *string, int flags,
+				 program_space *search_pspace)
 {
   if (string == nullptr)
     error (_("Empty line specification."));
 
-  return decode_line_with_last_displayed_allow_empty (string, flags);
+  return decode_line_with_last_displayed_allow_empty (string, flags,
+						      search_pspace);
 }
 
 /* See linespec.h.  */
 
 std::vector<symtab_and_line>
-decode_line_with_last_displayed_allow_empty (const char *string, int flags)
+decode_line_with_last_displayed_allow_empty (const char *string, int flags,
+					     program_space *search_pspace)
 {
   event_location_up location = string_to_event_location (&string,
 							 current_language);
   std::vector<symtab_and_line> sals
     = (last_displayed_sal_is_valid ()
-       ? decode_line_1 (location.get (), flags, NULL,
+       ? decode_line_1 (location.get (), flags, search_pspace,
 			get_last_displayed_symtab (),
 			get_last_displayed_line ())
-       : decode_line_1 (location.get (), flags, NULL, NULL, 0));
+       : decode_line_1 (location.get (), flags, search_pspace, NULL, 0));
 
   if (*string)
     error (_("Junk at end of line specification: %s"), string);

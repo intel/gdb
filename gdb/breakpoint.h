4c11ba762e9f1f542d9d2007fcd34410038e7d2b
Natalia Saiapova <natalia.saiapova@intel.com>
2020-03-19 15:37:03 +0100

    gdb, simd: add SIMD lane support to 'commands' command.
    By default, the breakpoint actions are applied once per thread hit of a
    breakpoint.  Before executing the action of a breakpoint, GDB switches
    its focus to the SIMD lane which would be chosen if this breakpoint was reported
    to a user.  If for all actions after completing the action, the SIMD lane does
    not change, GDB restores the original SIMD lane which was reported to the user.
    
    We add /a modifier which forces GDB to execute the breakpoint actions
    for all lanes which match the condition of the specified breakpoint(s).  Note,
    that if there are active lanes which do not match the breakpoint condition,
    actions are not executed for these lanes.
    
    In the following example, 2 breakpoints are defined at the same location and for
    the same thread.  Breakpoint 2 does not have a thread clause, breakpoint 3 is
    only for SIMD lane 4 in thread 2.  Thread 2 hits both breakpoints, breakpoint 3
    hit is reported to the user, its actions are executed only for the lane 4.
    Breakpoint 2 actions are executed for all active lanes in thread 2: 0, 2, 4, 6.
    After actions are completed, GDB returns its focus to the SIMD lane 4 of the
    thread 2:
    
    (gdb) break 8 thread 2
    Breakpoint 2 at 0xfffec920: file sample.cpp, line 8
    (gdb) break 8 thread 2:4
    Note: breakpoint 2 (thread 4:2) also set at pc 0xfffec920.
    Breakpoint 3 at 0xfffec920: file sample.cpp, line 8
    (gdb) commands /a 2-3
    Type commands for breakpoint(s) 2-3, one per line.
    End with a line saying just "end".
    >thread
    >end
    (gdb) continue
    Thread 2.2:4 hit Breakpoint 3, foo () at sample.cpp:8
        8	     a += 0;
    [Current thread is 2.2:4 (Thread 1073741824 lane 4)]
    [Current thread is 2.2:0 (Thread 1073741824 lane 0)]
    [Current thread is 2.2:2 (Thread 1073741824 lane 2)]
    [Current thread is 2.2:4 (Thread 1073741824 lane 4)]
    [Current thread is 2.2:6 (Thread 1073741824 lane 6)]
    
    (gdb) thread
    [Current thread is 2.2:4 (Thread 1073741824 lane 4)]

--- gdb/breakpoint.h
+++ gdb/breakpoint.h
@@ -819,6 +819,7 @@ struct breakpoint : public intrusive_list_node<breakpoint>
   /* Chain of command lines to execute when this breakpoint is
      hit.  */
   counted_command_line commands;
+  bool is_cmd_for_all_lanes = false;
   /* Stack depth (address of frame).  If nonzero, break only if fp
      equals this.  */
   struct frame_id frame_id = null_frame_id;
@@ -1406,6 +1407,7 @@ struct bpstat
 
     /* The associated command list.  */
     counted_command_line commands;
+    bool is_cmd_for_all_lanes;
 
     /* Old value associated with a watchpoint.  */
     value_ref_ptr old_val;

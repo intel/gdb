045ab0e98530dba84f8b488b79000f055188cecb
Abdul Basit Ijaz <abdul.b.ijaz@intel.com>
2020-04-14 13:38:25 +0200

    testsuite, openmp: add tests for offloading
    Add OpenMP tests for offloading to a target device.
    
    gdb/testsuite/ChangeLog:
    2020-04-20  Abdul Basit Ijaz  <abdul.b.ijaz@intel.com>
    	    Aleksandar Paunovic  <aleksandar.paunovic@intel.com>
    
    	* boards/openmp-cpu.exp: New file.
    	* boards/openmp-gpu.exp: New file.
    	* boards/openmp-host.exp: New file.
    	* lib/openmp.exp: New file.
    	* lib/openmp-devices.cpp: New file.
    	* lib/openmp-devices.f90: New file.
    	* gdb.openmp/break-before-kernel-workaround.exp: New file.
    	* gdb.openmp/break-before-kernel.exp: New file.
    	* gdb.openmp/break-inside-kernel.exp: New file.
    	* gdb.openmp/break.exp: New file.
    	* gdb.openmp/break2.exp: New file.
    	* gdb.openmp/cond-bp-on-local.exp: New file.
    	* gdb.openmp/delete-bp.exp: New file.
    	* gdb.openmp/disable-bp.exp: New file.
    	* gdb.openmp/parallel-for-1D.cpp: New file.
    	* gdb.openmp/parallel-for-1D.f90: New file.
    	* gdb.openmp/temporary-bp.exp: New file.
    	* gdb.openmp/infcall-parallel-for.exp: New file.
    	* gdb.openmp/step-parallel-for.exp: New file.
    	* gdb.openmp/local-vars.cpp: New file.
    	* gdb.openmp/local-vars.f90: New file.
    	* gdb.openmp/single-task.cpp: New file.
    	* gdb.openmp/single-task.f90: New file.
    	* gdb.openmp/call-stack.cpp: New file.
    	* gdb.openmp/call-stack.exp: New file.
    	* gdb.openmp/call-stack.f90: New file.
    	* gdb.openmp/parallel-for-2D.exp: New file.
    	* gdb.openmp/parallel-for-2D.cpp: New file.
    	* gdb.openmp/parallel-for-2D.f90: New file.
    	* gdb.openmp/read.exp: New file.
    	* gdb.openmp/step-into-function.exp: New file.
    	* gdb.openmp/step.exp: New file.
    	* gdb.openmp/write-local.exp: New file.
    	* gdb.openmp/multi-kernel-async.cpp: New file.
    	* gdb.openmp/multi-kernel-async.exp: New file.
    	* gdb.openmp/multi-kernel-async.f90: New file.
    	* gdb.openmp/multi-kernel-unnamed.cpp: New file.
    	* gdb.openmp/multi-kernel-unnamed.exp: New file.
    	* gdb.openmp/multi-kernel-unnamed.f90: New file.
    	* gdb.openmp/multi-kernel.cpp: New file.
    	* gdb.openmp/multi-kernel.exp: New file.
    	* gdb.openmp/multi-kernel.f90: New file.
    	* gdb.openmp/info-locals-and-args.exp: New file.
    	* gdb.openmp/intelgt-auto-attach.exp: New file.
    	* gdb.openmp/intelgt-run-inf2.exp: New file.
    	* gdb.openmp/run-to-termination.exp: New file.
    
    Co-authored-by: Abdul Basit Ijaz <abdul.b.ijaz@intel.com>
    Co-authored-by: Aleksandar Paunovic <aleksandar.paunovic@intel.com>

---
+++ gdb/testsuite/gdb.openmp/multi-kernel-async.cpp
@@ -0,0 +1,41 @@
+/* This testcase is part of GDB, the GNU debugger.
+
+   Copyright 2019-2020 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <iostream>
+#include "omp.h"
+
+int
+main (int argc, char *argv[])
+{
+  int data1 = 11;
+  int data2 = 22;
+
+#pragma omp target teams num_teams(1) thread_limit(1) map(to: data1) nowait
+  {
+    int item = data1 + 100; /* kernel-1-line */
+  }
+
+#pragma omp target teams num_teams(1) thread_limit(1) map(to: data2) nowait
+  {
+    int item = data2 + 200; /* kernel-2-line */
+  }
+
+  int total = data1 + data2; /* post-kernel-line */
+
+#pragma omp taskwait
+  return 0;
+}
---
+++ gdb/testsuite/lib/openmp-devices.f90
@@ -0,0 +1,44 @@
+! Copyright 2020 Free Software Foundation, Inc.
+!
+! This program is free software; you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation; either version 3 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <http://www.gnu.org/licenses/> .
+
+
+program devices
+  use omp_lib, ONLY : omp_get_default_device, omp_get_num_devices
+  external tgt_get_device_name, tgt_get_rtl_name
+
+  integer :: dd, nd, sum, i, ret_val
+  integer, parameter :: N = 100
+
+  nd = omp_get_num_devices()
+  if(nd < 1) then
+    call exit (1)
+  end if
+
+  print*, "OpenMP: number of devices is ", nd
+
+  dd = omp_get_default_device()
+  if (dd < 0) then
+    print*, "omp_get_default_device() call failed with an error code: ", nd
+    call exit (1)
+  end if
+
+  sum = 0
+  !$omp target teams distribute parallel do reduction (+:sum)
+    do i = 1, N
+      sum = sum + i
+    end do
+  !$omp end target teams distribute parallel do
+
+end program devices

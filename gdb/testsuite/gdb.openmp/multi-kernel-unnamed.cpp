4a87b6ac631868f25030ab8d9dfb7e72447a23d3
Abdul Basit Ijaz <abdul.b.ijaz@intel.com>
2020-04-14 13:38:25 +0200

    testsuite, openmp: add tests for offloading
    Add OpenMP tests for offloading to a target device.
    
    gdb/testsuite/ChangeLog:
    2020-04-20  Abdul Basit Ijaz  <abdul.b.ijaz@intel.com>
    	    Aleksandar Paunovic  <aleksandar.paunovic@intel.com>
    
    	* boards/openmp-cpu.exp: New file.
    	* boards/openmp-gpu.exp: New file.
    	* boards/openmp-host.exp: New file.
    	* lib/openmp.exp: New file.
    	* lib/openmp-devices.cpp: New file.
    	* lib/openmp-devices.f90: New file.
    	* gdb.openmp/break-before-kernel-workaround.exp: New file.
    	* gdb.openmp/break-before-kernel.exp: New file.
    	* gdb.openmp/break-inside-kernel.exp: New file.
    	* gdb.openmp/break.exp: New file.
    	* gdb.openmp/break2.exp: New file.
    	* gdb.openmp/cond-bp-on-local.exp: New file.
    	* gdb.openmp/delete-bp.exp: New file.
    	* gdb.openmp/disable-bp.exp: New file.
    	* gdb.openmp/parallel-for-1D.cpp: New file.
    	* gdb.openmp/parallel-for-1D.f90: New file.
    	* gdb.openmp/temporary-bp.exp: New file.
    	* gdb.openmp/infcall-parallel-for.exp: New file.
    	* gdb.openmp/step-parallel-for.exp: New file.
    	* gdb.openmp/local-vars.cpp: New file.
    	* gdb.openmp/local-vars.f90: New file.
    	* gdb.openmp/single-task.cpp: New file.
    	* gdb.openmp/single-task.f90: New file.
    	* gdb.openmp/call-stack.cpp: New file.
    	* gdb.openmp/call-stack.exp: New file.
    	* gdb.openmp/call-stack.f90: New file.
    	* gdb.openmp/parallel-for-2D.exp: New file.
    	* gdb.openmp/parallel-for-2D.cpp: New file.
    	* gdb.openmp/parallel-for-2D.f90: New file.
    	* gdb.openmp/read.exp: New file.
    	* gdb.openmp/step-into-function.exp: New file.
    	* gdb.openmp/step.exp: New file.
    	* gdb.openmp/write-local.exp: New file.
    	* gdb.openmp/multi-kernel-async.cpp: New file.
    	* gdb.openmp/multi-kernel-async.exp: New file.
    	* gdb.openmp/multi-kernel-async.f90: New file.
    	* gdb.openmp/multi-kernel-unnamed.cpp: New file.
    	* gdb.openmp/multi-kernel-unnamed.exp: New file.
    	* gdb.openmp/multi-kernel-unnamed.f90: New file.
    	* gdb.openmp/multi-kernel.cpp: New file.
    	* gdb.openmp/multi-kernel.exp: New file.
    	* gdb.openmp/multi-kernel.f90: New file.
    	* gdb.openmp/info-locals-and-args.exp: New file.
    	* gdb.openmp/intelgt-auto-attach.exp: New file.
    	* gdb.openmp/intelgt-run-inf2.exp: New file.
    	* gdb.openmp/run-to-termination.exp: New file.
    
    Co-authored-by: Abdul Basit Ijaz <abdul.b.ijaz@intel.com>
    Co-authored-by: Aleksandar Paunovic <aleksandar.paunovic@intel.com>

---
+++ gdb/testsuite/gdb.openmp/multi-kernel-unnamed.cpp
@@ -0,0 +1,46 @@
+/* This testcase is part of GDB, the GNU debugger.
+
+   Copyright 2019-2020 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <iostream>
+#include "omp.h"
+
+int
+main (int argc, char *argv[])
+{
+  constexpr unsigned int length = 4;
+  int in_arr[length];
+
+  /* Initialize the input.  */
+  for (unsigned int i = 0; i < length; i++)
+    in_arr[i] = i;
+
+  /* Spawn kernels that are independent of each other.  */
+  for (unsigned int i = 0; i < length; i++)
+    #pragma omp target teams num_teams(1) thread_limit(1) \
+		map(tofrom: in_arr) depend(out: in_arr) nowait
+      {
+	int item = in_arr[i] + 100; /* kernel-line */
+	in_arr[i] = item;
+      }
+#pragma omp barrier
+#pragma omp target teams num_teams(1) thread_limit(1) map(tofrom: in_arr)
+  {
+    in_arr[0] += 1; /* line-after-kernel */
+  }
+
+  return 0; /* exit-line */
+}
---
+++ gdb/testsuite/gdb.openmp/multi-kernel.f90
@@ -0,0 +1,36 @@
+! Copyright 2020 Free Software Foundation, Inc.
+!
+! This program is free software; you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation; either version 3 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <http://www.gnu.org/licenses/> .
+
+program multi_kernel
+  integer, dimension(0:2) :: in_arr = (/ 7, 8, 9 /)
+  integer :: num1, num2
+
+  !$omp target map(tofrom: in_arr) depend(out: in_arr)
+  !$omp teams num_teams(1) thread_limit(1)
+    in_arr(1) = 32
+    in_arr(2) = 10 ! kernel-1-line
+  !$omp end teams
+  !$omp end target
+
+  !$omp target map(tofrom: in_arr) depend(in: in_arr)
+  !$omp teams num_teams(1) thread_limit(1) private(num1, num2)
+    num1 = in_arr(1)
+    num2 = in_arr(2)
+    in_arr(0) = num1 + num2; ! kernel-2-line
+  !$omp end teams
+  !$omp end target
+
+  write(*,*) "Result is ", in_arr(0), "\n" ! line-after-kernel
+end program multi_kernel

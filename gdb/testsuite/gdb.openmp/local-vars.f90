d0656ab9984087be6de76f8e8df6fb026a130175
Abdul Basit Ijaz <abdul.b.ijaz@intel.com>
2020-04-14 13:38:25 +0200

    testsuite, openmp: add tests for offloading
    Add OpenMP tests for offloading to a target device.
    
    gdb/testsuite/ChangeLog:
    2020-04-20  Abdul Basit Ijaz  <abdul.b.ijaz@intel.com>
    	    Aleksandar Paunovic  <aleksandar.paunovic@intel.com>
    
    	* boards/openmp-cpu.exp: New file.
    	* boards/openmp-gpu.exp: New file.
    	* boards/openmp-host.exp: New file.
    	* lib/openmp.exp: New file.
    	* lib/openmp-devices.cpp: New file.
    	* lib/openmp-devices.f90: New file.
    	* gdb.openmp/break-before-kernel-workaround.exp: New file.
    	* gdb.openmp/break-before-kernel.exp: New file.
    	* gdb.openmp/break-inside-kernel.exp: New file.
    	* gdb.openmp/break.exp: New file.
    	* gdb.openmp/break2.exp: New file.
    	* gdb.openmp/cond-bp-on-local.exp: New file.
    	* gdb.openmp/delete-bp.exp: New file.
    	* gdb.openmp/disable-bp.exp: New file.
    	* gdb.openmp/parallel-for-1D.cpp: New file.
    	* gdb.openmp/parallel-for-1D.f90: New file.
    	* gdb.openmp/temporary-bp.exp: New file.
    	* gdb.openmp/infcall-parallel-for.exp: New file.
    	* gdb.openmp/step-parallel-for.exp: New file.
    	* gdb.openmp/local-vars.cpp: New file.
    	* gdb.openmp/local-vars.f90: New file.
    	* gdb.openmp/single-task.cpp: New file.
    	* gdb.openmp/single-task.f90: New file.
    	* gdb.openmp/call-stack.cpp: New file.
    	* gdb.openmp/call-stack.exp: New file.
    	* gdb.openmp/call-stack.f90: New file.
    	* gdb.openmp/parallel-for-2D.exp: New file.
    	* gdb.openmp/parallel-for-2D.cpp: New file.
    	* gdb.openmp/parallel-for-2D.f90: New file.
    	* gdb.openmp/read.exp: New file.
    	* gdb.openmp/step-into-function.exp: New file.
    	* gdb.openmp/step.exp: New file.
    	* gdb.openmp/write-local.exp: New file.
    	* gdb.openmp/multi-kernel-async.cpp: New file.
    	* gdb.openmp/multi-kernel-async.exp: New file.
    	* gdb.openmp/multi-kernel-async.f90: New file.
    	* gdb.openmp/multi-kernel-unnamed.cpp: New file.
    	* gdb.openmp/multi-kernel-unnamed.exp: New file.
    	* gdb.openmp/multi-kernel-unnamed.f90: New file.
    	* gdb.openmp/multi-kernel.cpp: New file.
    	* gdb.openmp/multi-kernel.exp: New file.
    	* gdb.openmp/multi-kernel.f90: New file.
    	* gdb.openmp/info-locals-and-args.exp: New file.
    	* gdb.openmp/intelgt-auto-attach.exp: New file.
    	* gdb.openmp/intelgt-run-inf2.exp: New file.
    	* gdb.openmp/run-to-termination.exp: New file.
    
    Co-authored-by: Abdul Basit Ijaz <abdul.b.ijaz@intel.com>
    Co-authored-by: Aleksandar Paunovic <aleksandar.paunovic@intel.com>

---
+++ gdb/testsuite/gdb.openmp/local-vars.f90
@@ -0,0 +1,38 @@
+! Copyright 2020 Free Software Foundation, Inc.
+!
+! This program is free software; you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation; either version 3 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <http://www.gnu.org/licenses/> .
+
+program local_vars
+  integer :: glob = 0
+  integer, target :: a
+  integer*8 :: b = 3
+  integer*2 :: c
+  integer, pointer :: pa
+
+  !$omp target teams num_teams(1) thread_limit(1) &
+  !$omp& map(from:glob) private(a, b, c, pa)
+  pa => a                 ! kernel-line-1
+  a = 0                   ! kernel-line-2
+  c = 2                   ! kernel-line-3
+  glob = 5                ! kernel-line-4
+  c = glob + a + b        ! kernel-line-5
+  a = a + 1               ! kernel-line-6
+  pa = 0                  ! kernel-line-7
+  a = a + 1               ! kernel-line-8
+  !$omp end target teams
+
+  !$omp single
+  glob = 10 ! line-after-kernel
+  !$omp end single
+end program local_vars ! return-stmt
---
+++ gdb/testsuite/gdb.openmp/multi-kernel-unnamed.f90
@@ -0,0 +1,39 @@
+! Copyright 2020 Free Software Foundation, Inc.
+!
+! This program is free software; you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation; either version 3 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <http://www.gnu.org/licenses/> .
+
+program multi_kernel_unnamed
+  integer, parameter :: length = 3
+  integer, dimension(0:length) :: in_arr
+  integer :: i, item
+
+  do i = 0, length
+    in_arr(i) = i
+  end do
+
+  do i = 0, length
+    !$omp target teams num_teams(1) thread_limit(1) map(tofrom: in_arr) &
+    !$omp depend(out: in_arr) nowait private(item)
+      item = in_arr(i) + 100 ! kernel-line
+      in_arr(i) = item
+    !$omp end target teams
+  end do
+
+  !$omp barrier
+  !$omp target teams num_teams(1) thread_limit(1) map(tofrom: in_arr)
+    in_arr(0) = in_arr(0) + 1 ! line-after-kernel
+  !$omp end target teams
+
+  print *, in_arr(0) ! exit-line
+end program multi_kernel_unnamed

9be2d03d39a2279892035eac1c868b79cca9e2d2
Abdul Basit Ijaz <abdul.b.ijaz@intel.com>
2020-04-14 13:38:25 +0200

    testsuite, openmp: add tests for offloading
    Add OpenMP tests for offloading to a target device.
    
    gdb/testsuite/ChangeLog:
    2020-04-20  Abdul Basit Ijaz  <abdul.b.ijaz@intel.com>
    	    Aleksandar Paunovic  <aleksandar.paunovic@intel.com>
    
    	* boards/openmp-cpu.exp: New file.
    	* boards/openmp-gpu.exp: New file.
    	* boards/openmp-host.exp: New file.
    	* lib/openmp.exp: New file.
    	* lib/openmp-devices.cpp: New file.
    	* lib/openmp-devices.f90: New file.
    	* gdb.openmp/break-before-kernel-workaround.exp: New file.
    	* gdb.openmp/break-before-kernel.exp: New file.
    	* gdb.openmp/break-inside-kernel.exp: New file.
    	* gdb.openmp/break.exp: New file.
    	* gdb.openmp/break2.exp: New file.
    	* gdb.openmp/cond-bp-on-local.exp: New file.
    	* gdb.openmp/delete-bp.exp: New file.
    	* gdb.openmp/disable-bp.exp: New file.
    	* gdb.openmp/parallel-for-1D.cpp: New file.
    	* gdb.openmp/parallel-for-1D.f90: New file.
    	* gdb.openmp/temporary-bp.exp: New file.
    	* gdb.openmp/infcall-parallel-for.exp: New file.
    	* gdb.openmp/step-parallel-for.exp: New file.
    	* gdb.openmp/local-vars.cpp: New file.
    	* gdb.openmp/local-vars.f90: New file.
    	* gdb.openmp/single-task.cpp: New file.
    	* gdb.openmp/single-task.f90: New file.
    	* gdb.openmp/call-stack.cpp: New file.
    	* gdb.openmp/call-stack.exp: New file.
    	* gdb.openmp/call-stack.f90: New file.
    	* gdb.openmp/parallel-for-2D.exp: New file.
    	* gdb.openmp/parallel-for-2D.cpp: New file.
    	* gdb.openmp/parallel-for-2D.f90: New file.
    	* gdb.openmp/read.exp: New file.
    	* gdb.openmp/step-into-function.exp: New file.
    	* gdb.openmp/step.exp: New file.
    	* gdb.openmp/write-local.exp: New file.
    	* gdb.openmp/multi-kernel-async.cpp: New file.
    	* gdb.openmp/multi-kernel-async.exp: New file.
    	* gdb.openmp/multi-kernel-async.f90: New file.
    	* gdb.openmp/multi-kernel-unnamed.cpp: New file.
    	* gdb.openmp/multi-kernel-unnamed.exp: New file.
    	* gdb.openmp/multi-kernel-unnamed.f90: New file.
    	* gdb.openmp/multi-kernel.cpp: New file.
    	* gdb.openmp/multi-kernel.exp: New file.
    	* gdb.openmp/multi-kernel.f90: New file.
    	* gdb.openmp/info-locals-and-args.exp: New file.
    	* gdb.openmp/intelgt-auto-attach.exp: New file.
    	* gdb.openmp/intelgt-run-inf2.exp: New file.
    	* gdb.openmp/run-to-termination.exp: New file.
    
    Co-authored-by: Abdul Basit Ijaz <abdul.b.ijaz@intel.com>
    Co-authored-by: Aleksandar Paunovic <aleksandar.paunovic@intel.com>

---
+++ gdb/testsuite/gdb.openmp/parallel-for-2D.cpp
@@ -0,0 +1,64 @@
+/* This testcase is part of GDB, the GNU debugger.
+
+   Copyright 2019-2020 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <iostream>
+#include <omp.h>
+
+int
+main (int argc, char *argv[])
+{
+  constexpr size_t DIM0 = 128;
+  constexpr size_t DIM1 = 64;
+
+  int in_arr[DIM0][DIM1];
+  int out_arr[DIM1][DIM0]; /* will transpose the input.  */
+  unsigned int i, j;
+
+  /* Initialize the input.  */
+  int val = 123;
+  for (i = 0; i < DIM0; i++)
+    for (j = 0; j < DIM1; j++)
+      in_arr[i][j] = val++;
+
+#pragma omp target data map(to : in_arr) map(from : out_arr)
+#pragma omp target teams num_teams(DIM0) thread_limit(DIM1)
+  {
+#pragma omp distribute parallel for collapse(2)
+    for (i = 0; i < DIM0; i++)
+      for (j = 0; j < DIM1; j++)
+	{
+	  int in_elem = in_arr[i][j]; /* kernel-first-line */
+	  int in_elem2 = i;
+	  int in_elem3 = j;
+	  /* Negate the value, write into the transpositional location.  */
+	  out_arr[j][i] = -1 * in_elem; /* kernel-last-line */
+	}
+  }
+
+  /* Verify the output.  */
+  for (i = 0; i < DIM0; i++)
+    for (j = 0; j < DIM1; j++)
+      if (in_arr[i][j] != -out_arr[j][i])
+	{
+	  std::cout << "Element " << j << "," << i << " is " << out_arr[j][i]
+		    << std::endl;
+	  return 1;
+	}
+
+  std::cout << "Correct" << std::endl;
+  return 0;
+}

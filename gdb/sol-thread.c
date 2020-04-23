725cf41928cea160ca9658d93ed7470e16dac807
Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>
2020-04-23 13:20:04 +0200

    gdb, multi-target: do not prune threads of targets other than current
    As far as I understand, prune_threads is designed to delete the
    threads of the current target.  So, while iterating the threads,
    ignore those that do not belong to the current target.  Otherwise,
    we may have to query targets that are not able to answer, e.g.
    an all-stop remote target.
    
    To do this, prune_threads is modified to take a process_stratum_target
    as a parameter.
    
    AR: do the same with delete_exited_threads.

--- gdb/sol-thread.c
+++ gdb/sol-thread.c
@@ -1018,7 +1018,7 @@ void
 sol_thread_target::update_thread_list ()
 {
   /* Delete dead threads.  */
-  prune_threads ();
+  prune_threads (current_inferior ()->process_target ());
 
   /* Find any new LWP's.  */
   beneath ()->update_thread_list ();

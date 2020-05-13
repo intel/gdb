dd1b58e5a3e2153818e8b96a5cd6c432ff94a786
Natalia Saiapova <natalia.saiapova@intel.com>
2020-05-13 13:26:57 +0200

    gdb: move indicator of thread star range from m_state to separate field.
    Do not consider the thread star range as a separate state, as in most cases
    it is treated the same way as the STATE_THREAD_RANGE.  Instead, introduce
    a new bool variable, which holds this information.  Later, when SIMD
    range parsing is added, this will ease the state manipulation.
    
    gdb/ChangeLog:
    2020-05-13  Natalia Saiapova  <natalia.saiapova@intel.com>
    	* thread.c (thread_apply_command): Update.
    	* tid-parse.c (tid_range_parser::init): Init m_in_thread_star_range.
    	(tid_range_parser::finished): Remove STATE_STAR_RANGE.
    	(tid_range_parser::cur_tok): Remove STATE_STAR_RANGE.
    	(tid_range_parser::skip_range): Remove STATE_STAR_RANGE.
    	(tid_range_parser::process_inferior_state): Update.
    	(tid_range_parser::get_tid_or_range): Update.
    	(tid_range_parser::in_star_range): Use m_in_thread_star_range
    	and rename to...
    	(tid_range_parser::in_thread_star_range): ... this.
    	* tid-parse.h (class tid_range_parser):
    	<m_state>: Remove STATE_STAR_RANGE from enum.
    	<m_in_thread_star_range>: New private field.
    
    Signed-off-by: Natalia Saiapova <natalia.saiapova@intel.com>

--- gdb/thread.c
+++ gdb/thread.c
@@ -2092,7 +2092,7 @@ thread_apply_command (const char *tidlist, int from_tty)
       if (inf != NULL)
 	tp = find_thread_id (inf, thr_num);
 
-      if (parser.in_star_range ())
+      if (parser.in_thread_star_range ())
 	{
 	  if (inf == NULL)
 	    {

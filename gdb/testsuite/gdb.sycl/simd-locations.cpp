b109c1080a813bc3e00b7635ac1e4a85125bad6f
Markus Metzger <markus.t.metzger@intel.com>
2020-07-01 11:55:41 +0200

    gdb, testsuite, sycl: test simd locations
    gdb/testsuite/ChangeLog:
    2020-07-02  Markus Metzger  <markus.t.metzger@intel.com>
    
    	* gdb.sycl/simd-locations.cpp: New test.
    	* gdb.sycl/simd-locations.exp: New file.
    	* lib/gdb.exp (gdb_test_expr_eq): New.
    	* lib/sycl.exp (current_thread_has_simd_lanes): New.

---
+++ gdb/testsuite/gdb.sycl/simd-locations.cpp
@@ -0,0 +1,148 @@
+/* This testcase is part of GDB, the GNU debugger.
+
+   Copyright 2020 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <CL/sycl.hpp>
+#include "../lib/sycl-util.cpp"
+#include <iostream>
+
+#define G 2
+#define L 8
+#define N (G * L)
+
+struct in_s {
+  uint16_t reserved;
+  uint16_t a;
+  uint16_t b;
+};
+
+struct out_s {
+  uint16_t reserved[5];
+  uint16_t a;
+  uint16_t c;
+};
+
+struct cs_s {
+  uint16_t reserved[4];
+  uint16_t c;
+};
+
+int
+main (int argc, char *argv[])
+{
+  cl::sycl::queue queue {get_sycl_queue (argc, argv)};
+
+  in_s in[N] = {};
+  out_s out[N] = {};
+  cs_s cs[L];
+
+  for (int i = 0; i < N; ++i)
+    in[i].a = i;
+
+  for (int i = 0; i < L; ++i)
+    cs[i].c = 4;
+
+  uint64_t ops = 0;
+
+  cl::sycl::buffer<in_s> bin (in, cl::sycl::range<1> (N));
+  cl::sycl::buffer<cs_s> bcs (cs, cl::sycl::range<1> (L));
+  cl::sycl::buffer<out_s> bout (out, cl::sycl::range<1> (N));
+  cl::sycl::buffer<uint64_t> bops (&ops, cl::sycl::range<1> (1));
+
+  queue.submit([&](sycl::handler& cgh)
+    {
+      cl::sycl::accessor<in_s, 1, cl::sycl::access::mode::read,
+			 cl::sycl::access::target::global_buffer> ain
+	= bin.get_access<cl::sycl::access::mode::read,
+			 cl::sycl::access::target::global_buffer> (cgh);
+
+      cl::sycl::accessor<cs_s, 1, cl::sycl::access::mode::read,
+			 cl::sycl::access::target::constant_buffer> acs
+	= bcs.get_access<cl::sycl::access::mode::read,
+			 cl::sycl::access::target::constant_buffer> (cgh);
+
+      cl::sycl::accessor<out_s, 1, cl::sycl::access::mode::write,
+			 cl::sycl::access::target::global_buffer> aout
+	= bout.get_access<cl::sycl::access::mode::write,
+			 cl::sycl::access::target::global_buffer> (cgh);
+
+      cl::sycl::accessor<uint64_t, 1, cl::sycl::access::mode::read_write,
+			 cl::sycl::access::target::global_buffer> aops
+	= bops.get_access<cl::sycl::access::mode::read_write,
+			  cl::sycl::access::target::global_buffer> (cgh);
+
+      cl::sycl::accessor<in_s, 1, cl::sycl::access::mode::read_write,
+			 cl::sycl::access::target::local>
+	atmp (cl::sycl::range<1> (N), cgh);
+
+      cgh.parallel_for_work_group (cl::sycl::range<1> (G),
+				   cl::sycl::range<1> (L),
+				   [=] (cl::sycl::group<1> wg)
+	{
+	  cl::sycl::private_memory<cl::sycl::id<1>> pgid (wg);
+	  uint16_t lcs;
+
+	  wg.parallel_for_work_item ([&] (cl::sycl::h_item<1> wi)
+	    {
+	      cl::sycl::id<1> gid = wi.get_global_id ();
+	      cl::sycl::id<1> lid = wi.get_local_id ();
+
+	      in_s in = ain[gid];
+	      uint16_t cs = acs[lid].c;
+
+	      atmp[gid] = in;
+	      pgid (wi) = gid;
+	      lcs = cs;
+
+	      aops[0] += 1; /* bp.1 */
+	    });
+
+	  wg.parallel_for_work_item ([&] (cl::sycl::h_item<1> wi)
+	    {
+	      cl::sycl::id<1> gid = pgid (wi);
+	      cl::sycl::id<1> lid = wi.get_local_id ();
+
+	      uint16_t in = atmp[gid].a;
+	      uint16_t cs = lcs;
+	      out_s out;
+
+	      out.a = in;
+	      out.c = in + cs;
+	      aout[gid] = out;
+
+	      aops[0] += 1; /* bp.2 */
+	    });
+	});
+    });
+
+  cl::sycl::accessor<out_s, 1, cl::sycl::access::mode::read,
+		     cl::sycl::access::target::host_buffer> aout
+    = bout.get_access<cl::sycl::access::mode::read> ();
+
+  int errcode = 0;
+  for (int i = 0; i < N; ++i)
+    {
+      if (aout[i].a != i || aout[i].c != (i + 4))
+	{
+	  std::cerr << "out[" << i << "] = (" << aout[i].a << ", "
+		    << aout[i].c << "), expected (" << i << ", " << i + 4
+		    << ")" << std::endl;
+	  errcode = 1;
+	}
+    }
+
+  return errcode;
+}

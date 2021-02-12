0abfb11e55d1fa849b619f38ce5dd4f7d7de12c8
Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>
2021-02-12 13:13:39 +0100

    Update copyright years to 2021
    Signed-off-by: Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>

--- gdb/intelgt-tdep.c
+++ gdb/intelgt-tdep.c
@@ -1,6 +1,6 @@
 /* Target-dependent code for the Intel(R) Graphics Technology architecture.
 
-   Copyright (C) 2019-2020 Free Software Foundation, Inc.
+   Copyright (C) 2019-2021 Free Software Foundation, Inc.
 
    This file is part of GDB.
 
--- gdbserver/nonstop-low.cc
+++ gdbserver/nonstop-low.cc
@@ -1,7 +1,7 @@
 /* Low level interface to for the remote server for GDB
    that implements architecture-independent non-stop behavior.
 
-   Copyright (C) 1995-2020 Free Software Foundation, Inc.
+   Copyright (C) 1995-2021 Free Software Foundation, Inc.
 
    This file is part of GDB.
 

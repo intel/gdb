bfc5dfb79ec20500cdad32c48918bd6a7ecdb941
Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>
2021-03-24 13:50:07 +0100

    gdb: introduce hidden inferiors
    Introduce the notion of a "hidden" inferior; an inferior that is not
    shown in the 'info inferiors' output.  Hidden inferiors are created by
    using the newly-introduced '-hidden' flag of the 'add-inferior'
    command.  A hidden inferior becomes visible when it's attached to a
    real process (e.g. with pid != 0).
    
    Hidden inferiors are listed with the 'maint info inferiors', which is
    a new command introduced with this patch.  It is analogous to internal
    breakpoints that can be listed via the 'maint info breakpoint'
    command.
    
    Hidden inferiors might be useful if it is desired to not expose an
    inferior to the user until its attached to a process.  For example, in
    heterogeneous computing, suppose a program will offload a kernel to a
    GPU.  In this case, the GPU kernel can be represented via a separate
    inferior.  The GPU target needs to listen the device for the kernel to
    appear, but until it's available, we may want to keep the
    corresponding inferior hidden from the user.  The creation
    and management of the inferior can be done via scripts in this case.
    
    A sample session that shows the relevant commands and how an inferior
    appears:
    
      (gdb) add-inferior -hidden
      [New inferior -1]
      Added inferior -1
      (gdb) inferior -1
      [Switching to inferior -1 [<null>] (<noexec>)]
      (gdb) info inferiors
        Num  Description       Connection           Executable
        1    <null>
      (gdb) maint info inferiors
        Num  Description       Connection           Executable
        1    <null>
      * -1   <null>
      (gdb)
    
    Above, a hidden inferior is added, which has a negative number and can
    be seen via the maint command.  Once an actual process is bound to the
    inferior, it becomes visible.
    
      (gdb) attach 26475
      Attaching to process 26475
      [New inferior 2]
      Reading symbols from ...
      (gdb) info inferiors
        Num  Description       Connection    Executable
        1    <null>
      * 2    process 26475     1 (native)    ...
      (gdb)

--- gdb/inferior.h
+++ gdb/inferior.h
@@ -398,7 +398,7 @@ class inferior : public refcounted_object,
 		 public intrusive_list_node<inferior>
 {
 public:
-  explicit inferior (int pid);
+  explicit inferior (int pid, bool hidden = false);
   ~inferior ();
 
   /* Returns true if we can delete this inferior.  */
@@ -559,7 +559,7 @@ public:
   { return m_gdbarch; }
 
   /* Convenient handle (GDB inferior id).  Unique across all
-     inferiors.  */
+     inferiors.  Negative for hidden inferiors.  */
   int num = 0;
 
   /* Actual target inferior id, usually, a process id.  This matches
@@ -699,11 +699,11 @@ private:
    inferior is found, and return the pointer to the new inferior.
    Caller may use this pointer to initialize the private inferior
    data.  */
-extern struct inferior *add_inferior (int pid);
+extern struct inferior *add_inferior (int pid, bool hidden = false);
 
 /* Same as add_inferior, but don't print new inferior notifications to
    the CLI.  */
-extern struct inferior *add_inferior_silent (int pid);
+extern struct inferior *add_inferior_silent (int pid, bool hidden = false);
 
 extern void delete_inferior (struct inferior *todel);
 
@@ -847,7 +847,7 @@ extern int number_of_inferiors (void);
 
 extern const std::string inferior_pid_to_str (inferior *inf);
 
-extern struct inferior *add_inferior_with_spaces (void);
+extern struct inferior *add_inferior_with_spaces (bool hidden = false);
 
 /* Print the current selected inferior.  */
 extern void print_selected_inferior (struct ui_out *uiout);

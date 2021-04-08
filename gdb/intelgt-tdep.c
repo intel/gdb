be89fff269fefb59804a67375f3248eac63283a3
Felix Willgerodt <felix.willgerodt@intel.com>
2021-04-08 09:30:49 +0200

    gdb, intelgt: Add basic support for SLM expression parsing.
    We use TYPE_INSTANCE_FLAG_ADDRESS_CLASS_1 for now.  If we need more spaces or
    implement some specific parsing for e.g. __local, we can still easily adapt
    this and add new instance flags to gdb.
    
    gdb/ChangeLog:
    2021-05-11  Felix Willgerodt  <felix.willgerodt@intel.com>
    
    	* intelgt-tdep.c (INTELGT_TYPE_INSTANCE_FLAG_SLM): New define.
    	(INTELGT_SLM_ADDRESS_QUALIFIER): Same
    	(intelgt_address_class_type_flags_to_name): New.
    	(intelgt_address_class_name_to_type_flags): New.
    	(intelgt_gdbarch_init): Set the new functions.

--- gdb/intelgt-tdep.c
+++ gdb/intelgt-tdep.c
@@ -24,6 +24,7 @@
 #include "frame-unwind.h"
 #include "gdbcmd.h"
 #include "gdb_obstack.h"
+#include "gdbtypes.h"
 #include "target.h"
 #include "target-descriptions.h"
 #include "value.h"
@@ -40,6 +41,13 @@
 #define GT_FEATURE_ARF9		"org.gnu.gdb.intelgt.arf9"
 #define GT_FEATURE_ARF11	"org.gnu.gdb.intelgt.arf11"
 #define GT_FEATURE_ARF12	"org.gnu.gdb.intelgt.arf12"
+
+/* Address space flags.
+   We are assigning the TYPE_INSTANCE_FLAG_ADDRESS_CLASS_1 to the shared
+   local memory address space.  */
+
+#define INTELGT_TYPE_INSTANCE_FLAG_SLM TYPE_INSTANCE_FLAG_ADDRESS_CLASS_1
+#define INTELGT_SLM_ADDRESS_QUALIFIER "slm"
 
 /* Global debug flag.  */
 static bool intelgt_debug = false;
@@ -754,6 +762,37 @@ intelgt_print_insn (bfd_vma memaddr, struct disassemble_info *info)
 #endif /* defined (HAVE_LIBIGA64)  */
 }
 
+/* Implementation of `address_class_type_flags_to_name' gdbarch method
+   as defined in gdbarch.h.  */
+
+static const char*
+intelgt_address_class_type_flags_to_name (struct gdbarch *gdbarch,
+					  int type_flags)
+{
+  if ((type_flags & INTELGT_TYPE_INSTANCE_FLAG_SLM) != 0)
+    return INTELGT_SLM_ADDRESS_QUALIFIER;
+  else
+    return nullptr;
+}
+
+/* Implementation of `address_class_name_to_type_flags' gdbarch method,
+   as defined in gdbarch.h.  */
+
+static int
+intelgt_address_class_name_to_type_flags (struct gdbarch *gdbarch,
+					  const char* name,
+					  int *type_flags_ptr)
+{
+  if (strcmp (name, INTELGT_SLM_ADDRESS_QUALIFIER) == 0)
+    {
+      *type_flags_ptr = INTELGT_TYPE_INSTANCE_FLAG_SLM;
+      return 1;
+    }
+  else
+    return 0;
+}
+
+
 /* Architecture initialization.  */
 
 static gdbarch *
@@ -885,6 +924,11 @@ intelgt_gdbarch_init (gdbarch_info info, gdbarch_list *arches)
 #if defined (USE_WIN32API)
   set_gdbarch_has_dos_based_file_system (gdbarch, 1);
 #endif
+
+  set_gdbarch_address_class_name_to_type_flags
+    (gdbarch, intelgt_address_class_name_to_type_flags);
+  set_gdbarch_address_class_type_flags_to_name
+    (gdbarch, intelgt_address_class_type_flags_to_name);
 
   return gdbarch;
 }

a30cc01269c9290dec50bb9c137381b65abd1015
Felix Willgerodt <felix.willgerodt@intel.com>
2021-04-08 09:30:50 +0200

    slm, gdbserver: Allow decoding of addr_space in m and x packets
    A later patch will enable sending an optional address space argument in the
    m/x packets.  To not break anything, we enable decoding and using it in
    gdbserver before actually sending it.

--- gdbserver/server.cc
+++ gdbserver/server.cc
@@ -617,9 +617,10 @@ parse_store_memtags_request (char *request, CORE_ADDR *addr, size_t *len,
 
   const char *p = request + strlen ("QMemTags:");
 
-  /* Read address and length.  */
+  /* Read address and length.
+     FIXME: Address space argument is not implemented for qMemTags.  */
   unsigned int length = 0;
-  p = decode_m_packet_params (p, addr, &length, ':');
+  p = decode_m_packet_params (p, addr, &length, ':', nullptr);
   *len = length;
 
   /* Read the tag type.  */
@@ -1185,10 +1186,11 @@ monitor_show_help (void)
    be served from a memory block that does not cover the whole request
    length.  A following request gets the rest served from either
    another block (of the same traceframe) or from the read-only
-   regions.  */
+   regions.  ADDR_SPACE is optional, zero indicates default access.  */
 
 static int
-gdb_read_memory (CORE_ADDR memaddr, unsigned char *myaddr, int len)
+gdb_read_memory (CORE_ADDR memaddr, unsigned char *myaddr, int len,
+		 unsigned int addr_space = 0)
 {
   client_state &cs = get_client_state ();
   int res;
@@ -1211,7 +1213,7 @@ gdb_read_memory (CORE_ADDR memaddr, unsigned char *myaddr, int len)
     }
 
   if (set_desired_process ())
-    res = read_inferior_memory (memaddr, myaddr, len);
+    res = read_inferior_memory (memaddr, myaddr, len, addr_space);
   else
     res = 1;
 
@@ -2464,9 +2466,10 @@ parse_fetch_memtags_request (char *request, CORE_ADDR *addr, size_t *len,
 
   const char *p = request + strlen ("qMemTags:");
 
-  /* Read address and length.  */
+  /* Read address and length.
+     FIXME: Address space argument is not implemented for qMemTags.  */
   unsigned int length = 0;
-  p = decode_m_packet_params (p, addr, &length, ':');
+  p = decode_m_packet_params (p, addr, &length, ':', nullptr);
   *len = length;
 
   /* Read the tag type.  */
@@ -4721,7 +4724,7 @@ process_serial_event (void)
 {
   client_state &cs = get_client_state ();
   int signal;
-  unsigned int len;
+  unsigned int len, addr_space;
   CORE_ADDR mem_addr;
   unsigned char sig;
   int packet_len;
@@ -4865,8 +4868,8 @@ process_serial_event (void)
     case 'm':
       {
 	require_running_or_break (cs.own_buf);
-	decode_m_packet (&cs.own_buf[1], &mem_addr, &len);
-	int res = gdb_read_memory (mem_addr, mem_buf, len);
+	decode_m_packet (&cs.own_buf[1], &mem_addr, &len, &addr_space);
+	int res = gdb_read_memory (mem_addr, mem_buf, len, addr_space);
 	if (res < 0)
 	  write_enn (cs.own_buf);
 	else
@@ -4884,8 +4887,8 @@ process_serial_event (void)
     case 'x':
       {
 	require_running_or_break (cs.own_buf);
-	decode_x_packet (&cs.own_buf[1], &mem_addr, &len);
-	int res = gdb_read_memory (mem_addr, mem_buf, len);
+	decode_x_packet (&cs.own_buf[1], &mem_addr, &len, &addr_space);
+	int res = gdb_read_memory (mem_addr, mem_buf, len, addr_space);
 	if (res < 0)
 	  write_enn (cs.own_buf);
 	else

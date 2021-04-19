d61cbd89edd8a1c67703e1732d4c2dbe512e4cec
Felix Willgerodt <felix.willgerodt@intel.com>
2021-04-19 10:17:29 +0200

    slm, gdb, gdbserver: Add multi-address-space feature to qSupported packet.
    This allows GDB to query gdbserver on supporting future extensions to the
    m, M, X and qSearch:memory packets, which will allow sending an optional
    address space argument.  This will be used in future patches.
    
    gdb/ChangeLog:
    2021-05-11  Felix Willgerodt  <felix.willgerodt@intel.com>
    
    	* remote.c (PACKET_multi_address_space_feature): New enum value.
    	(remote_multi_address_space_p): New function.
    	(remote_protocol_features): Add multi_addr_space.
    	(remote_target::remote_query_supported): Same.
    	(_initialize_remote): Same.
    
    gdb/doc/ChangeLog:
    2021-05-11  Felix Willgerodt  <felix.willgerodt@intel.com>
    
    	* gdb.texinfo (Debugging Remote Programs)
    	(Remote Serial Protocol): Document multi-address-space extensions.
    
    gdbserver/ChangeLog:
    2021-05-11  Felix Willgerodt  <felix.willgerodt@intel.com>
    
    	* server.cc (handle_query): Handle multi-address-space.
    	* server.h (client_state): Add multi_addr_space.

--- gdbserver/server.cc
+++ gdbserver/server.cc
@@ -2727,6 +2727,8 @@ handle_query (char *own_buf, int packet_len, int *new_packet_len_p)
 		}
 	      else if (feature == "vContSupported+")
 		cs.vCont_supported = 1;
+	      else if (feature == "multi-address-space+")
+		cs.multi_addr_space = 1;
 	      else if (feature == "QThreadEvents+")
 		;
 	      else if (feature == "QThreadOptions+")
@@ -2864,6 +2866,8 @@ handle_query (char *own_buf, int packet_len, int *new_packet_len_p)
 	  sprintf (end_buf, ";QThreadOptions=%s",
 		   phex_nz (supported_options));
 	}
+
+      strcat (own_buf, ";multi-address-space+");
 
       strcat (own_buf, ";QThreadEvents+");
 

945926b6d2893fce9cab7c56cc82955195859c9c
Aleksandar Paunovic <aleksandar.paunovic@intel.com>
2021-05-03 12:41:35 +0200

    gdb: Improve the resuming of the stepped thread
    Stepped thread should not be resumed (again) if the next stopping point
    (next stopping PC) cannot be determined.  Do not resume the stepped thread
    in this case.  Resuming would lead to an inconsistent state where the
    stepped thread would be running forever and GDB would never get to
    breakpoints of the other threads.  */
    
    gdb/ChangeLog:
    2021-04-30  Aleksandar Paunovic  <aleksandar.paunovic@intel.com>
    
            * infrun.c (keep_going_stepped_thread): Do not resume an
    	already executing thread.
    
    gdb/testsuite/ChangeLog:
    2021-04-30  Aleksandar Paunovic  <aleksandar.paunovic@intel.com>
    
    	* gdb.base/breakpoint-running-inferior.exp: Add a start_step
    	function check.
    
    Signed-off-by: Aleksandar Paunovic <aleksandar.paunovic@intel.com>

--- gdb/infrun.c
+++ gdb/infrun.c
@@ -7705,7 +7705,7 @@ restart_after_all_stop_detach (process_stratum_target *proc_target)
 
 /* Set a previously stepped thread back to stepping.  Returns true on
    success, false if the resume is not possible (e.g., the thread
-   vanished).  */
+   vanished, or the thread breakpoint position cannot be determined).  */
 
 static bool
 keep_going_stepped_thread (struct thread_info *tp)
@@ -7741,6 +7741,19 @@ keep_going_stepped_thread (struct thread_info *tp)
 
       delete_thread (tp);
       return false;
+    }
+  /* Step start function determines the location of the next stopping
+     point (next PC where the stepped thread should stop).  In case that
+     this position cannot be determined the step_start_function will not
+     be set.  Do not resume the stepped thread in this case.  Resuming
+     would lead to an inconsistent state where the stepped thread would be
+     running forever and GDB would never get to breakpoints of the other
+     threads.  */
+  if (tp->control.step_start_function == nullptr)
+    {
+      infrun_debug_printf ("not resuming previously stepped thread, it is "
+			   "already executing");
+      return 0;
     }
 
   infrun_debug_printf ("resuming previously stepped thread");

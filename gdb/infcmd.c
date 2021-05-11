ccef7a911a1d128b31236b99134b7e6d623005a0
Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>
2021-05-11 08:47:09 +0200

    gdb/infcmd: use the pid from inferior in setup_inferior
    gdb/ChangeLog:
    2021-05-05  Tankut Baris Aktemur  <tankut.baris.aktemur@intel.com>
    
    	* infcmd.c (setup_inferior): Use inferior's pid instead of
    	inferior_ptid.
    
    Signed-off-by: Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>

--- gdb/infcmd.c
+++ gdb/infcmd.c
@@ -2470,7 +2470,7 @@ setup_inferior (int from_tty)
   /* If no exec file is yet known, try to determine it from the
      process itself.  */
   if (get_exec_file (0) == NULL)
-    exec_file_locate_attach (inferior_ptid.pid (), 1, from_tty);
+    exec_file_locate_attach (inferior->pid, 1, from_tty);
   else
     {
       reopen_exec_file ();
@@ -2478,7 +2478,7 @@ setup_inferior (int from_tty)
     }
 
   /* Take any necessary post-attaching actions for this platform.  */
-  target_post_attach (inferior_ptid.pid ());
+  target_post_attach (inferior->pid);
 
   post_create_inferior (from_tty);
 }

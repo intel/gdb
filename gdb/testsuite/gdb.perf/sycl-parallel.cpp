e4d07d3fa83460bf8cf0ad84834a8d2adb0badf7
Abdul Basit Ijaz <abdul.b.ijaz@intel.com>
2021-05-18 18:55:56 +0200

    [INTERNAL], testsuite, perf: Add sycl performance test.
    - Add performance test to measure GDB performance for Sycl test using:
    
    1) step/stepi/next commands.
    2) breakpoint to first line in kernel.
    3) Breakpoints with true and false conditions.
    4) Thread info/apply commands.
    5) interrupt command.
    
    gdb/testsuite/ChangeLog:
    2021-05-31  Abdul Basit Ijaz  <abdul.b.ijaz@intel.com>
    
    	* gdb.perf/lib/perftest/measure.py: Replace time.clock()
    	with time.perf_counter() to support Python 3.9 and update
    	cpu clock time measurement to milliseconds.
    	* gdb.perf/lib/perftest/reporter.pyy: Update cpu time unit
    	to milliseconds.
    	* gdb.perf/sycl-breakpoint.exp: New file.
    	* gdb.perf/sycl-breakpoint.py: New file.
    	* gdb.perf/sycl-falsecondbp.exp: New file.
    	* gdb.perf/sycl-falsecondbp.py: New file.
    	* gdb.perf/sycl-parallel.cpp: New file.
    	* gdb.perf/sycl-step.exp: New file.
    	* gdb.perf/sycl-step.py: New file.
    	* gdb.perf/sycl-thread.exp: New file.
    	* gdb.perf/sycl-thread.py: New file.
    
    Signed-off-by: abijaz <abdul.b.ijaz@intel.com>

---
+++ gdb/testsuite/gdb.perf/sycl-parallel.cpp
@@ -0,0 +1,75 @@
+/* This testcase is part of GDB, the GNU debugger.
+
+   Copyright 2019-2021 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <CL/sycl.hpp>
+#include <iostream>
+#include "../lib/sycl-util.cpp"
+
+#ifndef DIM0
+#define DIM0 1024
+#endif
+
+static int
+get_dim (cl::sycl::id<1> wi, int index)
+{
+  return wi[index];
+}
+
+int
+main (int argc, char *argv[])
+{
+  int in[DIM0];
+  int out[DIM0];
+
+  /* Initialize the input.  */
+  for (unsigned int i = 0; i < DIM0; i++)
+    in[i] = i + 123;
+
+  { /* Extra scope enforces waiting on the kernel.  */
+    cl::sycl::queue deviceQueue {get_sycl_queue (argc, argv)};
+    cl::sycl::range<1> dataRange {DIM0};
+    cl::sycl::buffer<int, 1> bufferIn {&in[0], dataRange};
+    cl::sycl::buffer<int, 1> bufferOut {&out[0], dataRange};
+
+    deviceQueue.submit ([&] (cl::sycl::handler& cgh) /* line-before-kernel */
+      {
+	auto accessorIn
+	  = bufferIn.get_access<cl::sycl::access::mode::read> (cgh);
+	auto accessorOut
+	  = bufferOut.get_access<cl::sycl::access::mode::write> (cgh);
+
+	cgh.parallel_for<class kernel> (dataRange, [=] (cl::sycl::id<1> wiID)
+	  {
+	    int dim0 = get_dim (wiID, 0); /* kernel-first-line */
+	    int in_elem = accessorIn[wiID];
+	    int in_elem2 = accessorIn[dim0] + accessorIn[dim0] + accessorIn[dim0];
+	    accessorOut[wiID] = in_elem + 100; /* kernel-last-line */
+	  });
+      });
+  }
+
+  /* Verify the output.  */
+  for (unsigned int i = 0; i < DIM0; i++)
+    if (out[i] != in[i] + 100)
+      {
+	std::cout << "Element " << i << " is " << out[i] << std::endl;
+	return 1;
+      }
+
+  std::cout << "Correct" << std::endl; /* end-marker */
+  return 0;
+}

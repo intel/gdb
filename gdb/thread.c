5b8c205881ee360c1d28626e80777c1f00261566
Natalia Saiapova <natalia.saiapova@intel.com>
2021-05-21 13:15:24 +0200

    gdb, simd: introduce thread_info::is_active.
    The method returns true, if the thread has active SIMD lanes.

--- gdb/thread.c
+++ gdb/thread.c
@@ -179,6 +179,14 @@ thread_info::active_simd_lanes_mask ()
 /* See gdbthread.h.  */
 
 bool
+thread_info::is_active ()
+{
+  return active_simd_lanes_mask () != 0;
+}
+
+/* See gdbthread.h.  */
+
+bool
 thread_info::is_unavailable ()
 {
   if (state == THREAD_EXITED)
@@ -1937,7 +1945,7 @@ thread_try_catch_cmd (thread_info *thr, gdb::optional<int> ada_task,
       std::string lane_info = "";
       std::vector<int> lanes;
 
-      if (thr->has_simd_lanes () && thr->active_simd_lanes_mask () != 0x0)
+      if (thr->has_simd_lanes () && thr->is_active ())
 	{
 	  /* Show lane information only for active threads.  */
 	  int lane = thr->current_simd_lane ();
@@ -1967,7 +1975,7 @@ thread_try_catch_cmd (thread_info *thr, gdb::optional<int> ada_task,
       if (!flags.silent)
 	{
 	  if (!flags.quiet)
-	    gdb_printf (_("%s"), thr_header.c_str ());
+	    gdb_printf ("%s", thr_header.c_str ());
 	  if (flags.cont)
 	    gdb_printf ("%s\n", ex.what ());
 	  else
@@ -2630,7 +2638,7 @@ print_selected_thread_frame (struct ui_out *uiout,
 	{
 	  uiout->text ("[Switching to thread ");
 	  std::vector<int> lanes;
-	  bool is_active = tp->active_simd_lanes_mask () != 0;
+	  bool is_active = tp->is_active ();
 
 	  if (tp->has_simd_lanes () && is_active)
 	    lanes.push_back (tp->current_simd_lane ());

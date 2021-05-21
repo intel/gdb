a43def773128a5a9c9e1e623370aba258ff35d50
Natalia Saiapova <natalia.saiapova@intel.com>
2021-05-21 13:15:24 +0200

    gdb: introduce thread_info::is_active.
    The method returns true, if the thread has active SIMD lanes.
    
    gdb/ChangeLog:
    2021-05-21  Natalia Saiapova  <natalia.saiapova@intel.com>
    
    	* gdbthread.h (class thread_info): New method is_active.
    	* thread.c (thread_info::is_active): Implement the method.
    	(thread_execute_cmd): Use the method.
    	(thr_try_catch_cmd): Likewise.
    	(print_selected_thread_frame): Likewise.
    
    Signed-off-by: Natalia Saiapova <natalia.saiapova@intel.com>

--- gdb/gdbthread.h
+++ gdb/gdbthread.h
@@ -453,6 +453,9 @@ public:
   /* Return active lanes mask for this thread.  */
   unsigned int active_simd_lanes_mask ();
 
+  /* Return true if thread has active SIMD lanes.  */
+  bool is_active ();
+
   /* Return the current simd lane.  */
   int current_simd_lane ();
 

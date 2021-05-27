f0b1ccf8b47910ae5818c3d5759edfd606999df8
Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>
2021-05-27 17:06:10 +0200

    gdb/progspace: limit updating the address spaces to the current target
    Consider a multi-target debugging scenario, where we create a second
    inferior with a remote target after hitting a breakpoint in the first
    inferior.  Going back to Inferior 1 and resuming shows the breakpoint
    hit again.
    
      $ gdb -q /tmp/test
      Reading symbols from /tmp/test...
      (gdb) break main
      Breakpoint 1 at 0x114e: file /tmp/test.c, line 2.
      (gdb) run
      Starting program: /tmp/test
    
      Breakpoint 1, main () at /tmp/test.c:2
      2         int a = 42;
      (gdb) add-inferior -no-connection
      [New inferior 2]
      Added inferior 2
      (gdb) inferior 2
      [Switching to inferior 2 [<null>] (<noexec>)]
      (gdb) target extended-remote | gdbserver --multi -
      Remote debugging using | gdbserver --multi -
      Remote debugging using stdio
      (gdb) inferior 1
      [Switching to inferior 1 [process 12762] (/tmp/test)]
      [Switching to thread 1.1 (process 12762)]
      #0  main () at /tmp/test.c:2
      2         int a = 42;
      (gdb) continue
      Continuing.
    
      Breakpoint 1, main () at /tmp/test.c:2
      2         int a = 42;
      (gdb)
    
    The breakpoint is hit again because the first inferior's thread is not
    marked for stepping over the breakpoint.  It is not marked for a step-over,
    because the address space of the thread is corrupt.  Thus, GDB does not
    detect that the thread is sitting on a breakpoint.
    
    The address space of the thread becomes corrupt because pushing the
    remote target on the second inferior frees and updates the address
    spaces of all program spaces.  This is done in 'update_address_spaces',
    which is called only when starting a remote target.
    
    Prevent the problem by considering the address spaces for the current
    target only.
    
    gdb/ChangeLog:
    2021-05-27  Tankut Baris Aktemur  <tankut.baris.aktemur@intel.com>
    
    	* progspace.c: Include <unordered_set>.
    	(update_address_spaces): Limit updating the address
    	spaces to the current target.
    
    Signed-off-by: Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>

--- gdb/progspace.c
+++ gdb/progspace.c
@@ -27,6 +27,7 @@
 #include "gdbthread.h"
 #include "inferior.h"
 #include <algorithm>
+#include <unordered_set>
 #include "cli/cli-style.h"
 
 /* The last program space number assigned.  */
@@ -406,8 +407,27 @@ void
 update_address_spaces (void)
 {
   int shared_aspace = gdbarch_has_shared_address_space (target_gdbarch ());
+  process_stratum_target *current_target
+    = current_inferior ()->process_target ();
+  gdb_assert (current_target != nullptr);
 
   init_address_spaces ();
+
+  /* Find the program spaces whose address spaces will be updated, and
+     adjust the address space counter.  */
+  std::unordered_set<program_space *> pspaces_to_update;
+  for (program_space *pspace : program_spaces)
+    {
+      for (inferior *inf : all_inferiors (current_target))
+	if (inf->pspace == pspace)
+	  {
+	    pspaces_to_update.insert (pspace);
+	    break;
+	  }
+
+      if (pspaces_to_update.count (pspace) == 0)
+	++highest_address_space_num;
+    }
 
   if (shared_aspace)
     {
@@ -415,16 +435,20 @@ update_address_spaces (void)
 
       free_address_space (current_program_space->aspace);
       for (struct program_space *pspace : program_spaces)
-	pspace->aspace = aspace;
+	if (pspaces_to_update.count (pspace) > 0)
+	  pspace->aspace = aspace;
     }
   else
     for (struct program_space *pspace : program_spaces)
       {
-	free_address_space (pspace->aspace);
-	pspace->aspace = new_address_space ();
+	if (pspaces_to_update.count (pspace) > 0)
+	  {
+	    free_address_space (pspace->aspace);
+	    pspace->aspace = new_address_space ();
+	  }
       }
 
-  for (inferior *inf : all_inferiors ())
+  for (inferior *inf : all_inferiors (current_target))
     if (gdbarch_has_global_solist (target_gdbarch ()))
       inf->aspace = maybe_new_address_space ();
     else

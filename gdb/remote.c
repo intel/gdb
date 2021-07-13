b0b6c32381013dd2122d7117f38d1e0319741a63
Markus Metzger <markus.t.metzger@intel.com>
2021-07-13 14:49:25 +0200

    gdb, remote, ze: fix "$Hc-1#09...Packet received: E01" during startup
    When opening a connection, the remote target does
    
        set_continue_thread (minus_one_ptid);
    
    which results in
    
        $Hc-1#09
    
    to be send to gdbserver.  In remote-utils.c:read_ptid (), this is read as
    
        { <current inferior>, -1, 0 }
    
    and not recognized as minus_one_ptid when handling 'H' in
    
    	  ptid_t thread_id = read_ptid (&cs.own_buf[2], NULL);
    
    	  if (thread_id == null_ptid || thread_id == minus_one_ptid)
    	    thread_id = null_ptid;
    
    Since minus_one_ptid and null_ptid are treated the same way, this is
    probably what we want.
    
    In remote_target::remote_resume_with_hc (), we do
    
          if (ptid == minus_one_ptid)
            set_continue_thread (any_thread_ptid);
          else
            set_continue_thread (ptid);
    
    which amounts to the same thing as set_thread () does
    
          *buf++ = 'H';
          *buf++ = gen ? 'g' : 'c';
          if (ptid == magic_null_ptid)
            xsnprintf (buf, endbuf - buf, "0");
          else if (ptid == any_thread_ptid)
            xsnprintf (buf, endbuf - buf, "0");
    
    so any_thread_ptid is pretty much the same as null_ptid and as
    minus_one_ptid in this context.
    
    Use any_thread_ptid to align with remote_target::remote_resume_with_hc ().
    
    gdb/ChangeLog:
    2021-07-13  Markus Metzger  <markus.t.metzger@intel.com>
    
    	* remote.c (struct threads_listing_context): Call set_continue_thread
    	with any_thread_ptid.

--- gdb/remote.c
+++ gdb/remote.c
@@ -5120,7 +5120,7 @@ remote_target::start_remote_1 (int from_tty, int extended_p)
       target_update_thread_list ();
 
       /* Let the stub know that we want it to return the thread.  */
-      set_continue_thread (minus_one_ptid);
+      set_continue_thread (any_thread_ptid);
 
       if (thread_count (this) == 0)
 	{

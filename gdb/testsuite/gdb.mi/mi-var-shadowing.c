6b2413c4de643d5659f50e84aa236e64a4858ff0
Abdul Basit Ijaz <abdul.b.ijaz@intel.com>
2021-07-21 21:20:05 +0200

    gdb: add shadowed field in '-stack-list-locals/variables' mi commands
    For C/C++/Fortran languages GDB prints same name variable multiple times in
    case of variable shadowing and it is confusing for user to identify which
    variable belongs to the current scope.  So add 'shadowed' field in
    '-stack-list-locals' mi command for value of super-block shadowed variable.
    
    Suppose we have
    
    int x = 42;
      {
        int x = 99;
        x = 99; /* break here */
      }
    
    The "-stack-list-locals" and "-stack-list-variables" mi commands at the
    "break here" line gives the following output:
    
    (gdb)
    -stack-list-locals 0
    ^done,locals=[name="x",name="x"]
    (gdb)
    -stack-list-locals 1
    ^done,locals=[{name="x",value="99"},{name="x",value="42"}]
    (gdb)
    -stack-list-locals 2
    ^done,locals=[{name="x",type="int",value="99"},{name="x",type="int",value="42"}]
    (gdb)
    -stack-list-variables 0
    ^done,variables=[{name="x"},{name="x"}]
    (gdb)
    -stack-list-variables 1
    ^done,variables=[{name="x",value="99"},{name="x",value="42"}]
    (gdb)
    -stack-list-variables 2
    ^done,variables=[{name="x",type="int",value="99"},{name="x",type="int",value="42"}]
    
    So far this scenario is only known case for C/C++/Fortran language so
    to tackle in this case, when iterating over the stack frame variables keep
    the track of inner block variables and when same variable is found in the
    outer-block then add shadowed in mi command output with only exception to
    'stack-list-locals 0' case where it is not added.  Iteration of stack
    frame variables always starts with inner block so gdb now print shadowed
    field with value true for super-block duplicate variables like this:
    
    (gdb)
    -stack-list-locals 0
    ^done,locals=[name="x",name="x"]
    (gdb)
    -stack-list-locals 1
    ^done,locals=[{name="x",value="99"},{name="x",shadowed="true",value="42"}]
    (gdb)
    -stack-list-locals 2
    ^done,locals=[{name="x",type="int",value="99"},{name="x",shadowed="true",type="int",value="42"}]
    (gdb)
    -stack-list-variables 0
    ^done,variables=[{name="x"},{name="x",shadowed="true"}]
    (gdb)
    -stack-list-variables 1
    ^done,variables=[{name="x",value="99"},{name="x",shadowed="true",value="42"}]
    (gdb)
    -stack-list-variables 2
    ^done,variables=[{name="x",type="int",value="99"},{name="x",shadowed="true",type="int",value="42"}]
    
    In case of rust language it is possible to declare variable with same name
    many times so in this case just print only first instance of variable.
    
    gdb/ChangeLog:
    2021-07-21  Abdul Basit Ijaz  <abdul.b.ijaz@intel.com>
    
    	* mi/mi-cmd-stack.c (list_arg_or_local): Print shadowed field
    	with value true after the value of super-block variable in
    	case of variable shadowing.
    	(list_args_or_locals): Declare unordered_set type object and
    	pass it to function 'list_arg_or_local'.
    
    gdb/testsuite/ChangeLog:
    2021-07-21  Abdul Basit Ijaz  <abdul.b.ijaz@intel.com>
    
    	* gdb.mi/mi2-amd64-entry-value.exp: Update regex expressions for
    	testing '-stack-list-variables' mi command output to handle shadowed
    	field.
    	* gdb.trace/entry-values.exp: Ditto.
    	* gdb.mi/mi-var-shadowing.c: New file.
    	* gdb.mi/mi-var-shadowing.exp: New file.
    
    Signed-off-by: Abdul Basit Ijaz <abdul.b.ijaz@intel.com>

---
+++ gdb/testsuite/gdb.mi/mi-var-shadowing.c
@@ -0,0 +1,48 @@
+/* Copyright (C) 2021 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <stdlib.h>
+
+void
+shadowing ()
+{
+  int a;
+  unsigned int val1 = 1;
+  unsigned int val2 = 2;
+  a = 101;  /* bp for locals 1 */
+  {
+    unsigned int val2 = 3;
+    unsigned int val3 = 4;
+    a = 102;  /* bp for locals 2 */
+    {
+      unsigned int val1 = 5;
+      a = 103;  /* bp for locals 3 */
+      {
+	unsigned int val1 = 6;
+	unsigned int val2 = 7;
+	unsigned int val3 = 8;
+	a = 104;  /* bp for locals 4 */
+      }
+    }
+  }
+  a = 105; /* bp for locals 5 */
+}
+
+int
+main (void)
+{
+  shadowing ();
+  return 0;
+}

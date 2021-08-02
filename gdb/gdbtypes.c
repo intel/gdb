ea82823ad6aca1f0dec44ca8961d296222f70db1
Abdul Basit Ijaz <abdul.b.ijaz@intel.com>
2021-08-02 21:11:50 +0200

    gdb: Fix arrays of variable length strings for FORTRAN
    GDB is not able to print arrays of FORTRAN variable length strings
    when dwarf info contains DW_AT_string_length only regarding the string
    length information.  So handing of dynamic array is updated to handle
    such cases.
    
    Suppose we have
    
    subroutine vla_array (arr1, arr2)
      character (len=*):: arr1 (:)
      character (len=5):: arr2 (:)
    
      print *, arr1 ! break-here
      print *, arr2
    end subroutine vla_array
    
    The "print arr1" and "print arr2" command at the "break-here" line
    gives the following output:
    
    (gdb) print arr1
    $1 = (<error reading variable>
    (gdb) print arr2
    $2 = ('abcde', 'abcde', 'abcde')
    (gdb) ptype arr1
    type = character*(*) (5)
    (gdb) ptype arr2
    type = character*5 (3)
    
    So GDB is able to print the array of string with static length but for
    variable length it fail to do so.  For this case now improve handling of
    the TYPE_CODE_STRING code in dynamic array length resolving function to set
    the static length of strings as well when only DW_AT_string_length is given
    in the dwarf info.
    
    Dwarf info using Intel® Fortran Compiler for such case contains following:
     <1><fd>: Abbrev Number: 12 (DW_TAG_string_type)
        <fe>   DW_AT_name        : (indirect string, offset: 0xd2): .str.ARR1
        <102>   DW_AT_string_length: 3 byte block: 97 23 8 	(DW_OP_push_object_address; DW_OP_plus_uconst: 8)
    
    After fixing it and compiling using Intel® Fortran Compiler now gdb shows
    following:
    
    (gdb) p arr1
    $1 = ('abddefghij', 'abddefghij', 'abddefghij', 'abddefghij', 'abddefghij')
    (gdb) p arr2
    $2 = ('abcde', 'abcde', 'abcde')
    (gdb) ptype arr1
    type = character*10 (5)
    (gdb) ptype arr2
    type = character*5 (3)
    
    gdb/ChangeLog:
    2021-08-02  Abdul Basit Ijaz  <abdul.b.ijaz@intel.com>
    
    	* gdbtypes.c (resolve_dynamic_array_or_string): Improve handling
    	of TYPE_CODE_STRING code to use return value of create_string_type
    	outcome for this case.
    	* c-valprint.c (c_value_print_inner): Handle String type code
    	in the same way as the Array type code.
    
    gdb/testsuite/ChangeLog:
    2021-08-02  Abdul Basit Ijaz  <abdul.b.ijaz@intel.com>
    
    	* gdb.fortran/vla-array.f90: New fie.
    	* gdb.fortran/vla-array.exp: New fie.
    
    Signed-off-by: Abdul Basit Ijaz <abdul.b.ijaz@intel.com>

--- gdb/gdbtypes.c
+++ gdb/gdbtypes.c
@@ -2305,7 +2305,9 @@ resolve_dynamic_array_or_string (struct type *type,
   range_type = resolve_dynamic_range (range_type, addr_stack, resolve_p);
 
   ary_dim = check_typedef (TYPE_TARGET_TYPE (type));
-  if (ary_dim != NULL && ary_dim->code () == TYPE_CODE_ARRAY)
+  if (ary_dim != NULL
+      && (ary_dim->code () == TYPE_CODE_ARRAY
+	  || ary_dim->code () == TYPE_CODE_STRING))
     elt_type = resolve_dynamic_array_or_string (ary_dim, addr_stack, resolve_p);
   else
     elt_type = TYPE_TARGET_TYPE (type);
@@ -2330,8 +2332,11 @@ resolve_dynamic_array_or_string (struct type *type,
   else
     bit_stride = TYPE_FIELD_BITSIZE (type, 0);
 
-  return create_array_type_with_stride (type, elt_type, range_type, NULL,
-					bit_stride);
+  if (type->code () == TYPE_CODE_STRING)
+    return create_string_type (type, elt_type, range_type);
+  else
+    return create_array_type_with_stride (type, elt_type, range_type, NULL,
+					  bit_stride);
 }
 
 /* Resolve dynamic bounds of members of the union TYPE to static

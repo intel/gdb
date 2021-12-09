cb47bd0c1e5f44d1989571befade94e7f99c57fe
Christina Schimpe <christina.schimpe@intel.com>
2021-12-09 14:18:36 +0100

    gdb: Remove workaround for the vCont packet
    The workaround for the vCont packet is no longer required due to the
    former commit "gdb: Make global feature array a per-remote target array".
    The vCont packet is now checked once when the connection is started and
    the supported vCont actions are set to the target's remote state
    attribute.
    
    gdb/ChangeLog:
    2021-12-13  Christina Schimpe  <christina.schimpe@intel.com>
    
    	* remote.c: Remove supports_vCont_probed check workaround.

--- gdb/remote.c
+++ gdb/remote.c
@@ -434,9 +434,6 @@ public: /* data */
 
   /* The status of the stub support for the various vCont actions.  */
   vCont_action_support supports_vCont;
-  /* Whether vCont support was probed already.  This is a workaround
-     until packet_support is per-connection.  */
-  bool supports_vCont_probed;
 
   /* True if the user has pressed Ctrl-C, but the target hasn't
      responded to that.  */
@@ -4801,6 +4798,10 @@ remote_target::start_remote (int from_tty, int extended_p)
      which later probes to skip.  */
   remote_query_supported ();
 
+  /* Check vCont support and set the remote state's vCont_action_support
+     attribute.  */
+  remote_vcont_probe ();
+
   /* If the stub wants to get a QAllow, compose one and send it.  */
   if (m_features.packet_support (PACKET_QAllow) != PACKET_DISABLE)
     set_permissions ();
@@ -6325,7 +6326,6 @@ remote_target::remote_vcont_probe ()
     }
 
   packet_ok (rs->buf, &m_features.m_protocol_packets[PACKET_vCont]);
-  rs->supports_vCont_probed = true;
 }
 
 /* Helper function for building "vCont" resumptions.  Write a
@@ -6514,9 +6514,6 @@ remote_target::remote_resume_with_vcont (ptid_t ptid, int step,
   /* No reverse execution actions defined for vCont.  */
   if (::execution_direction == EXEC_REVERSE)
     return 0;
-
-  if (m_features.packet_support (PACKET_vCont) == PACKET_SUPPORT_UNKNOWN)
-    remote_vcont_probe ();
 
   if (m_features.packet_support (PACKET_vCont) == PACKET_DISABLE)
     return 0;
@@ -7090,12 +7087,6 @@ remote_target::remote_stop_ns (ptid_t ptid)
 	    remote_thr->set_resumed ();
 	  }
       }
-
-  /* FIXME: This supports_vCont_probed check is a workaround until
-     packet_support is per-connection.  */
-  if (m_features.packet_support (PACKET_vCont) == PACKET_SUPPORT_UNKNOWN
-      || !rs->supports_vCont_probed)
-    remote_vcont_probe ();
 
   if (!rs->supports_vCont.t)
     error (_("Remote server does not support stopping threads"));
@@ -14581,9 +14572,6 @@ remote_target::can_do_single_step ()
     {
       struct remote_state *rs = get_remote_state ();
 
-      if (m_features.packet_support (PACKET_vCont) == PACKET_SUPPORT_UNKNOWN)
-	remote_vcont_probe ();
-
       return rs->supports_vCont.s && rs->supports_vCont.S;
     }
   else
@@ -14874,9 +14862,6 @@ show_range_stepping (struct ui_file *file, int from_tty,
 bool
 remote_target::vcont_r_supported ()
 {
-  if (m_features.packet_support (PACKET_vCont) == PACKET_SUPPORT_UNKNOWN)
-    remote_vcont_probe ();
-
   return (m_features.packet_support (PACKET_vCont) == PACKET_ENABLE
 	  && get_remote_state ()->supports_vCont.r);
 }

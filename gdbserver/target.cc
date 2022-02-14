ce68c1ea4026dde44d817431f8600b07cf6e44fe
Nils-Christian Kempke <nils-christian.kempke@intel.com>
2022-02-14 11:06:56 +0100

    gdbserver: make support of run command target dependent
    Some targets like GPUs might not support the run command via the R/vRun
    packets.
    
    This patch adds a new "supports" method to the target and adapts the
    support of vRun and R packets accordingly.
    
    gdbserver/ChangeLog:
    2022-02-14  Nils-Christian Kempke  <nils-christian.kempke@intel.com>
    
    	* server.cc (handle_query): Return vRun and R not supported when
    	not supported by target.
    	(handle_v_requests): Only execute when supported by target.
    	(process_serial_event): Only execute restart when supported by
    	target.
    	* target.cc (supports_run_command): New method, true by default.
    	* target.h (supports_run_command): New method.
    
    Signed-off-by: Nils-Christian Kempke <nils-christian.kempke@intel.com>

--- gdbserver/target.cc
+++ gdbserver/target.cc
@@ -827,6 +827,12 @@ process_stratum_target::supports_catch_syscall ()
   return false;
 }
 
+bool
+process_stratum_target::supports_run_command ()
+{
+  return true;
+}
+
 int
 process_stratum_target::get_ipa_tdesc_idx ()
 {

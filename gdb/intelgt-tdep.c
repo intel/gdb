3cbdb7fdc97b41c555e25bf809dac348cbec55f1
Mohamed Bouhaouel <mohamed.bouhaouel@intel.com>
2022-02-24 12:17:57 +0100

    [HACK] gdb/intelgt: Disable GPU inferior calls for Windows
    Inferior calls is not fully supported for Windows.  Most of the time
    when the infcall involves passing arguments on the stack (e.g. struct
    by value, all GRF registers are used, ...) the GPU hangs and a system
    reboot is required to be able to use the GPU again.
    
    gdb/ChangeLog:
    2022-02-24  Mohamed Bouhaouel  <mohamed.bouhaouel@intel.com>
    
    	* intelgt-tdep.c (intelgt_gdbarch_init): Disable infcall related
    	methodes for Windows.
    
    Signed-off-by: Mohamed Bouhaouel <mohamed.bouhaouel@intel.com>

--- gdb/intelgt-tdep.c
+++ gdb/intelgt-tdep.c
@@ -1121,6 +1121,9 @@ get_field_total_memory (type *struct_type, int field_index)
   return total_memory;
 }
 
+/* Inferior calls is still not fully supported on Windows.  */
+#ifndef USE_WIN32API
+
 /* Return the number of registers required to store an argument.  ARG_TYPE is
    the type of the argument.  */
 
@@ -1444,6 +1447,7 @@ intelgt_push_dummy_call (gdbarch *gdbarch, value *function, regcache *regcache,
   regcache->cooked_write_part (framedesc_regnum, 24, 8, (gdb_byte *) &fe_sp);
   return fe_sp;
 }
+#endif /* not USE_WIN32API */
 
 /* See GRF_HANDLER declaration.  */
 
@@ -1768,6 +1772,8 @@ intelgt_gdbarch_init (gdbarch_info info, gdbarch_list *arches)
 
   set_gdbarch_is_inferior_device (gdbarch, true);
 
+/* Inferior calls is still not fully supported on Windows.  */
+#ifndef USE_WIN32API
   /* Enable inferior call support.  */
   set_gdbarch_push_dummy_call (gdbarch, intelgt_push_dummy_call);
   set_gdbarch_unwind_sp (gdbarch, intelgt_unwind_sp);
@@ -1776,6 +1782,7 @@ intelgt_gdbarch_init (gdbarch_info info, gdbarch_list *arches)
     gdbarch, intelgt_return_in_first_hidden_param_p);
   set_gdbarch_value_arg_coerce (gdbarch, intelgt_value_arg_coerce);
   set_gdbarch_dummy_id (gdbarch, intelgt_dummy_id);
+#endif /* not USE_WIN32API */
 
   return gdbarch;
 }

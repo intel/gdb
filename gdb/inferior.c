e278888ddba0aa7f0ecef2f2c7e25c1cea2c6887
Nils-Christian Kempke <nils-christian.kempke@intel.com>
2022-03-14 11:25:35 +0100

    gdb: hint info devices command in info inferiors when devices are displayed
    This commit adds a hint to the 'info devices' command to the output of
    'info inferiors' when a device is being displayed.  The old behavior:
    
    ~~~
    (gdb) info inferiors
      Num  Description            Connection     Executable
      1    process XXXXX          1 (native)     XXXXXXXXXX
    * 2    device [00:02.0].1     2 (XXXXXXXXXX)
    ~~~
    
    vs. the new behavior:
    
    ~~~
    (gdb) info inferiors
      Num  Description            Connection     Executable
      1    process XXXXX          1 (native)     XXXXXXXXXX
    * 2    device [00:02.0].1     2 (XXXXXXXXXX)
    Type "info devices" to see details of the devices.
    ~~~
    
    There is no additional output when only 'non-device' inferiors are
    displayed.

--- gdb/inferior.c
+++ gdb/inferior.c
@@ -568,6 +568,7 @@ print_inferior (struct ui_out *uiout, const char *requested_inferiors)
   int inf_count = 0;
   size_t connection_id_len = 20;
 
+  bool device_in_inferiors = false;
   /* Compute number of inferiors we will print.  */
   for (inferior *inf : all_inferiors ())
     {
@@ -577,6 +578,11 @@ print_inferior (struct ui_out *uiout, const char *requested_inferiors)
       std::string conn = uiout_field_connection (inf->process_target ());
       if (connection_id_len < conn.size ())
 	connection_id_len = conn.size ();
+
+      /* Check if the inferior is a device to display the additional help
+	 later.  */
+      device_in_inferiors = (device_in_inferiors
+			     || gdbarch_is_inferior_device (inf->gdbarch));
 
       ++inf_count;
     }
@@ -646,6 +652,9 @@ print_inferior (struct ui_out *uiout, const char *requested_inferiors)
 
       uiout->text ("\n");
     }
+
+    if (device_in_inferiors)
+      uiout->text ("Type \"info devices\" to see details of the devices.\n");
 }
 
 static void

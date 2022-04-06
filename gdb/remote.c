7ec4c75c9dc13dcea7262d73ba8b41e3ca4c75e3
Nils-Christian Kempke <nils-christian.kempke@intel.com>
2022-04-06 15:22:41 +0200

    gdb: convert indentation to tabs
    While working on infrun.c and remote.c I noticed a few places where we
    actually had 8 spaces instead of a tab.  This commit fixes that.
    
    gdb/ChangeLog:
    2022-04-06  Nils-Christian Kempke  <nils-christian.kempke@intel.com>
    
    	* infrun.c: Convert 8 spaces to tabs.
    	* remote.c: Dito.
    
    Signed-off-by: Nils-Christian Kempke <nils-christian.kempke@intel.com>

--- gdb/remote.c
+++ gdb/remote.c
@@ -6727,7 +6727,7 @@ remote_target::resume (ptid_t scope_ptid, int step, enum gdb_signal siggnal)
 	= get_remote_thread_info (inferior_thread ());
 
       /* We don't expect the core to ask to resume an already resumed (from
-         its point of view) thread.  */
+	 its point of view) thread.  */
       gdb_assert (remote_thr->get_resume_state () == resume_state::NOT_RESUMED);
 
       remote_thr->set_resumed_pending_vcont (step, siggnal);
@@ -7019,7 +7019,7 @@ remote_target::commit_resumed ()
       gdb_assert (!thread_is_in_step_over_chain (tp));
 
       /* We should never be commit-resuming a thread that has a stop reply.
-         Otherwise, we would end up reporting a stop event for a thread while
+	 Otherwise, we would end up reporting a stop event for a thread while
 	 it is running on the remote target.  */
       remote_state *rs = get_remote_state ();
       for (const auto &stop_reply : rs->stop_reply_queue)
@@ -7029,7 +7029,7 @@ remote_target::commit_resumed ()
 	= remote_thr->resumed_pending_vcont_info ();
 
       /* Check if we need to send a specific action for this thread.  If not,
-         it will be included in a wildcard resume instead.  */
+	 it will be included in a wildcard resume instead.  */
       if (info.step || info.sig != GDB_SIGNAL_0
 	  || !get_remote_inferior (tp->inf)->may_wildcard_vcont)
 	vcont_builder.push_action (tp->ptid, info.step, info.sig);

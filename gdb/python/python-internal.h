feaffe336c7bdf36eeb3bb5f6c05254863da68a4
Tom Tromey <tromey@adacore.com>
2022-06-03 07:59:49 -0600

    Make 'import gdb.events' work
    Pierre-Marie noticed that, while gdb.events is a Python module, it
    can't be imported.  This patch changes how this module is created, so
    that it can be imported, while also ensuring that the module is always
    visible, just as it was in the past.
    
    This new approach required one non-obvious change -- when running
    gdb.base/warning.exp, where --data-directory is intentionally not
    found, the event registries can now be nullptr.  Consequently, this
    patch probably also requires
    
        https://sourceware.org/pipermail/gdb-patches/2022-June/189796.html
    
    Note that this patch obsoletes
    
        https://sourceware.org/pipermail/gdb-patches/2022-June/189797.html

--- gdb/python/python-internal.h
+++ gdb/python/python-internal.h
@@ -539,8 +539,6 @@ int gdbpy_initialize_eventregistry (void)
   CPYCHECKER_NEGATIVE_RESULT_SETS_EXCEPTION;
 int gdbpy_initialize_event (void)
   CPYCHECKER_NEGATIVE_RESULT_SETS_EXCEPTION;
-int gdbpy_initialize_py_events (void)
-  CPYCHECKER_NEGATIVE_RESULT_SETS_EXCEPTION;
 int gdbpy_initialize_arch (void)
   CPYCHECKER_NEGATIVE_RESULT_SETS_EXCEPTION;
 int gdbpy_initialize_registers ()
@@ -558,6 +556,8 @@ int gdbpy_initialize_connection ()
 int gdbpy_initialize_micommands (void)
   CPYCHECKER_NEGATIVE_RESULT_SETS_EXCEPTION;
 void gdbpy_finalize_micommands ();
+
+PyMODINIT_FUNC gdbpy_events_mod_func ();
 
 /* A wrapper for PyErr_Fetch that handles reference counting for the
    caller.  */

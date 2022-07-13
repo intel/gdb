2ca4f0944c0de3bcb9c2d1602939a51d3fb2ec15
Ari Hannula <ari.hannula@intel.com>
2022-07-13 16:16:46 +0200

    gdb, mi: add -thread-simd-width MI command
    New MI command -thread-simd-width returns the SIMD width of a thread.
    Getting the SIMD width for a specific thread can be defined with
    '--thread ID' argument.
    
    Example:
    
    -thread-simd-width
    ^done,simd-width="8"
    (gdb)
    
    gdb/ChangeLog:
    2022-07-14  Ari Hannula  <ari.hannula@intel.com>
    
    	* mi/mi-cmds.c: Add new command.
    	* mi/mi-cmds.h: Add declarations.
    	* mi/mi-main.c: New command implementation.
    	* NEWS: Document changes.
    
    gdb/doc/ChangeLog:
    2022-07-14  Ari Hannula  <ari.hannula@intel.com>
    
    	* gdb.texinfo: Add documentation of the command.
    
    gdb/testsuite/ChangeLog:
    2022-07-14  Ari Hannula  <ari.hannula@intel.com>
    
    	* gdb.threads/simd-mi-thread-simd-width.exp: New file.

--- gdb/mi/mi-cmds.c
+++ gdb/mi/mi-cmds.c
@@ -328,6 +328,7 @@ add_builtin_mi_commands ()
   add_mi_cmd_mi ("target-file-put", mi_cmd_target_file_put);
   add_mi_cmd_mi ("target-flash-erase", mi_cmd_target_flash_erase);
   add_mi_cmd_cli ("target-select", "target", 1);
+  add_mi_cmd_mi ("thread-simd-width", mi_cmd_thread_simd_width);
   add_mi_cmd_mi ("thread-info", mi_cmd_thread_info);
   add_mi_cmd_mi ("thread-list-ids", mi_cmd_thread_list_ids);
   add_mi_cmd_mi ("thread-select", mi_cmd_thread_select,

59190ea2539d1be0bda9f349f26470a4d1921350
Ari Hannula <ari.hannula@intel.com>
2022-07-20 14:02:01 +0200

    gdb, mi: add -thread-hit-lanes-mask MI command
    Returns the hit-lanes-mask value of the thread where it is currently
    stopped.
    
    gdb/ChangeLog:
    2022-07-20  Ari Hannula  <ari.hannula@intel.com>
    
    	* mi/mi-cmds.c: Add new command.
    	* mi/mi-cmds.h: Add declarations.
    	* mi/mi-main.c: New command implementation.
    	* NEWS: Document changes.
    
    gdb/doc/ChangeLog:
    2022-07-20  Ari Hannula  <ari.hannula@intel.com>
    
    	* gdb.texinfo: Add documentation of the command.
    
    gdb/testsuite/ChangeLog:
    2022-07-20  Ari Hannula  <ari.hannula@intel.com>
    
    	* gdb.threads/simd-mi-hit-lanes-mask.exp: New file.

--- gdb/mi/mi-cmds.c
+++ gdb/mi/mi-cmds.c
@@ -330,6 +330,7 @@ add_builtin_mi_commands ()
   add_mi_cmd_cli ("target-select", "target", 1);
   add_mi_cmd_mi ("thread-simd-width", mi_cmd_thread_simd_width);
   add_mi_cmd_mi ("thread-execution-mask", mi_cmd_thread_execution_mask);
+  add_mi_cmd_mi ("thread-hit-lanes-mask", mi_cmd_thread_hit_lanes_mask);
   add_mi_cmd_mi ("thread-info", mi_cmd_thread_info);
   add_mi_cmd_mi ("thread-list-ids", mi_cmd_thread_list_ids);
   add_mi_cmd_mi ("thread-select", mi_cmd_thread_select,

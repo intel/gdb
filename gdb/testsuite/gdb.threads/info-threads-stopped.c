d053b73ec576c6d584b3808593679dde25281304
Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>
2022-07-21 11:35:35 +0000

    gdb: add a '-stopped' option to "info threads"
    Add a '-stopped' option to the "info threads" command to print stopped
    threads only and skip the running ones.  This is a convenience flag to
    filter out the running threads in programs that have many threads.
    
    Suppose we have an application with 5 threads, 2 of which have hit a
    breakpoint.  The "info threads" command in the non-stop mode gives:
    
      (gdb) info threads
        Id   Target Id             Frame
      * 1    Thread 0x7ffff7d99740 (running)
        2    Thread 0x7ffff7d98700 something () at file.c:30
        3    Thread 0x7ffff7597700 (running)
        4    Thread 0x7ffff6d96700 something () at file.c:30
        5    Thread 0x7ffff6595700 (running)
      (gdb)
    
    Using the "-stopped" flag, we get
    
      (gdb) info threads -stopped
        Id   Target Id             Frame
        2    Thread 0x7ffff7d98700 something () at file.c:30
        4    Thread 0x7ffff6d96700 something () at file.c:30
      (gdb)
    
    When combined with a thread ID, the behavior is as follows:
    
      (gdb) info threads 3
        Id   Target Id             Frame
        3    Thread 0x7ffff7597700 (running)
      (gdb) info threads -stopped 3
      No stopped threads match '3'.
      (gdb)
    
    gdb/ChangeLog:
    2022-07-21  Tankut Baris Aktemur  <tankut.baris.aktemur@intel.com>
    
    	* thread.c (struct info_threads_opts) <show_stopped_threads>:
    	New field.
    	(should_print_thread): Check the '-stopped' (show_stopped_threads)
    	option.
    	(print_thread_info_1): Use the command options in an output message.
    	(print_thread_info): Update the option values.
    	* NEWS: Document the '-stopped' flag.
    
    gdb/doc/ChangeLog:
    2022-07-27  Tankut Baris Aktemur  <tankut.baris.aktemur@intel.com>
    
    	* gdb.texinfo (Threads): Document the '-stopped' flag for the
    	"info threads" command.
    
    gdb/testsuite/ChangeLog:
    2022-07-21  Tankut Baris Aktemur  <tankut.baris.aktemur@intel.com>
    
    	* gdb.base/options.exp: Update the test to include "-stopped"
    	for the "info threads" command.
    	* gdb.threads/info-threads-stopped.c: New file.
    	* gdb.threads/info-threads-stopped.exp: New file.
    
    Signed-off-by: Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>

---
+++ gdb/testsuite/gdb.threads/info-threads-stopped.c
@@ -0,0 +1,75 @@
+/* This testcase is part of GDB, the GNU debugger.
+
+   Copyright 2022 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define NUM 4
+
+volatile int should_spin = 1;
+
+static void
+something ()
+{
+}
+
+static void
+spin ()
+{
+  while (should_spin)
+    usleep (1);
+}
+
+static void *
+work (void *arg)
+{
+  int id = *((int *) arg);
+
+  /* Sleep a bit to give the other threads a chance to run.  */
+  usleep (1);
+
+  if (id % 2 == 0)
+    something (); /* break-here */
+  else
+    spin ();
+
+  pthread_exit (NULL);
+}
+
+int
+main ()
+{
+  pthread_t threads[NUM];
+  void *thread_result;
+  int ids[NUM];
+
+  for (int i = 0; i < NUM; i++)
+    {
+      ids[i] = i + 2;
+      pthread_create (&threads[i], NULL, work, &(ids[i]));
+    }
+
+  sleep (10);
+  should_spin = 0;
+
+  for (int i = 0; i < NUM; i++)
+    pthread_join(threads[i], &thread_result);
+
+  return 0;
+}

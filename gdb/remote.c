c8a7b849f5938c2c373d6670df746b93e4a7ead7
Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>
2022-09-14 12:44:32 +0000

    gdb, remote: set up a new inferior when noticed
    A remote target starts up within the context of an inferior.  If we
    notice another inferior during start-up, we need to complete the setup
    of that new inferior.  This applies to inferiors that are, for
    example, discovered when updating the thread list while the remote
    target is starting.
    
    gdb/ChangeLog:
    2022-09-15  Tankut Baris Aktemur  <tankut.baris.aktemur@intel.com>
    
    	* remote.c (remote_target::remote_notice_new_inferior): Setup
    	a brand new inferior even when the remote target is starting up.
    
    Signed-off-by: Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>

--- gdb/remote.c
+++ gdb/remote.c
@@ -2963,6 +2963,8 @@ remote_target::remote_add_thread (ptid_t ptid, bool running, bool executing,
 void
 remote_target::remote_notice_new_inferior (ptid_t currthread, bool executing)
 {
+  inferior *current_inf = current_inferior ();
+
   /* In non-stop mode, we assume new found threads are (externally)
      running until proven otherwise with a stop reply.  In all-stop,
      we can only get here if all threads are stopped.  */
@@ -3043,7 +3045,7 @@ remote_target::remote_notice_new_inferior (ptid_t currthread, bool executing)
 	{
 	  struct remote_state *rs = get_remote_state ();
 
-	  if (!rs->starting_up)
+	  if (!rs->starting_up || inf != current_inf)
 	    {
 	      inf->needs_setup = 1;
 	      notice_new_inferior (new_thr, executing, 0);

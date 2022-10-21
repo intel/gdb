0d03f77dd99aed4178365300ceae14473beb74e3
Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>
2022-10-21 10:20:30 +0000

    gdbserver, ze: overload read_memory and write_memory with thread context
    Accessing the memory requires specifying a debug session handle, which
    is available through a thread.  For convenience, introduce overloaded
    versions of read_memory and write_memory that take a `thread_info *`
    to denote the context in which memory should be accessed.  The
    existing read_memory and write_memory target ops use the overloaded
    versions simply by passing current_thread.  This should preserve the
    existing behavior.  The new overloaded versions will be re-used in a
    follow-up patch.
    
    Signed-off-by: Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>

--- gdbserver/ze-low.h
+++ gdbserver/ze-low.h
@@ -414,6 +414,16 @@ protected:
    This sets the ze execution state, typically to running.  */
   virtual void prepare_thread_resume (thread_info *tp,
 				      enum resume_kind rkind) = 0;
+
+  /* Read the memory in the context of thread TP.  */
+  int read_memory (thread_info *tp, CORE_ADDR memaddr,
+		   unsigned char *myaddr, int len,
+		   unsigned int addr_space = 0);
+
+  /* Write the memory in the context of thread TP.  */
+  int write_memory (thread_info *tp, CORE_ADDR memaddr,
+		    const unsigned char *myaddr, int len,
+		    unsigned int addr_space = 0);
 };
 
 #endif /* GDBSERVER_LEVEL_ZERO_LOW_H */

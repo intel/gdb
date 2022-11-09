974b0442dad3af929b6302678836b663f77f3df8
Bouhaouel, Mohamed <mohamed.bouhaouel@intel.com>
2022-11-09 17:13:21 +0100

    gdb, breakpoint: add a breakpoint type converter
    Add a new function 'bptype_to_target_hw_bp_type' to perform
    the translation from 'bptype' to 'target_hw_bp_type'.
    
    Signed-off-by: Mohamed Bouhaouel <mohamed.bouhaouel@intel.com>

--- gdb/breakpoint.c
+++ gdb/breakpoint.c
@@ -9842,6 +9842,26 @@ break_range_command (const char *arg, int from_tty)
   install_breakpoint (false, std::move (br), true);
 }
 
+/* See breakpoint.h.  */
+
+target_hw_bp_type
+bptype_to_target_hw_bp_type (bptype type)
+{
+  switch (type)
+    {
+    case bp_hardware_watchpoint:
+      return hw_write;
+    case bp_read_watchpoint:
+      return hw_read;
+    case bp_access_watchpoint:
+      return hw_access;
+    case bp_hardware_breakpoint:
+      return hw_execute;
+    default:
+      error (_ ("Bad breakpoint type: bptype %d."), type);
+    }
+}
+
 /*  Return non-zero if EXP is verified as constant.  Returned zero
     means EXP is variable.  Also the constant detection may fail for
     some constant expressions and in such case still falsely return

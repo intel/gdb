bda90976e20c9752d87d40a96680336698ded48c
Bouhaouel, Mohamed <mohamed.bouhaouel@intel.com>
2022-11-09 17:13:21 +0100

    gdb, breakpoint: add a breakpoint type converter
    Add a new function 'bptype_to_target_hw_bp_type' to perform
    the translation from 'bptype' to 'target_hw_bp_type'.
    
    Signed-off-by: Mohamed Bouhaouel <mohamed.bouhaouel@intel.com>

--- gdb/breakpoint.h
+++ gdb/breakpoint.h
@@ -2123,4 +2123,8 @@ extern void enable_disable_bp_location (bp_location *loc, bool enable);
 
 extern void notify_breakpoint_modified (breakpoint *b);
 
+/* Translate BPTYPE to TARGET_HW_BP_TYPE.  */
+
+extern target_hw_bp_type bptype_to_target_hw_bp_type (bptype type);
+
 #endif /* GDB_BREAKPOINT_H */

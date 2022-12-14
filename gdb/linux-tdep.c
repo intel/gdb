9ea62086799812480971e2379c4b84d2967beefe
Oguzhan Karakaya <oguzhan.karakaya@intel.com>
2022-12-14 13:28:06 +0100

    [coregen] gdb, ze: split file note handling into a separate module
    Splits NT_FILE note handling into a separate utility, provided by a new
    elfutils module.  Goal is to encapsulate handling of file note binary
    format in a single place so that it can be reused by different
    platforms.  Many existing tools don't treat NT_FILE as an arch specific
    note so it is convenient to reuse the same format to make it printable
    by common tools like readelf.
    
    This is an intermediate commit, further refactoring will follow in the
    next one.  This commit ensures that all core related tests are still
    passing and simplifies the diff review for the follow-up changes.
    
    gdb/ChangeLog:
    2020-12-17  Mihails Strasuns  <mihails.strasuns@intel.com>
    
    	* elfnote-file.h: New file.
    	* elfnote-file.c: New file.
    	* linux-tdep.c (linux_make_mappings_corefile_notes): Start using
    	  elfnote-file.
    
    Co-authored-by:  Oguzhan Karakaya  <oguzhan.karakaya@intel.com>
    Signed-off-by:  Mihails Strasuns  <mihails.strasuns@intel.com>

--- gdb/linux-tdep.c
+++ gdb/linux-tdep.c
@@ -28,6 +28,7 @@
 #include "regset.h"
 #include "elf/common.h"
 #include "elf-bfd.h"
+#include "elfnote-file.h"
 #include "inferior.h"
 #include "cli/cli-utils.h"
 #include "arch-utils.h"
@@ -1656,25 +1657,6 @@ linux_find_memory_regions (struct gdbarch *gdbarch,
 					 &data);
 }
 
-/* This is used to pass information from
-   linux_make_mappings_corefile_notes through
-   linux_find_memory_regions_full.  */
-
-struct linux_make_mappings_data
-{
-  /* Number of files mapped.  */
-  ULONGEST file_count;
-
-  /* The obstack for the main part of the data.  */
-  struct obstack *data_obstack;
-
-  /* The filename obstack.  */
-  struct obstack *filename_obstack;
-
-  /* The architecture's "long" type.  */
-  struct type *long_type;
-};
-
 /* A callback for linux_find_memory_regions_full that updates the
    mappings data for linux_make_mappings_corefile_notes.
 
@@ -1688,22 +1670,10 @@ linux_make_mappings_callback (ULONGEST vaddr, ULONGEST size,
 			      bool memory_tagged,
 			      const std::string &filename, void *data)
 {
-  struct linux_make_mappings_data *map_data
-    = (struct linux_make_mappings_data *) data;
-  gdb_byte buf[sizeof (ULONGEST)];
-
   gdb_assert (filename.length () > 0);
 
-  ++map_data->file_count;
-
-  pack_long (buf, map_data->long_type, vaddr);
-  obstack_grow (map_data->data_obstack, buf, map_data->long_type->length ());
-  pack_long (buf, map_data->long_type, vaddr + size);
-  obstack_grow (map_data->data_obstack, buf, map_data->long_type->length ());
-  pack_long (buf, map_data->long_type, offset);
-  obstack_grow (map_data->data_obstack, buf, map_data->long_type->length ());
-
-  obstack_grow_str0 (map_data->filename_obstack, filename.c_str ());
+  auto *map = (file_mappings_builder *) data;
+  map->add (vaddr, size, offset, filename.c_str ());
 
   return 0;
 }
@@ -1717,46 +1687,23 @@ linux_make_mappings_corefile_notes (struct gdbarch *gdbarch, bfd *obfd,
 				    gdb::unique_xmalloc_ptr<char> &note_data,
 				    int *note_size)
 {
-  struct linux_make_mappings_data mapping_data;
   type_allocator alloc (gdbarch);
   struct type *long_type
     = init_integer_type (alloc, gdbarch_long_bit (gdbarch), 0, "long");
-  gdb_byte buf[sizeof (ULONGEST)];
 
-  auto_obstack data_obstack, filename_obstack;
+  file_mappings_builder mapping_builder (long_type);
 
-  mapping_data.file_count = 0;
-  mapping_data.data_obstack = &data_obstack;
-  mapping_data.filename_obstack = &filename_obstack;
-  mapping_data.long_type = long_type;
-
-  /* Reserve space for the count.  */
-  obstack_blank (&data_obstack, long_type->length ());
-  /* We always write the page size as 1 since we have no good way to
-     determine the correct value.  */
-  pack_long (buf, long_type, 1);
-  obstack_grow (&data_obstack, buf, long_type->length ());
-
-  linux_find_memory_regions_full (gdbarch, 
-				  dump_note_entry_p,
+  linux_find_memory_regions_full (gdbarch, dump_note_entry_p,
 				  linux_make_mappings_callback,
-				  &mapping_data);
+				  &mapping_builder);
 
-  if (mapping_data.file_count != 0)
-    {
-      /* Write the count to the obstack.  */
-      pack_long ((gdb_byte *) obstack_base (&data_obstack),
-		 long_type, mapping_data.file_count);
+  int file_note_size = 0;
+  void *file_note_data = mapping_builder.build (&file_note_size);
 
-      /* Copy the filenames to the data obstack.  */
-      int size = obstack_object_size (&filename_obstack);
-      obstack_grow (&data_obstack, obstack_base (&filename_obstack),
-		    size);
-
-      note_data.reset (elfcore_write_file_note (obfd, note_data.release (), note_size,
-						obstack_base (&data_obstack),
-						obstack_object_size (&data_obstack)));
-    }
+  if (file_note_data != nullptr)
+    note_data.reset (elfcore_write_file_note (obfd, note_data.release (),
+					      note_size, file_note_data,
+					      file_note_size));
 }
 
 /* Fetch the siginfo data for the specified thread, if it exists.  If

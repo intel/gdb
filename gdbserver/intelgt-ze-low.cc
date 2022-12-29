f143a31ffc1238564520d150cc08f810caf85254
Abdul Basit Ijaz <abdul.b.ijaz@intel.com>
2022-12-29 12:49:19 +0100

    [internal] Update copyrights for Intel added files or contains Intel copyrights
    Signed-off-by: Abdul Basit Ijaz <abdul.b.ijaz@intel.com>

--- gdbserver/intelgt-ze-low.cc
+++ gdbserver/intelgt-ze-low.cc
@@ -1,6 +1,6 @@
 /* Target interface for Intel GT based on level-zero for gdbserver.
 
-   Copyright (C) 2020-2022 Free Software Foundation, Inc.
+   Copyright (C) 2020-2023 Free Software Foundation, Inc.
 
    This file is part of GDB.
 

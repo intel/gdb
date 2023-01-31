0533ecc3be032b643387d5013e0ac72e070ce913
Saiapova, Natalia <natalia.saiapova@intel.com>
2023-01-31 16:34:45 +0000

    gdb, mi: add the "set/show suppress-mi-notifications" command
    This patch adds a user controllable flag which guards suppression of
    some MI notifications.  This is the MI follow up for commit
    
      gdb: add the 'set/show suppress-cli-notifications' command
    
    We suppress MI notifications for
    =thread-selected...
    *stopped...
    ^running
    *running,thread-id="..."
    
    This is useful when GUI has to perform some GDB commands, e.g.  to select
    a target and immediately continue.  When we have thousands of threads,
    change of thread status causes GUI to refresh the related views.
    However, if the stop is artificial, we need a means to avoid it.
    
    Signed-off-by: Saiapova, Natalia <natalia.saiapova@intel.com>

--- gdb/mi/mi-interp.c
+++ gdb/mi/mi-interp.c
@@ -678,8 +678,11 @@ mi_on_normal_stop_1 (struct bpstat *bs, int print_frame)
 	mi_uiout->field_signed ("core", core);
     }
   
-  gdb_puts ("*stopped", mi->raw_stdout);
-  mi_out_put (mi_uiout, mi->raw_stdout);
+  if (!mi_suppress_notification.thread_state)
+    {
+      gdb_puts ("*stopped", mi->raw_stdout);
+      mi_out_put (mi_uiout, mi->raw_stdout);
+    }
   mi_out_rewind (mi_uiout);
   mi_print_timing_maybe (mi->raw_stdout);
   gdb_puts ("\n", mi->raw_stdout);
@@ -1001,19 +1004,23 @@ mi_on_resume_1 (struct mi_interp *mi,
      In future (MI3), we'll be outputting "^done" here.  */
   if (!running_result_record_printed && mi_proceeded)
     {
-      gdb_printf (mi->raw_stdout, "%s^running\n",
-		  current_token ? current_token : "");
+      if (!mi_suppress_notification.thread_state)
+	gdb_printf (mi->raw_stdout, "%s^running\n",
+		    current_token ? current_token : "");
     }
 
   /* Backwards compatibility.  If doing a wildcard resume and there's
      only one inferior, output "all", otherwise, output each resumed
      thread individually.  */
-  if ((ptid == minus_one_ptid || ptid.is_pid ())
-      && !multiple_inferiors_p ())
-    gdb_printf (mi->raw_stdout, "*running,thread-id=\"all\"\n");
-  else
-    for (thread_info *tp : all_non_exited_threads (targ, ptid))
-      mi_output_running (tp);
+  if (!mi_suppress_notification.thread_state)
+    {
+      if ((ptid == minus_one_ptid || ptid.is_pid ())
+	  && !multiple_inferiors_p ())
+	gdb_printf (mi->raw_stdout, "*running,thread-id=\"all\"\n");
+      else
+	for (thread_info *tp : all_non_exited_threads (targ, ptid))
+	  mi_output_running (tp);
+    }
 
   if (!running_result_record_printed && mi_proceeded)
     {

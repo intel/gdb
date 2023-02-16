c3fe926291a2b784876cd1ccd71f9f9ccaec77db
Sergey Bobko <sergey.bobko@intel.com>
2023-02-16 10:24:31 +0000

    gdb, mi: Added --stopped argument in mi_cmd_thread_info
    -thread-info now takes an optional flag, '-stopped', this is
    the MI equivalent of the CLI 'info threads -stopped' command.

--- gdb/mi/mi-main.c
+++ gdb/mi/mi-main.c
@@ -645,11 +645,13 @@ mi_cmd_thread_info (const char *command, const char *const *argv, int argc)
   enum option
     {
       QID_OPT,
+      STOPPED_OPT,
     };
 
   static const struct mi_opt opts[] =
   {
     {"-qid", QID_OPT, 0},
+    {"-stopped", STOPPED_OPT, 0},
     { 0, 0, 0 }
   };
 
@@ -657,6 +659,7 @@ mi_cmd_thread_info (const char *command, const char *const *argv, int argc)
   int oind = 0;
   const char *oarg;
   bool enable_qid = false;
+  bool enable_stopped = false;
 
   while (true)
     {
@@ -669,13 +672,16 @@ mi_cmd_thread_info (const char *command, const char *const *argv, int argc)
 	case QID_OPT:
 	  enable_qid = true;
 	  break;
+	case STOPPED_OPT:
+	  enable_stopped = true;
+	  break;
 	}
     }
 
   if (argc - oind > 1)
     error ("-thread-info: too many arguments");
 
-  info_threads_opts it_opts {false, false, false, enable_qid};
+  info_threads_opts it_opts {false, !enable_stopped, true, enable_qid};
   print_thread_info (current_uiout, argv[oind], -1, it_opts);
 }
 

d22f99d4244d3e5ce3b58d131526e6b48b413adc
Natalia Saiapova <natalia.saiapova@intel.com>
2023-03-21 15:07:09 +0000

    simd: add pretty-printer for workitem coordinates.
    This pretty-printer is applied to `_gdb_workitem` type and is intended
    to change the printing of the work item convenience variables.
    
    Add the pretty printer, which prints the variables as:
    
      (gdb) p $_thread_workgroup
      $1 = {<x>: 2, <y>: 0, <z>: 1}
    
    Signed-off-by: Natalia Saiapova <natalia.saiapova@intel.com>

--- gdb/thread.c
+++ gdb/thread.c
@@ -3108,6 +3108,7 @@ thread_workgroup_make_value (gdbarch *gdbarch, internalvar *var, void *ignore)
     = gdbarch_thread_workgroup (gdbarch, tp);
 
   type *result_type = init_vector_type (bt->builtin_unsigned_int, 3);
+  result_type->set_name ("_gdb_workitem");
   value *val = value_from_contents (result_type,
 				    (gdb_byte *) (workgroup_id.data ()));
 
@@ -3131,6 +3132,7 @@ workitem_local_id_make_value (gdbarch *gdbarch, internalvar *var, void *ignore)
     = gdbarch_current_workitem_local_id (gdbarch, tp);
 
   type *result_type = init_vector_type (bt->builtin_unsigned_int, 3);
+  result_type->set_name ("_gdb_workitem");
   value *val = value_from_contents (result_type,
 				    (gdb_byte *) (local_id.data ()));
 

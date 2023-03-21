fae50e5fdd95140f4b8073275501c72d04dcaf2c
Natalia Saiapova <natalia.saiapova@intel.com>
2023-03-21 15:07:09 +0000

    simd: add pretty-printer for workitem coordinates.
    This pretty-printer is applied to `_gdb_workitem` type and is intended
    to change the printing of the work item convenience variables.
    
    Add the pretty printer, which prints the variables as:
    
      (gdb) p $_thread_workgroup
      $1 = {<x>: 2, <y>: 0, <z>: 1}
    
    Signed-off-by: Natalia Saiapova <natalia.saiapova@intel.com>

---
+++ gdb/python/lib/gdb/printer/workitem.py
@@ -0,0 +1,33 @@
+# Pretty-printers for workitem coordinates _gdb_workitem.
+# Copyright (C) 2023 Free Software Foundation, Inc.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+
+import gdb.printing
+
+
+class WorkitemPrinter:
+    """Prints workitem coordinates with their names."""
+
+    def __init__(self, val):
+        self.val = val
+
+    def to_string(self):
+        return "{<x>: %d, <y>: %d, <z>: %d}" % (self.val[0], self.val[1], self.val[2])
+
+gdb.printing.add_builtin_pretty_printer(
+    "workitem", "^_gdb_workitem", WorkitemPrinter
+)
--- gdb/thread.c
+++ gdb/thread.c
@@ -3090,6 +3090,7 @@ thread_workgroup_make_value (gdbarch *gdbarch, internalvar *var, void *ignore)
     = gdbarch_thread_workgroup (gdbarch, tp);
 
   type *result_type = init_vector_type (bt->builtin_unsigned_int, 3);
+  result_type->set_name ("_gdb_workitem");
   value *val = value_from_contents (result_type,
 				    (gdb_byte *) (workgroup_id.data ()));
 
@@ -3113,6 +3114,7 @@ workitem_local_id_make_value (gdbarch *gdbarch, internalvar *var, void *ignore)
     = gdbarch_current_workitem_local_id (gdbarch, tp);
 
   type *result_type = init_vector_type (bt->builtin_unsigned_int, 3);
+  result_type->set_name ("_gdb_workitem");
   value *val = value_from_contents (result_type,
 				    (gdb_byte *) (local_id.data ()));
 
@@ -3137,6 +3139,7 @@ workitem_global_id_make_value (gdbarch *gdbarch, internalvar *var,
     = gdbarch_current_workitem_global_id (gdbarch, tp);
 
   type *result_type = init_vector_type (bt->builtin_unsigned_int, 3);
+  result_type->set_name ("_gdb_workitem");
   value *val = value_from_contents (result_type,
 				    (gdb_byte *) (global_id.data ()));
 

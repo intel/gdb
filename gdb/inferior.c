a2048265dde6c09daafda1aaf29bc70b944e8591
Gerlicher, Klaus <klaus.gerlicher@intel.com>
2023-05-17 07:54:34 +0000

    gdb: use inferior_pid_to_str where suitable
    When displaying an inferior in the "info inferior" command
    or when switching to an inferior, we use `inferior_pid_to_str` to
    convert an inferior with its PID to a string.  This gives us the chance
    to control how to display an inferior, e.g. when its PID is 0.
    
    In many places in GDB, we use target_pid_to_str to convert a plain
    process PID to a string.  In this patch we replace those instances to
    use inferior_pid_to_str so that we have a uniform place to control
    how PIDs are displayed to the user.
    
    Examples of such suitable locations are:
    * maintenance info prog-spaces
    * detach

--- gdb/inferior.c
+++ gdb/inferior.c
@@ -242,7 +242,7 @@ add_inferior (int pid, bool hidden)
       if (pid != 0)
 	gdb_printf (_("[New inferior %d (%s)]\n"),
 		    inf->num,
-		    target_pid_to_str (ptid_t (pid)).c_str ());
+		    inferior_pid_to_str (inf).c_str ());
       else
 	gdb_printf (_("[New inferior %d]\n"), inf->num);
     }
@@ -357,15 +357,16 @@ exit_inferior (struct inferior *inf)
 void
 detach_inferior (inferior *inf)
 {
-  /* Save the pid, since exit_inferior will reset it.  */
-  int pid = inf->pid;
+  std::string msg;
+
+  if (print_inferior_events)
+    msg = inferior_pid_to_str (inf);
 
   exit_inferior (inf);
 
   if (print_inferior_events)
-    gdb_printf (_("[Inferior %d (%s) detached]\n"),
-		inf->num,
-		target_pid_to_str (ptid_t (pid)).c_str ());
+    gdb_printf (_("[Inferior %d (%s) detached]\n"), inf->num,
+		msg.c_str ());
 }
 
 /* Unhide INF by moving it to the end and assigning a positive num.  */
@@ -530,14 +531,15 @@ number_of_inferiors (void)
 /* Converts an inferior process id to a string.  Like target_pid_to_str, but
    special cases the null process and devices.  */
 
-static std::string
+const std::string
 inferior_pid_to_str (inferior *inf)
 {
   const int pid = inf->pid;
   if (pid != 0)
     {
       gdbarch *target_gdbarch = inf->arch ();
-      if (gdbarch_is_inferior_device (target_gdbarch))
+      if ((target_gdbarch != nullptr)
+	  && gdbarch_is_inferior_device (target_gdbarch))
 	{
 	  const target_desc *tdesc = gdbarch_target_desc (target_gdbarch);
 	  const tdesc_device *device_info = tdesc_device_info (tdesc);

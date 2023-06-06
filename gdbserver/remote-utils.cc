bb817b4fc7baa8ea277f35f62c2454de5bfded94
Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>
2023-06-06 09:51:03 +0000

    gdbserver: handle the 'e' packet
    Implement the 'e' packet by sending the expedited registers
    of the current thread in the RSP response.

--- gdbserver/remote-utils.cc
+++ gdbserver/remote-utils.cc
@@ -1320,6 +1320,20 @@ prepare_resume_reply (char *buf, ptid_t ptid, const target_waitstatus &status)
 
 /* See remote-utils.h.  */
 
+void
+output_expedite_registers (char *buf)
+{
+  regcache *regcache = get_thread_regcache (current_thread);
+
+  for (const std::string &expedited_reg : regcache->tdesc->expedite_regs)
+    buf = outreg (regcache, find_regno (regcache->tdesc,
+					expedited_reg.c_str ()), buf);
+
+  *buf = '\0';
+}
+
+/* See remote-utils.h.  */
+
 const char *
 decode_m_packet_params (const char *from, CORE_ADDR *mem_addr_ptr,
 			unsigned int *len_ptr, const char end_marker,

bbba8e33091ba3247b540bb837cf18ffad3800ca
Rohr, Stephan <stephan.rohr@intel.com>
2023-06-15 05:43:19 -0700

    gdb: fix overflow in 'print_thread_id' function.
    The 'print_thread_id' function uses a buffer with fixed size PRINT_CELL_SIZE
    as defined in print_utils.h to generate a beautified version of the thread ID.
    The thread ID string may include lane information.  GDB terminates with an
    assertion if the output string exceeds the buffer size. For example,
    thread ID with active lanes
    
      * 2.762:[*0 3 6 8-9 11-12 14-16 19 22 24-25 27 30-31]
    
    triggers an assertion in 'xsnprintf'.  This patch fixes this issue by
    
      * Increase PRINT_CELL_SIZE to 100 characters.
      * Truncate lane mask if output exceeds buffer size.
      * Add testcase for 'info threads'.

---
+++ gdb/testsuite/gdb.sycl/intelgt-info-threads.cpp
@@ -0,0 +1,49 @@
+/* This testcase is part of GDB, the GNU debugger.
+
+   Copyright 2023 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <sycl/sycl.hpp>
+#include "../lib/sycl-util.cpp"
+
+int
+main (int argc, char *argv[])
+{
+  constexpr unsigned long data_size = 4096;
+  int out[data_size];
+
+  { /* Extra scope enforces waiting on the kernel.  */
+    sycl::queue deviceQueue {get_sycl_queue (argc, argv)};
+    sycl::range<1> dataRange {data_size};
+    sycl::buffer<int, 1> bufferOut {&out[0], dataRange};
+
+    deviceQueue.submit ([&] (sycl::handler& cgh)
+      {
+	auto accessorOut = bufferOut.get_access<sycl::access::mode::write> (cgh);
+
+	cgh.parallel_for<class kernel> (dataRange, [=] (sycl::id<1> wiID)
+					[[sycl::reqd_sub_group_size(32)]]
+	  {
+	    int dim0 = wiID[0];
+	    if (dim0 % 2 == 0)
+	      accessorOut[wiID] = dim0 - 1;  /* then-branch */
+	    else
+	      accessorOut[wiID] = dim0 + 1;  /* else-branch */
+	  });
+      });
+  }
+
+  return 0;
+}

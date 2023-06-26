e6659ed9ae63969387434a4d8ef4fd63f78d563a
Felix Willgerodt <felix.willgerodt@intel.com>
2023-06-26 16:54:25 +0200

    btrace: Add support for interrupt events.
    Newer Intel CPUs support recording asynchronous events in the PT trace.
    Libipt also recently added support for decoding these.
    
    This patch adds support for interrupt events, based on the existing aux
    infrastructure.  GDB can now display such events during the record
    instruction-history and function-call-history commands.
    
    Subsequent patches will add the rest of the events currently supported.

---
+++ gdb/testsuite/gdb.btrace/null-deref.c
@@ -0,0 +1,34 @@
+/* Copyright 2024 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <stddef.h>
+
+/* To have a function-call-history.  */
+int
+call1 (int a)
+{
+  return a;
+}
+
+int
+main ()
+{
+ int a = call1 (34);
+ int *b = &a;
+ b = NULL;
+
+ a = *b; /* BP1.  */
+ return 0;
+}

8c46a7ca0a20f59f241a962c8a47a950cacf7b0d
Christina Schimpe <christina.schimpe@intel.com>
2023-06-28 15:24:00 +0200

    LAM: Support kernel space debugging
    Sign-extend watchpoint address for kernel space support.

--- gdb/amd64-linux-tdep.c
+++ gdb/amd64-linux-tdep.c
@@ -59,6 +59,8 @@
 
 #include <string_view>
 
+/* Bit 63 is used to select between a kernel-space and user-space address.  */
+#define VA_RANGE_SELECT_BIT_MASK  0x8000000000000000UL
 #define DEFAULT_TAG_MASK 0xffffffffffffffffULL
 
 /* Mapping between the general-purpose registers in `struct user'
@@ -1913,9 +1915,21 @@ static CORE_ADDR
 amd64_linux_remove_non_address_bits_watchpoint (gdbarch *gdbarch,
 						CORE_ADDR addr)
 {
-  /* Clear insignificant bits of a target address using the untag
-     mask.  */
-  return (addr & amd64_linux_lam_untag_mask ());
+  /* The topmost bit tells us if we have a kernel-space address or a user-space
+     address.  */
+  const bool kernel_address = (addr & VA_RANGE_SELECT_BIT_MASK) != 0;
+
+  CORE_ADDR mask = amd64_linux_lam_untag_mask ();
+  /* Clear insignificant bits of a target address using the untag mask.
+     The untag mask preserves the topmost bit, which distinguishes user space
+     from kernel space address.  */
+  addr &= mask;
+
+  /* Sign-extend if we have a kernel-space address.  */
+  if (kernel_address)
+    addr |= ~mask;
+
+  return addr;
 }
 
 /* Fetch and return the TLS DTV (dynamic thread vector) address for PTID.

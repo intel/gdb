c9fbacb8b798e5eeda25a8fd8fc7a0d20b71ca90
Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>
2023-07-20 19:24:46 +0200

    gdbserver: remove the target_thread_handle macro
    The 'target_thread_handle' macro is used in only one place.
    Inline the use and remove the macro.

--- gdbserver/server.cc
+++ gdbserver/server.cc
@@ -2120,7 +2120,7 @@ handle_qxfer_threads_worker (thread_info *thread, std::string *buffer)
   std::string id_str = target_thread_id_str (thread);
   int handle_len;
   gdb_byte *handle;
-  bool handle_status = target_thread_handle (ptid, &handle, &handle_len);
+  bool handle_status = the_target->thread_handle (ptid, &handle, &handle_len);
 
   /* If this is a (v)fork/clone child (has a (v)fork/clone parent),
      GDB does not yet know about this thread, and must not know about

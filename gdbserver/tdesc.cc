9a26e418933b8ad9046973dbeed5aec5b6c97fbf
Willgerodt, Felix <felix.willgerodt@intel.com>
2023-08-28 11:47:49 +0000

    fixup! gdb, gdbserver: replace PBUFSIZ with a target op
    When building with --enable-inprocess-agent, the build is currently failing
    (output with make SHELL='sh -x'):
    
    ~~~
      CXXLD  libinproctrace.so
    
    + g++ -shared -fPIC -Wl,--soname=libinproctrace.so -Wl,--no-undefined -I. -I/user/fwillger/sources/gdb/gdbserver -I/user/fwillger/sources/gdb/gdbserver/../gdb/regformats -I/user/fwillger/sources/gdb/gdbserver/.. -I/user/fwillger/sources/gdb/gdbserver/../include -I/user/fwillger/sources/gdb/gdbserver/../gdb -I/user/fwillger/sources/gdb/gdbserver/../gnulib/import -I../gnulib/import -I/user/fwillger/sources/gdb/gdbserver/.. -I.. -pthread -Wall -Wpointer-arith -Wno-unused -Wunused-value -Wunused-variable -Wunused-function -Wno-switch -Wno-char-subscripts -Wempty-body -Wunused-but-set-parameter -Wunused-but-set-variable -Wno-sign-compare -Wno-error=maybe-uninitialized -Wno-mismatched-tags -Wsuggest-override -Wimplicit-fallthrough=3 -Wduplicated-cond -Wshadow=local -Wdeprecated-copy -Wdeprecated-copy-dtor -Wredundant-move -Wmissing-declarations -Wstrict-null-sentinel -Wformat -Wformat-nonliteral -DGDBSERVER -Wl,--dynamic-list=/user/fwillger/sources/gdb/gdbserver/proc-service.list -g -O0 -o libinproctrace.so alloc-ipa.o ax-ipa.o gdbsupport/common-utils-ipa.o gdbsupport/errors-ipa.o gdbsupport/format-ipa.o gdbsupport/print-utils-ipa.o gdbsupport/rsp-low-ipa.o gdbsupport/safe-strerror-ipa.o gdbsupport/tdesc-ipa.o regcache-ipa.o remote-utils-ipa.o tdesc-ipa.o tracepoint-ipa.o utils-ipa.o linux-amd64-ipa.o linux-x86-tdesc-ipa.o arch/amd64-ipa.o ../libiberty/pic/libiberty.a -ldl -pthread
    /usr/bin/ld: tdesc-ipa.o: in function `init_target_desc(target_desc*, char const**)':
    /user/fwillger/sources/gdb/gdbserver/tdesc.cc:90: undefined reference to `the_target'
    /usr/bin/ld: /user/fwillger/sources/gdb/gdbserver/tdesc.cc:90: undefined reference to `the_target'
    collect2: error: ld returned 1 exit status
    make[1]: *** [Makefile:393: libinproctrace.so] Error 1
    make: *** [Makefile:13501: all-gdbserver] Error 2
    ~~~
    
    This patch fixes that, without including target.h for the IPA build.

--- gdbserver/tdesc.cc
+++ gdbserver/tdesc.cc
@@ -86,7 +86,13 @@ init_target_desc (struct target_desc *tdesc,
 
   /* Make sure PBUFSIZ is large enough to hold a full register
      packet.  */
+#ifndef IN_PROCESS_AGENT
   gdb_assert (2 * tdesc->registers_size + 32 <= target_query_pbuf_size ());
+#else
+  /* We don't have target.h for IPA.  */
+  gdb_assert (2 * tdesc->registers_size + 32 <= PBUFSIZ);
+#endif
+
 
 #ifndef IN_PROCESS_AGENT
   tdesc->expedite_regs = expedite_regs;

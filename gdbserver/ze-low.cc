8674f822b52dfc732f215d68298a4fca57f3bc71
Markus Metzger <markus.t.metzger@intel.com>
2023-09-20 06:27:00 +0000

    fixup! gdbserver, ze: track the number of resumed threads on a device
    We got nresumed tracking wrong in a case where a thread gets silently
    resumed in response to a stop event.
    
    Both resumed and unavailable count as resumed so do the tracking after
    try/catch.

--- gdbserver/ze-low.cc
+++ gdbserver/ze-low.cc
@@ -1543,10 +1543,6 @@ ze_target::fetch_events (ze_device_info &device)
 		if (ze_thread_stopped (tp))
 		  return;
 
-		/* Prevent underflowing.  */
-		if (device.nresumed > 0)
-		  device.nresumed--;
-
 		ze_thread_info *zetp = ze_thread (tp);
 		gdb_assert (zetp != nullptr);
 
@@ -1603,6 +1599,10 @@ ze_target::fetch_events (ze_device_info &device)
 		    zetp->exec_state = ze_thread_state_unavailable;
 		    zetp->waitstatus.set_unavailable ();
 		  }
+
+		/* Prevent underflowing.  */
+		if (device.nresumed > 0)
+		  device.nresumed--;
 	      });
 
 	    dprintf ("device %lu's nresumed=%ld%s",

ffed205f2406db722d733e41284808fcd318cc32
Matti Puputti <matti.puputti@intel.com>
2023-09-22 14:08:38 +0000

    testsuite, mpi: Add testsuite for debugging SYCL applications with IMPI
    This is a testsuite for debugging SYCL applications with Intel MPI (ÌMPI).  It
    uses gtool to attach debugger to spawned processes.
    The testsuite introduces following procs:
      * impi_get_sycl_supported_devices: Use in place of get_sycl_supported_devices
        from sycl.exp.
      * impi_init_sycl_devices_list: Use in place of init_sycl_devices_list from
        sycl.exp.
      * impi_get_gdb_output: Collect the output from all the ranks, until (and
        including) mpigdb-prompt.
      * impi_get_ranks_in_promt: Create a list of ranks in the given prompt.
      * impi_gdb_breakpoint: Use in place of gdb_breakpoint from gdb.exp.
      * impi_gdb_continue_to_breakpoint: Use in place of gdb_continue_to_breakpoint
        from gdb.exp.
      * impi_continue_sycl_program_to_termination: Use in place of
        continue_sycl_program_to_termination from sycl.exp.
      * impi_clean_restart: Use in place of clean_restart from gdb.exp.
      * impi_gdb_start: Use in place of gdb_start from gdb.exp.
      * impi_gdb_spawn: Use in place of gdb_spawn from gdb.exp.
      * sycl_num_subdevices: Get number of subdevices.
    
    This commit adds following test files:
      * impi-break.exp
      * impi-accessor1d.exp
    
    To run:
      $ cd <build>/gdb/testsuite
      $ export GDBFLAGS='-ex "set auto-load safe-path"'
      $ make check TESTS="gdb.sycl/impi-*.exp" RUNTESTFLAGS="--target_board=intel-offload GDBFLAGS='${GDBFLAGS}'"
    
    Signed-off-by: Matti Puputti <matti.puputti@intel.com>

---
+++ gdb/testsuite/lib/sycl-numdevices.cpp
@@ -0,0 +1,69 @@
+/* This testcase is part of GDB, the GNU debugger.
+
+   Copyright 2023 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* Utility file for SYCL test programs to get number of available
+   devices.  */
+
+#include <sycl/sycl.hpp>
+#include <string>
+
+int
+main (int argc, char *argv[])
+{
+  if (argc < 2)
+    {
+      std::cout << "Usage: " << argv[0]
+		<< " <cpu|gpu|accelerator>" << std::endl;
+      exit (1);
+    }
+
+  sycl::info::device_type type;
+  std::string str = argv[1];
+  if (str == "gpu")
+    type = sycl::info::device_type::gpu;
+  else if (str == "cpu")
+    type = sycl::info::device_type::cpu;
+  else if (str == "accelerator")
+    type = sycl::info::device_type::accelerator;
+  else
+    {
+      std::cout << "Unknown device type " << str << std::endl;
+      exit (0);
+    }
+
+  const std::vector<sycl::device> devices
+    = sycl::device::get_devices (type);
+
+  bool devices_same_driver = std::all_of(devices.begin(), devices.end(),
+					 [&devices] (const sycl::device &d)
+    {
+      return d.get_info<sycl::info::device::driver_version> ()
+	== devices.front ().get_info<sycl::info::device::driver_version> ();
+    });
+
+  if (!devices_same_driver) /* devices-driver-check */
+    {
+      std::cerr << "failure: found devices use different drivers" << std::endl;
+      return -1;
+    }
+
+  const unsigned long num_devices = devices.size ();
+
+  std::cout << "SYCL: Number of devices: " << num_devices << std::endl;
+
+  return 0;
+}

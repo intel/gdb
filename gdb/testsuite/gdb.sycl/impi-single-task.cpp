7fa20c06d4d639e4938b4427422d317d94738437
Matti Puputti <matti.puputti@intel.com>
2023-09-22 14:08:38 +0000

    [IMPI] testsuite: Add testsuite for debugging SYCL applications with IMPI
    This is a testsuite for debugging SYCL applications with Intel MPI (ÌMPI).  It
    uses gtool to attach debugger to spawned processes.
    The testsuite introduces following procs:
      * impi_get_sycl_supported_devices: Use in place of get_sycl_supported_devices
        from sycl.exp.
      * impi_init_sycl_devices_list: Use in place of init_sycl_devices_list from
        sycl.exp.
      * impi_get_gdb_output: Collect the output from all the ranks, until (and
        including) mpigdb-prompt.
      * impi_get_ranks_in_promt: Create a list of ranks in the given prompt.
      * impi_gdb_breakpoint: Use in place of gdb_breakpoint from gdb.exp.
      * impi_gdb_continue_to_breakpoint: Use in place of gdb_continue_to_breakpoint
        from gdb.exp.
      * impi_continue_sycl_program_to_termination: Use in place of
        continue_sycl_program_to_termination from sycl.exp.
      * impi_clean_restart: Use in place of clean_restart from gdb.exp.
      * impi_gdb_start: Use in place of gdb_start from gdb.exp.
      * impi_gdb_spawn: Use in place of gdb_spawn from gdb.exp.
      * sycl_num_subdevices: Get number of subdevices.
    
    This commit adds following test files:
      * impi-break.exp
      * impi-accessor1d.exp
    
    To run:
      $ cd <build>/gdb/testsuite
      $ export GDBFLAGS='-ex "set auto-load safe-path"'
      $ make check TESTS="gdb.sycl/impi-*.exp" RUNTESTFLAGS="--target_board=intel-offload GDBFLAGS='${GDBFLAGS}'"
    
    Signed-off-by: Matti Puputti <matti.puputti@intel.com>

---
+++ gdb/testsuite/gdb.sycl/impi-single-task.cpp
@@ -0,0 +1,58 @@
+/* This testcase is part of GDB, the GNU debugger.
+
+   Copyright 2023 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <sycl/sycl.hpp>
+#include <iostream>
+#include <mpi.h>
+#include "../lib/sycl-util.cpp"
+
+int
+main (int argc, char *argv[])
+{
+  int rank = 0;
+  int num_procs = 0;
+  int data[4] = {7, 8, 9};
+
+  MPI_Init (&argc, &argv);
+  MPI_Comm_size (MPI_COMM_WORLD, &num_procs); /* line-after-mpi-init */
+  MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+
+  data[3] = rank;
+
+  { /* Extra scope enforces waiting on the kernel.  */
+    sycl::queue deviceQueue {get_sycl_queue (argc, argv)};
+    sycl::buffer<int, 1> buf {data, sycl::range<1> {3}};
+
+    deviceQueue.submit ([&] (sycl::handler& cgh)
+      {
+	auto numbers = buf.get_access<sycl::access::mode::read_write> (cgh);
+
+	cgh.single_task<> ([=] ()
+	  {
+	    int ten = numbers[1] + 2;  /* kernel-line-1 */
+	    int four = numbers[2] - 5; /* kernel-line-2 */
+	    int fourteen = ten + four; /* kernel-line-3 */
+	    numbers[0] = fourteen * 3; /* kernel-line-4 */
+	  });
+      });
+  }
+
+  std::cout << "Result is " << data[0] << std::endl;  /* line-after-kernel */
+
+  MPI_Finalize ();
+  return data[0] == 42 ? 0 : 1; /* return-stmt */
+}

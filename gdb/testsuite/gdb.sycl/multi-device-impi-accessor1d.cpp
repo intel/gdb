83c54296fa85ff0a8b76ec3b753c38b47a825e0e
Matti Puputti <matti.puputti@intel.com>
2023-09-22 14:08:38 +0000

    testsuite, mpi: Add testsuite for debugging SYCL applications with IMPI
    This is a testsuite for debugging SYCL applications with Intel MPI (ÌMPI).  It
    uses gtool to attach debugger to spawned processes.
    The testsuite introduces following procs:
      * impi_get_sycl_supported_devices: Use in place of get_sycl_supported_devices
        from sycl.exp.
      * impi_init_sycl_devices_list: Use in place of init_sycl_devices_list from
        sycl.exp.
      * impi_get_gdb_output: Collect the output from all the ranks, until (and
        including) mpigdb-prompt.
      * impi_get_ranks_in_promt: Create a list of ranks in the given prompt.
      * impi_gdb_breakpoint: Use in place of gdb_breakpoint from gdb.exp.
      * impi_gdb_continue_to_breakpoint: Use in place of gdb_continue_to_breakpoint
        from gdb.exp.
      * impi_continue_sycl_program_to_termination: Use in place of
        continue_sycl_program_to_termination from sycl.exp.
      * impi_clean_restart: Use in place of clean_restart from gdb.exp.
      * impi_gdb_start: Use in place of gdb_start from gdb.exp.
      * impi_gdb_spawn: Use in place of gdb_spawn from gdb.exp.
      * sycl_num_subdevices: Get number of subdevices.
    
    This commit adds following test files:
      * impi-break.exp
      * impi-accessor1d.exp
    
    To run:
      $ cd <build>/gdb/testsuite
      $ export GDBFLAGS='-ex "set auto-load safe-path"'
      $ make check TESTS="gdb.sycl/impi-*.exp" RUNTESTFLAGS="--target_board=intel-offload GDBFLAGS='${GDBFLAGS}'"
    
    Signed-off-by: Matti Puputti <matti.puputti@intel.com>

---
+++ gdb/testsuite/gdb.sycl/multi-device-impi-accessor1d.cpp
@@ -0,0 +1,50 @@
+/* This testcase is part of GDB, the GNU debugger.
+
+   Copyright 2023 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include "../lib/sycl-util.cpp"
+#include <mpi.h>
+#include <sycl/sycl.hpp>
+
+int
+main (int argc, char *argv[])
+{
+  int rank = 0;
+  int num_procs = 0;
+  int data[3] = { 1, 2, 3 };
+
+  MPI_Init (&argc, &argv);
+  MPI_Comm_size (MPI_COMM_WORLD, &num_procs); /* line-after-mpi-init */
+  MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+
+  {
+    sycl::queue queue = { get_sycl_queue (argc, argv) };
+    sycl::buffer<int, 1> buffer = { data, sycl::range<1> { 3 } };
+    queue.submit ([&] (sycl::handler &cgh)
+      {
+	auto input = buffer.get_access<sycl::access::mode::read> (cgh);
+	cgh.single_task<> ([=] ()
+	  {
+	    int one = input[0];
+	    auto id = sycl::id<1> (1);
+	    int two = input[id];
+	    int dummy = one + two; /* kernel-line */
+	  });
+      });
+  }
+  MPI_Finalize ();
+  return 0;
+}

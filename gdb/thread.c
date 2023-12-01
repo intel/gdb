965d34a299f2f48cacd5edcbbbbdd42522ad6ff9
Rohr, Stephan <stephan.rohr@intel.com>
2023-12-01 01:55:15 -0800

    gdb, mi: Add kernel instance id to -thread-info MI command
    Add `kernel-instance-id` field to the output of the -thread-info MI
    command if the target provides a kernel instance id.  The field can be
    used by IDEs to identify the currently running kernels.

--- gdb/thread.c
+++ gdb/thread.c
@@ -54,6 +54,7 @@
 #include "interps.h"
 #include <array>
 #include <bitset>
+#include "valprint.h"
 
 /* See gdbthread.h.  */
 
@@ -1537,6 +1538,29 @@ print_thread_row (ui_out *uiout, thread_info *tp,
 		}
 
 	      print_workitem_data_mi (uiout, tp);
+
+	      if (gdbarch_kernel_instance_id_p (tp->inf->arch ()))
+		{
+		  try
+		    {
+		      string_file buffer;
+		      value_print_options print_opts;
+		      get_user_print_options (&print_opts);
+
+		      value *val
+			= gdbarch_kernel_instance_id (tp->inf->arch (), tp);
+
+		      common_val_print (val, &buffer, 0, &print_opts,
+					current_language);
+		      uiout->field_string (
+			"kernel-instance-id", buffer.string ().c_str ());
+		    }
+		  catch (const gdb_exception_error &e)
+		    {
+		      /* Skip `kernel-instance-id` field in case of errors.
+			 We don't want to abort the MI command.  */
+		    }
+		}
 	    }
 	}
     }

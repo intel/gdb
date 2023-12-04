8650139047184630e6a22dbab52c0e5f9bf6096a
Natalia Saiapova <natalia.saiapova@intel.com>
2023-12-04 09:19:13 +0000

    gdb: add gdbarch defined methods to determine an entry point.
    For some targets, an entry point cannot be determined solely by looking
    into the object file.  Add an optional arch function
      gdbarch_entry_point (CORE_ADDR *entry_p)
    The function is used in entry_point_address_query (CORE_ADDR *entry_p)
    to get the entry point the way specified by gdbarch.  In that case,
    skip looking into the objfile.
    
    Signed-off-by: Natalia Saiapova <natalia.saiapova@intel.com>

--- gdb/objfiles.c
+++ gdb/objfiles.c
@@ -48,6 +48,8 @@
 #include "gdb_bfd.h"
 #include "btrace.h"
 #include "gdbsupport/pathstuff.h"
+#include "gdbarch.h"
+#include "inferior.h"
 
 #include <algorithm>
 
@@ -295,6 +297,13 @@ objfile::objfile (gdb_bfd_ref_ptr bfd_, program_space *pspace,
 int
 entry_point_address_query (program_space *pspace, CORE_ADDR *entry_p)
 {
+  gdbarch *const gdbarch = current_inferior ()->arch ();
+
+  /* On some architectures the entry point cannot be determined
+     based on object file.  */
+  if (gdbarch_entry_point_p (gdbarch))
+    return gdbarch_entry_point (gdbarch, entry_p);
+
   objfile *objf = pspace->symfile_object_file;
   if (objf == NULL || !objf->per_bfd->ei.entry_point_p)
     return 0;

bb9569fa6a47fc864a48678905ac12f62a3cac45
Schimpe, Christina <christina.schimpe@intel.com>
2023-12-18 18:47:09 +0100

    gdbserver: Add optional runtime register set type.
    Some register sets can be activated and deactivated by the OS during the
    runtime of a process.  One example register is the Intel CET shadow stack
    pointer.  This adds a new type of register set to handle such cases.  We
    shouldn't deactivate these regsets and should not show a warning if we
    fail to read them.

--- gdbserver/linux-low.cc
+++ gdbserver/linux-low.cc
@@ -4976,23 +4976,31 @@ regsets_fetch_inferior_registers (struct regsets_info *regsets_info,
       if (res < 0)
 	{
 	  if (errno == EIO
-	      || (errno == EINVAL && regset->type == OPTIONAL_REGS))
+	      || (errno == EINVAL
+		  && (regset->type == OPTIONAL_REGS
+		      || regset->type == OPTIONAL_RUNTIME_REGS)))
 	    {
 	      /* If we get EIO on a regset, or an EINVAL and the regset is
-		 optional, do not try it again for this process mode.  */
+		 optional, do not try it again for this process mode.
+		 Even if the regset can be enabled at runtime it is safe
+		 to deactivate the regset in case of EINVAL, as we know
+		 the regset itself was the invalid argument of the ptrace
+		 call.  */
 	      disable_regset (regsets_info, regset);
 	    }
-	  else if (errno == ENODATA)
+	  else if (errno == ENODATA
+		   || (errno == ENODEV
+		       && regset->type == OPTIONAL_RUNTIME_REGS)
+		   || errno == ESRCH)
 	    {
-	      /* ENODATA may be returned if the regset is currently
-		 not "active".  This can happen in normal operation,
-		 so suppress the warning in this case.  */
-	    }
-	  else if (errno == ESRCH)
-	    {
-	      /* At this point, ESRCH should mean the process is
-		 already gone, in which case we simply ignore attempts
-		 to read its registers.  */
+	      /* ENODATA or ENODEV may be returned if the regset is
+		 currently not "active".  For ENODEV we additionally check
+		 if the register set is of type OPTIONAL_RUNTIME_REGS.
+		 This can happen in normal operation, so suppress the
+		 warning in this case.
+		 ESRCH should mean the process is already gone at this
+		 point, in which case we simply ignore attempts to read
+		 its registers.  */
 	    }
 	  else
 	    {
@@ -5081,12 +5089,17 @@ regsets_store_inferior_registers (struct regsets_info *regsets_info,
 		 optional, do not try it again for this process mode.  */
 	      disable_regset (regsets_info, regset);
 	    }
-	  else if (errno == ESRCH)
+	  else if ((errno == ENODEV
+		    && regset->type == OPTIONAL_RUNTIME_REGS)
+		   || errno == ESRCH)
 	    {
-	      /* At this point, ESRCH should mean the process is
-		 already gone, in which case we simply ignore attempts
-		 to change its registers.  See also the related
-		 comment in resume_one_lwp.  */
+	      /* If we get ENODEV on a regset and the regset can be
+		 enabled at runtime try it again for this process mode.
+		 This can happen in normal operation, so suppress the
+		 warning in this case.
+		 ESRCH should mean the process is already gone, in which
+		 case we simply ignore attempts to change its registers.
+		 See also the related comment in resume_one_lwp.  */
 	      free (buf);
 	      return 0;
 	    }

14d2b6e5592e6a5caf61e8c9f144e4ce290812c3
Puputti, Matti <matti.puputti@intel.com>
2023-12-21 08:56:01 +0000

    testsuite, openmp: add new tests for kernel read and write pagefaults
    These tests trigger read and write pagefaults in kernel.
    We check that GDB shows the signal SIGSEGV.

---
+++ gdb/testsuite/gdb.openmp/kernel-pagefault-write.f90
@@ -0,0 +1,24 @@
+! Copyright 2023 Free Software Foundation, Inc.
+!
+! This program is free software; you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation; either version 3 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <http://www.gnu.org/licenses/> .
+
+program kernel_pagefault_read
+  integer, pointer :: src
+
+  !$omp target teams num_teams(1) thread_limit(1) map(tofrom: src)
+  src => null()          ! line-before-pagefault
+  src = 0
+  !$omp end target teams
+
+end program kernel_pagefault_read ! return-stmt

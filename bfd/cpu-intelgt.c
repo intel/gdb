b29ee6d0753c64bb314c46a375ad08b0631d2547
Ijaz, Abdul B <abdul.b.ijaz@intel.com>
2024-01-17 17:13:53 +0100

    bfd: update copyright years
    Signed-off-by: Ijaz, Abdul B <abdul.b.ijaz@intel.com>

--- bfd/cpu-intelgt.c
+++ bfd/cpu-intelgt.c
@@ -1,5 +1,5 @@
 /* BFD support for the Intel(R) Graphics Technology architecture.
-   Copyright (C) 2019-2023 Free Software Foundation, Inc.
+   Copyright (C) 2019-2024 Free Software Foundation, Inc.
 
    This file is part of BFD, the Binary File Descriptor library.
 

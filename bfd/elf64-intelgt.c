07b321bf80c87071d598235d00a00cca0810b9dc
Ijaz, Abdul B <abdul.b.ijaz@intel.com>
2024-01-18 12:59:22 +0100

    bfd: update copyright years
    Signed-off-by: Ijaz, Abdul B <abdul.b.ijaz@intel.com>

--- bfd/elf64-intelgt.c
+++ bfd/elf64-intelgt.c
@@ -1,5 +1,5 @@
 /* Intel(R) Graphics Technology-specific support for ELF
-   Copyright (C) 2022 Free Software Foundation, Inc.
+   Copyright (C) 2022-2024 Free Software Foundation, Inc.
 
    This file is part of BFD, the Binary File Descriptor library.
 

b5c0017450c17a7ce25ca6d8daceb2288bfecca7
Schimpe, Christina <christina.schimpe@intel.com>
2024-02-06 13:37:27 -0500

    gdbserver: Add assert in x86_linux_read_description.
    On x86 the PTRACE_GETREGSET request is currently only used for the xstate regset.
    The size of the xstate regset is initialized to 0 such that it can be reset to
    the appropriate size once we know it is supported for the current target
    in x86_linux_read_description.
    
    However, this configuration would not just affect the xstate regset but any regset
    with PTRACE_GETREGSET request that is added in the future.  The new regset  would be
    misconfigured with the xstate regset size.  To avoid this we add an assert for
    unsupported regsets and check explicitly for the note type of the register set.

--- gdbserver/linux-x86-low.cc
+++ gdbserver/linux-x86-low.cc
@@ -895,8 +895,14 @@ x86_linux_read_description ()
 	   regset->fill_function != nullptr;
 	   regset++)
 	{
+
 	  if (regset->get_request == PTRACE_GETREGSET)
-	    regset->size = xsave_len;
+	    {
+	      if (regset->nt_type == NT_X86_XSTATE)
+		regset->size = xsave_len;
+	      else
+		gdb_assert_not_reached ("invalid regset type.");
+	    }
 	  else if (regset->type != GENERAL_REGS)
 	    regset->size = 0;
 	}

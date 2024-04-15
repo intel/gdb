2a0b0b2b8c953550e60149854887dff9f025e02b
Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>
2024-04-15 08:40:36 +0000

    gdb, multi-target: filter for the current target in for_each_just_stopped_thread
    This patch is a preparation for GDB to support an all-stop target in a
    multi-target configuration.
    
    In `for_each_just_stopped_thread`, we apply the callback function on
    the eventing thread only, if the current target is non-stop, or on all
    threads, if the target is all-stop.  In the latter case, we would be
    applying the function on the threads of other targets, too, if we were
    in a multi-target setting.
    
    Filter the iteration using the current target to prevent
    over-application.

--- gdb/infrun.c
+++ gdb/infrun.c
@@ -4200,7 +4200,9 @@ for_each_just_stopped_thread (for_each_just_stopped_thread_callback_func func)
   else
     {
       /* In all-stop mode, all threads have stopped.  */
-      for (thread_info *tp : all_non_exited_threads ())
+      process_stratum_target *current_target
+	= current_inferior ()->process_target ();
+      for (thread_info *tp : all_non_exited_threads (current_target))
 	func (tp);
     }
 }

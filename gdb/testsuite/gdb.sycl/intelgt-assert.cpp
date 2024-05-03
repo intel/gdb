a651b9c2e00ad43fdcd33ced881e9e38c70d9800
Gerlicher, Klaus <klaus.gerlicher@intel.com>
2024-05-03 15:44:32 +0000

    testsuite, sycl: add a test for software exception stops
    SYCL allows usage of assert () in kernels.  This will signal a stop
    through a ZE event which we need to decode and pass on to GDB.
    
    CR0.1 contains the relevant bit that signals such an event.  Handle it
    like we handle other non-breakpoint or single step events.
    
    Add a test to verify basic functionality by hitting an assert and
    verifying the backtrace.

---
+++ gdb/testsuite/gdb.sycl/intelgt-assert.cpp
@@ -0,0 +1,44 @@
+/* This testcase is part of GDB, the GNU debugger.
+
+   Copyright 2024 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <cassert>
+#include "../lib/sycl-util.cpp"
+#include <sycl/sycl.hpp>
+
+void
+force_assert ()
+{
+  assert (false && "assertion hit");
+}
+
+int
+main (int argc, char *argv[])
+{
+  {
+    sycl::queue deviceQueue {get_sycl_queue (argc, argv)};
+
+    deviceQueue.submit ([&] (sycl::handler& cgh)
+      {
+	cgh.single_task<class simple_kernel> ([=] ()
+	  {
+	    force_assert ();
+	  });
+      });
+  }
+
+  return 0;
+}

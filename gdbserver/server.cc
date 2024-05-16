883c05a9cc72bbc7853bdacb84787dd512c4fa8f
Felix Willgerodt <felix.willgerodt@intel.com>
2024-05-16 13:00:36 +0200

    fixup! btrace, gdbserver: Add ptwrite to btrace_config_pt.
    Get rid of an unnessery temporary variable.

--- gdbserver/server.cc
+++ gdbserver/server.cc
@@ -563,20 +563,16 @@ handle_btrace_conf_general_set (char *own_buf)
     }
   else if (strncmp (op, "pt:ptwrite=", strlen ("pt:ptwrite=")) == 0)
     {
-      bool ptwrite;
-
       op += strlen ("pt:ptwrite=");
       if (strncmp (op, "\"yes\"", strlen ("\"yes\"")) == 0)
-	ptwrite = true;
+	current_btrace_conf.pt.ptwrite = true;
       else if (strncmp (op, "\"no\"", strlen ("\"no\"")) == 0)
-	ptwrite = false;
+	current_btrace_conf.pt.ptwrite = false;
       else
 	{
 	  strcpy (own_buf, "E.Bad ptwrite value.");
 	  return -1;
 	}
-
-      current_btrace_conf.pt.ptwrite = ptwrite;
     }
   else
     {

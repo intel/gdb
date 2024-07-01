ea90fdd6d0a14ac780cb35179171f12fbee8a2b8
Markus Metzger <markus.t.metzger@intel.com>
2024-07-01 10:37:19 +0000

    gdb, gdbserver, rsp, ze: acknowledge libraries
    On some accelerator devices, device shared libraries are loaded from a
    host thread rather than from a device thread.  The reporting entity may
    not be the one that actually does the load.
    
    Intel GPU devices, for example, that are based on Level-Zero, will report
    shared library events via the device's debug interface.  This is triggered
    from a host thread calling the run-time interface for loading a device
    shared library.
    
    The Level-Zero run-time ensures that this host thread will not return
    until the respective debug event has been acknowledged by the debugger.
    This allows debuggers to set breakpoints before the new library is used.
    
    Add a mechanism that allows gdbserver to request acknowledgement of newly
    reported shared libraries and GDB to acknowledge requested libraries after
    placing breakpoints.

--- gdbserver/target.h
+++ gdbserver/target.h
@@ -519,6 +519,13 @@ public:
      Returns true if successful and false otherwise.  */
   virtual bool store_memtags (CORE_ADDR address, size_t len,
 			      const gdb::byte_vector &tags, int type);
+
+  /* Acknowledge a library reported by name.  */
+  virtual void ack_library (process_info *process, const char *name);
+
+  /* Acknowledge an in-memory library reported by address.  */
+  virtual void ack_in_memory_library (process_info *process, CORE_ADDR begin,
+				      CORE_ADDR end);
 };
 
 extern process_stratum_target *the_target;
@@ -714,6 +721,19 @@ static inline thread_info *
 target_thread_pending_child (thread_info *thread, target_waitkind *kind)
 {
   return the_target->thread_pending_child (thread, kind);
+}
+
+static inline void
+target_ack_library (process_info *process, const char *name)
+{
+  the_target->ack_library (process, name);
+}
+
+static inline void
+target_ack_in_memory_library (process_info *process,  CORE_ADDR begin,
+			      CORE_ADDR end)
+{
+  the_target->ack_in_memory_library (process, begin, end);
 }
 
 /* Read LEN bytes from MEMADDR in ADDR_SPACE into the buffer MYADDR.

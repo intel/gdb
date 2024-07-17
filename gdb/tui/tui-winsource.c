d61d141c3e2437372ba24fa13e44f72987030dc7
Gerlicher, Klaus <klaus.gerlicher@intel.com>
2024-07-17 08:35:29 +0000

    gdb: rename find_line_pc to find_line_first_pc
    This function really returns only the first location it finds but this is good
    enough for most cases.
    
    Subsequent changes will add functionality to find all pcs of a line which
    will consequently be named find_line_pc.

--- gdb/tui/tui-winsource.c
+++ gdb/tui/tui-winsource.c
@@ -209,7 +209,7 @@ tui_update_source_windows_with_line (struct symtab_and_line sal)
   struct gdbarch *gdbarch = nullptr;
   if (sal.symtab != nullptr)
     {
-      find_line_pc (sal.symtab, sal.line, &sal.pc);
+      find_line_first_pc (sal.symtab, sal.line, &sal.pc);
       gdbarch = sal.symtab->compunit ()->objfile ()->arch ();
     }
 

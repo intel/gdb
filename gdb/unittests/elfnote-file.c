c79c14298c1e43535853b44d1d7cdf1742b08424
Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>
2024-07-30 07:19:53 +0000

    Update copyright years
    This is the result of running
    
      $ cd gdb
      $ ./copyright.py

--- gdb/unittests/elfnote-file.c
+++ gdb/unittests/elfnote-file.c
@@ -1,6 +1,6 @@
 /* Self tests for ELF NT_FILE builder/iterator.
 
-   Copyright (C) 2021 Free Software Foundation, Inc.
+   Copyright (C) 2021-2024 Free Software Foundation, Inc.
 
    This file is part of GDB.
 

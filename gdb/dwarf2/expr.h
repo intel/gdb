346aa3e83f970058e59b7d64305795df5610bd0f
Markus Metzger <markus.t.metzger@intel.com>
2024-12-13 15:17:26 +0100

    gdb, dwarf, ze: add DW_OP_INTEL_regval_bits
    Add support for a new DWARF expression proposed in
    https://dwarfstd.org/ShowIssue.php?issue=201007.1.
    
    Cc: <binutils@sourceware.org>

--- gdb/dwarf2/expr.h
+++ gdb/dwarf2/expr.h
@@ -257,6 +257,11 @@ private:
      memory reads to come from the passed-in buffer.  */
   void read_mem (gdb_byte *buf, CORE_ADDR addr, size_t length);
 
+  /* Read BITSIZE bits from the register indicated by the DWARF register
+     number DWREGNUM starting at bit BITOFFSET into BUF.  */
+  void read_reg (gdb_byte *buf, size_t bitoffset, size_t bitsize,
+		 int dwregnum);
+
   /* Deref ADDR with size SIZE and return a value of type TYPE.
      If TYPE == nullptr, defaults to this->address_type ().  */
   value *deref (CORE_ADDR addr, int size, struct type *type = nullptr);

94efecae0852c325fb661956b68c36a1b700371a
Markus Metzger <markus.t.metzger@intel.com>
2024-12-13 15:17:21 +0100

    gdb, infrun, ze: allow saving process events
    When a process stop reply is encountered during stop_all_threads (), a new
    thread with the process' ptid is created and the waitstatus is saved in
    that thread.  The effect is that we have a thread with a process ptid and
    we dropped the event.
    
    Save the waitstatus in the inferior, prevent that inferior from resuming,
    and return it on the next wait.

--- gdb/inferior.h
+++ gdb/inferior.h
@@ -64,6 +64,7 @@ struct thread_info;
 #include "displaced-stepping.h"
 
 #include "gdbsupport/unordered_map.h"
+#include <optional>
 
 struct infcall_suspend_state;
 struct infcall_control_state;
@@ -326,6 +327,9 @@ struct inferior_control_state
 
   /* See the definition of stop_kind above.  */
   enum stop_kind stop_soon;
+
+  /* The waitstatus for this inferior's last event.  */
+  std::optional<target_waitstatus> waitstatus;
 };
 
 /* Initialize the inferior-related global state.  */

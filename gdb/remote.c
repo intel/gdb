299b8d1a3336783a6142f037d2295599806b2d32
Markus Metzger <markus.t.metzger@intel.com>
2024-12-13 15:17:22 +0100

    gdb, gdbserver, ze: add U stop reply
    Add a new stop reply U for unavailable saying that we tried to stop a
    process or thread but it would not respond and we cannot afford waiting.
    
    This may occur when modeling threads as hardware threads on GPUs, where
    threads that are currently idle cannot be interacted with.  We cannot
    afford waiting for threads to become available again as that may require
    submitting new work from the host process.  Or it may never happen for
    some devices that simply are not used (anymore).
    
    Reviewed-By: Eli Zaretskii <eliz@gnu.org>

--- gdb/remote.c
+++ gdb/remote.c
@@ -412,6 +412,9 @@ enum {
      errors, and so they should not need to check for this feature.  */
   PACKET_accept_error_message,
 
+  /* Support TARGET_WAITKIND_UNAVAILABLE.  */
+  PACKET_unavailable,
+
   PACKET_MAX
 };
 
@@ -5919,6 +5922,8 @@ static const struct protocol_feature remote_protocol_features[] = {
   { "binary-upload", PACKET_DISABLE, remote_supported_packet, PACKET_x },
   { "vRun", PACKET_ENABLE, remote_supported_packet, PACKET_vRun },
   { "R", PACKET_ENABLE, remote_supported_packet, PACKET_R },
+  { "unavailable", PACKET_DISABLE, remote_supported_packet,
+    PACKET_unavailable },
   { "vAck:library", PACKET_DISABLE, remote_supported_packet,
     PACKET_vAck_library },
   { "vAck:in-memory-library", PACKET_DISABLE, remote_supported_packet,
@@ -6045,6 +6050,10 @@ remote_target::remote_query_supported ()
       if (m_features.packet_set_cmd_state (PACKET_R)
 	  != AUTO_BOOLEAN_FALSE)
 	remote_query_supported_append (&q, "R+");
+
+      if (m_features.packet_set_cmd_state (PACKET_unavailable)
+	  != AUTO_BOOLEAN_FALSE)
+	remote_query_supported_append (&q, "unavailable+");
 
       remote_query_supported_append
 	(&q, "qXfer:libraries:read:in-memory-library+");
@@ -8447,6 +8456,10 @@ Packet: '%s'\n"),
       event->ws.set_no_resumed ();
       event->ptid = minus_one_ptid;
       break;
+    case 'U':
+      event->ws.set_unavailable ();
+      event->ptid = read_ptid (&buf[1], NULL);
+      break;
     }
 }
 
@@ -8860,7 +8873,7 @@ remote_target::wait_as (ptid_t ptid, target_waitstatus *status,
 	     again.  Keep waiting for events.  */
 	  rs->waiting_for_stop_reply = 1;
 	  break;
-	case 'N': case 'T': case 'S': case 'X': case 'W': case 'w':
+	case 'N': case 'T': case 'S': case 'X': case 'W': case 'w': case 'U':
 	  {
 	    /* There is a stop reply to handle.  */
 	    rs->waiting_for_stop_reply = 0;
@@ -16815,6 +16828,9 @@ Show the maximum size of the address (in bits) in a memory packet."), NULL,
 
   add_packet_config_cmd (PACKET_accept_error_message,
 			 "error-message", "error-message", 0);
+
+  add_packet_config_cmd (PACKET_unavailable,
+			 "U stop reply", "unavailable-stop-reply", 0);
 
   /* Assert that we've registered "set remote foo-packet" commands
      for all packet configs.  */

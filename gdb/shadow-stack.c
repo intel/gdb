957c42a52db07e58549646a0b701d09a5e435b23
Christina Schimpe <christina.schimpe@intel.com>
2025-01-20 08:57:39 +0000

    Update copyright years
    This is a result of running
    
    $ cd gdb
    $ ./copyright.py
    
    Also the files gdb.texinfo, refcard.tex and update-netbsd.sh were
    manually adapted.

--- gdb/shadow-stack.c
+++ gdb/shadow-stack.c
@@ -1,6 +1,6 @@
 /* Manage a shadow stack pointer for GDB, the GNU debugger.
 
-   Copyright (C) 2024 Free Software Foundation, Inc.
+   Copyright (C) 2024-2025 Free Software Foundation, Inc.
    This file is part of GDB.
 
    This program is free software; you can redistribute it and/or modify

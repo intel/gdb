243eb600059681db8615e7919573e81f6e7c8f76
Piotr Rudnicki <piotr.rudnicki@intel.com>
2025-02-11 10:36:17 +0100

    gdb, testsuite, rust: fix for empty array
    For the Rust language, to avoid segmentation fault in case of an empty
    array, do not try to copy any elements, but allocate and return
    the empty array immediately.
    
    With the command before the change, gdb crashes with message:
    
    (gdb) set lang rust
    (gdb) p [1;0]
    Fatal signal: Segmentation fault
    
    After the fix in this commit, gdb shows following message:
    (gdb) set lang rust
    (gdb) p [1;0]
    $1 = []
    
    Update the existing test case gdb.rust/expr.exp to verify the change.

--- gdb/rust-lang.c
+++ gdb/rust-lang.c
@@ -1460,7 +1460,7 @@ eval_op_rust_array (struct type *expect_type, struct expression *exp,
   if (copies < 0)
     error (_("Array with negative number of elements"));
 
-  if (noside == EVAL_NORMAL)
+  if (noside == EVAL_NORMAL && copies > 0)
     return value_array (0, std::vector<value *> (copies, elt));
   else
     {

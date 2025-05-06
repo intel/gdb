33fd84228a01525ab9ccac5866843eae263de57c
Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>
2025-05-06 17:49:27 +0200

    gdbserver: allow gradually populating and selectively storing a regcache
    Currently, the regcache can be used by fetching all the registers from
    the target.  For some targets, this could be a costly operation
    because there is a large number of threads with a large register file
    each.  In this patch, we revise the regcache implementation to allow
    populating the contents gradually and storing the registers only when
    they have updated values.
    
    To this aim, we introduce a new register status: REG_DIRTY.  This
    status denotes that a register value has been cached and also updated.
    When invalidating the cache, only the dirty registers are stored to
    the target.  In a typical debug session, it is more likely that only a
    small subset of the register file has changed.  Therefore, selectively
    storing the registers on targets with many threads and registers can
    save substantial costs, with respect to storing the whole set.
    
    The collect_register function now performs a lazy fetch.  If the
    requested register value is not cached yet, it is requested from the
    target.
    
    The supply_register function updates the status of the supplied
    register as follows: if the register was not available in the cache,
    its status becomes REG_VALID, denoting that the value is now cached.
    If the register is supplied again, it becomes REG_DIRTY.
    
    The function that supply the whole register set (supply_regblock and
    registers_from_string) are also updated to compare the present and new
    values of each register, so that we can track the register statuses
    (i.e.  dirty or not) properly.
    
    Regression-tested on an X86_64 Linux target using the native-gdbserver
    and native-extended-gdbserver board files.

--- gdbserver/remote-utils.cc
+++ gdbserver/remote-utils.cc
@@ -1360,7 +1360,9 @@ prepare_resume_reply (char *buf, ptid_t ptid, const target_waitstatus &status)
 void
 output_expedite_registers (char *buf)
 {
-  regcache *regcache = get_thread_regcache (current_thread);
+  /* We are being asked to return only the expedite regs.  Do not
+     fetch the registers alltogether.  */
+  regcache *regcache = get_thread_regcache (current_thread, false);
 
   for (const std::string &expedited_reg : regcache->tdesc->expedite_regs)
     buf = outreg (regcache, find_regno (regcache->tdesc,

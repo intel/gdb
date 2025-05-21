ded13bf7379a13e33e37aabdcab5ad158079897c
Kupczak, Pawel <pawel.kupczak@intel.com>
2025-05-21 08:31:56 +0000

    gdb, amd64: return after stack alignment skip if current_pc is reached
    Make sure we bail out early from amd64_analyze_prologue if CURRENT_PC
    is reached to avoid unnecessary call to amd64_analyze_frame_setup.

--- gdb/amd64-tdep.c
+++ gdb/amd64-tdep.c
@@ -2704,6 +2704,9 @@ amd64_analyze_prologue (gdbarch *gdbarch, CORE_ADDR pc, CORE_ADDR current_pc,
   else
     pc = amd64_analyze_stack_align (pc, current_pc, cache);
 
+  if (current_pc <= pc)
+    return current_pc;
+
   pc = amd64_analyze_frame_setup (gdbarch, pc, current_pc, cache);
   if (current_pc <= pc)
     return current_pc;

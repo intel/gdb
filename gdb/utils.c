8d9e658d1d0832f29e04b8c6c1dabd0a9918bfee
Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>
2025-06-16 19:14:11 +0000

    [temp] Revert "gdb: Introduce RAII signal handler setter"
    This reverts commit 3cfa137ca5327c759fd192ad1f0fdc981bf05b82.
    
    Starting with GDB-16, gdb.sycl/intelgt-live-attach.exp gives an error
    because removing an intelgt inferior that is connected to a gdbserver
    target hangs until a serial input is received.  If the GPU workload is
    running forever, this causes and infinite wait when detaching from the
    host inferior and the auto-attach script attempts to remove the
    intelgt inferiors.
    
    Until we fix the issue properly, revert the two patches that caused
    the problem.

--- gdb/utils.c
+++ gdb/utils.c
@@ -19,7 +19,6 @@
 
 #include <ctype.h>
 #include "gdbsupport/gdb_wait.h"
-#include "gdbsupport/scoped_signal_handler.h"
 #include "event-top.h"
 #include "gdbthread.h"
 #include "fnmatch.h"
@@ -3468,7 +3467,18 @@ wait_to_die_with_timeout (pid_t pid, int *status, int timeout)
   if (timeout > 0)
     {
 #ifdef SIGALRM
-      scoped_signal_handler<SIGALRM> alarm_restore (sigalrm_handler);
+#if defined (HAVE_SIGACTION) && defined (SA_RESTART)
+      struct sigaction sa, old_sa;
+
+      sa.sa_handler = sigalrm_handler;
+      sigemptyset (&sa.sa_mask);
+      sa.sa_flags = 0;
+      sigaction (SIGALRM, &sa, &old_sa);
+#else
+      sighandler_t ofunc;
+
+      ofunc = signal (SIGALRM, sigalrm_handler);
+#endif
 
       alarm (timeout);
 #endif
@@ -3477,6 +3487,11 @@ wait_to_die_with_timeout (pid_t pid, int *status, int timeout)
 
 #ifdef SIGALRM
       alarm (0);
+#if defined (HAVE_SIGACTION) && defined (SA_RESTART)
+      sigaction (SIGALRM, &old_sa, NULL);
+#else
+      signal (SIGALRM, ofunc);
+#endif
 #endif
     }
   else

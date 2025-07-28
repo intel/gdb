cfea7629e300d09a6ac5e48ed1ee1ba1fd9d7c46
Christina Schimpe <christina.schimpe@intel.com>
2025-07-28 07:00:45 -0700

    gdb: change default initialization for register numbers on x86
    As defined by the enums amd64_regnum and i386_regnum the register
    numbering starts at 0.
    Defaults for register numbers are currently set to 0 which seems to
    be the wrong default.  Set them to -1 instead.  Configure the right
    register number if we find out it's supported in i386_gdbarch_init.
    
    Similarly we don't have to set the num_*regname* variables to 0 in
    i386_gdbarch_init, as it's already assigned to 0 by default.
    
    Approved-By: Andrew Burgess <aburgess@redhat.com>
    Approved-By: Simon Marchi <simon.marchi@efficios.com>

--- gdb/i386-tdep.c
+++ gdb/i386-tdep.c
@@ -9119,22 +9119,6 @@ i386_gdbarch_init (struct gdbarch_info info, struct gdbarch_list *arches)
   tdep->byte_names = i386_byte_names;
   tdep->word_names = i386_word_names;
 
-  /* No upper YMM registers.  */
-  tdep->ymmh_register_names = NULL;
-  tdep->ymm0h_regnum = -1;
-
-  /* No upper ZMM registers.  */
-  tdep->zmmh_register_names = NULL;
-  tdep->zmm0h_regnum = -1;
-
-  /* No high XMM registers.  */
-  tdep->xmm_avx512_register_names = NULL;
-  tdep->xmm16_regnum = -1;
-
-  /* No upper YMM16-31 registers.  */
-  tdep->ymm16h_register_names = NULL;
-  tdep->ymm16h_regnum = -1;
-
   /* No eip register.  */
   tdep->eip_regnum = -1;
 
@@ -9146,19 +9130,6 @@ i386_gdbarch_init (struct gdbarch_info info, struct gdbarch_list *arches)
   tdep->num_dword_regs = 0;
   tdep->num_mmx_regs = 8;
   tdep->num_ymm_regs = 0;
-
-  /* No AVX512 registers.  */
-  tdep->k0_regnum = -1;
-  tdep->num_zmm_regs = 0;
-  tdep->num_ymm_avx512_regs = 0;
-  tdep->num_xmm_avx512_regs = 0;
-
-  /* No PKEYS registers  */
-  tdep->pkru_regnum = -1;
-  tdep->num_pkeys_regs = 0;
-
-  /* No segment base registers.  */
-  tdep->fsbase_regnum = -1;
 
   /* No AMX registers.  */
   tdep->tilecfg_regnum = -1;

398aa4ce0050bbd54aff9d72c445a885e675d257
Matti Puputti <matti.puputti@intel.com>
2025-08-22 09:10:13 +0000

    [coregen] testsuite, sycl: Add testsuite coregen-nd_item-workitem-ids
    Add a test which checks the workgroup values for several threads of
    interest in a CoreGen generated corefile.

---
+++ gdb/testsuite/gdb.sycl/coregen-nd_item-workitem-ids.cpp
@@ -0,0 +1,83 @@
+/* This testcase is part of GDB, the GNU debugger.
+
+   Copyright 2025 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <sycl/sycl.hpp>
+#include <iostream>
+#include "../lib/sycl-util.cpp"
+
+int
+main (int argc, char *argv[])
+{
+  constexpr size_t DIM0 = 60;
+  constexpr size_t DIM1 = 40;
+  constexpr size_t DIM2 = 20;
+
+  int in[DIM0][DIM1][DIM2];
+  int out[DIM0][DIM1][DIM2];
+
+  /* Initialize the input.  */
+  int val = 1;
+  for (unsigned int i = 0; i < DIM0; i++)
+    for (unsigned int j = 0; j < DIM1; j++)
+      for (unsigned int k = 0; k < DIM2; k++)
+	in[i][j][k] = val++;
+
+  { /* Extra scope enforces waiting on the kernel.  */
+    sycl::queue deviceQueue {get_sycl_queue (argc, argv)};
+    sycl::range<3> dataRange {DIM0, DIM1, DIM2};
+    sycl::buffer<int, 3> bufferIn {&in[0][0][0], dataRange};
+    sycl::buffer<int, 3> bufferOut {&out[0][0][0], dataRange};
+
+    deviceQueue.submit ([&] (sycl::handler& cgh)
+      {
+	auto accessorIn = bufferIn.get_access<sycl::access::mode::read> (cgh);
+	auto accessorOut
+	  = bufferOut.get_access<sycl::access::mode::write> (cgh);
+
+	sycl::nd_range<3> kernel_range (dataRange, sycl::range<3> (5, 5, 5));
+	cgh.parallel_for (kernel_range, [=] (sycl::nd_item<3> item)
+	  [[sycl::reqd_sub_group_size(32)]]
+	  {
+	    sycl::id<3> gid = item.get_global_id ();
+
+	    size_t thread_workgroup0 = item.get_group (0);
+	    size_t thread_workgroup1 = item.get_group (1);
+	    size_t thread_workgroup2 = item.get_group (2);
+
+	    size_t workitem_global_id0 = item.get_global_id (0);
+	    size_t workitem_global_id1 = item.get_global_id (1);
+	    size_t workitem_global_id2 = item.get_global_id (2);
+
+	    size_t workitem_local_id0 = item.get_local_id (0);
+	    size_t workitem_local_id1 = item.get_local_id (1);
+	    size_t workitem_local_id2 = item.get_local_id (2);
+
+	    /* Spin a while, before triggering pagefault,
+	       to let all threads reach this line.  */
+	    size_t count = 1e5;
+	    while (count > 0) count--;
+	    int *p = nullptr;
+	    int in_elem = *p;
+
+	    accessorOut[gid] = in_elem; /* kernel-last-line */
+	  });
+      });
+  }
+
+  std::cout << "Done" << std::endl;
+  return 0;
+}

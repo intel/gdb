6137d9e3be4b8c88f0621052332ed2cfbc45bd16
Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>
2025-11-25 10:17:17 +0000

    gdb, multi-target: detect intended stop events of all-stop targets
    In stop_all_threads we stop all-stop targets by sending them
    interrupts (CtrlC), because running all-stop targets cannot be sent
    other requests.  Therefore, an all-stop target responds with a SIGINT
    instead of GDB_SIGNAL_0.  (Note: in contrast, a non-stop target is
    stopped with a 't' packet, resulting in the target responding with a
    GDB_SIGNAL_0.)
    
    Upon receiving a non-zero signal, we save the stop event in handle_one
    to be reported later.  This causes the behavior that after
    interrupting a multi-target debug session, "continue" shows the
    pending stop event instead of resuming the inferiors.
    
    Suppose we have a combination of non-stop + all-stop targets.
    E.g. Inferior 1 running on the native target, inferior 2 on the remote
    target.  Both inferiors are currently executing and then we interrupt
    with a Ctrl-C:
    
        ...
        ^C
        Thread 1.1 "test" received signal SIGINT, Interrupt.
        [Switching to thread 1.1 (Thread 0x7ffff4e44200 (LWP 16749))]
        0x00007ffff776dc9b in sched_yield () from /lib/x86_64-linux-gnu/libc.so.6
        (gdb) continue
        Continuing.
    
        Thread 2.1 "a.out" received signal SIGINT, Interrupt.
        [Switching to thread 2.1 (Thread 429389.429389)]
        0x00007ffff7e6878a in clock_nanosleep () from target:/lib/x86_64-linux-gnu/libc.so.6
        (gdb)
    
    The SIGINT event from Thread 2.1 above should not have been there.
    Instead, we should have simply continued.
    
    Fix the problem by identifying intended stop events coming from
    all-stop targets in stop_all_threads, and convert them to
    GDB_SIGNAL_0, so that those events are not saved as pending events.
    Also add a regression test.

---
+++ gdb/testsuite/gdb.multi/interrupt-running-allstop.c
@@ -0,0 +1,34 @@
+/* This testcase is part of GDB, the GNU debugger.
+
+   Copyright 2025 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <stdlib.h>
+#include <unistd.h>
+
+volatile int spin = 1;
+
+int
+main (void)
+{
+  alarm (60);
+
+  while (spin)
+    usleep (20);
+
+  int a = 42; /* break-here */
+
+  return 0;
+}
